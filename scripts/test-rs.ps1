205d85a301e981ac192d99e1c22a3dc77a22de04
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-02-05 23:09:33 +0800

    更新 test-rs.ps1

--- scripts/test-rs.ps1
+++ scripts/test-rs.ps1
@@ -3,6 +3,6 @@ Set-Location ..
 $pkg_config = Resolve-Path .pixi/envs/dev/Library/bin
 $dll_path=Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/bin
 $env:PATH=$env:PATH+";$pkg_config;$dll_path"
-$env:PKG_CONFIG_PATH=Resolve-Path vcpkg_installed/x64-windows/lib/pkgconfig
+$env:PKG_CONFIG_PATH=Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/lib/pkgconfig
 
 pixi run cargo test

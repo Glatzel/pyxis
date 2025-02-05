ac43a18e2ae31e8d92c0ac5eec2bab7a9c2cdc72
Glatzel <893016099@qq.com>
2025-02-05 22:31:21 +0800

    fix test scripts

--- scripts/test-rs.ps1
+++ scripts/test-rs.ps1
@@ -1,7 +1,7 @@
 Set-Location $PSScriptRoot
 Set-Location ..
 $pkg_config = Resolve-Path .pixi/envs/dev/Library/bin
-$dll_path=Resolve-Path vcpkg_installed/x64-windows/bin
+$dll_path=Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/bin
 $env:PATH=$env:PATH+";$pkg_config;$dll_path"
 $env:PKG_CONFIG_PATH=Resolve-Path vcpkg_installed/x64-windows/lib/pkgconfig
 

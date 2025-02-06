f9ec2027cf6bc4484526861dbfb1b7daf2f620e7
Glatzel <893016099@qq.com>
2025-02-06 18:27:14 +0800

    add rust cov

--- scripts/test-rs.ps1
+++ scripts/test-rs.ps1
@@ -1,8 +1,13 @@
 Set-Location $PSScriptRoot
 Set-Location ..
 $pkg_config = Resolve-Path .pixi/envs/dev/Library/bin
-$dll_path=Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/bin
-$env:PATH=$env:PATH+";$pkg_config;$dll_path"
-$env:PKG_CONFIG_PATH=Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/lib/pkgconfig
+$dll_path = Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/bin
+$env:PATH = $env:PATH + ";$pkg_config;$dll_path"
+$env:PKG_CONFIG_PATH = Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/lib/pkgconfig
 
-pixi run cargo test
+if ($env:CI) {
+    pixi run cargo llvm-cov --all-features --workspace --lcov --output-path lcov.info
+}
+else {
+    pixi run cargo test
+}

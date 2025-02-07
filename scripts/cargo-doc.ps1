e67a120adb4458995958936e152bc526a68e9a9d
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-02-07 23:00:15 +0800

    Update cargo-doc.ps1

--- scripts/cargo-doc.ps1
+++ scripts/cargo-doc.ps1
@@ -1,11 +1,11 @@
 Set-Location $PSScriptRoot
 Set-Location ..
-& $PSScriptRoot/set-env.ps1
+if(-not $env:CI){
+    & $PSScriptRoot/set-env.ps1
+}
 
 pixi run cargo doc --no-deps --all
 
 Remove-Item ./dist/rust-doc-*.zip -Recurse -Force -ErrorAction SilentlyContinue
 New-Item ./dist -ItemType Directory -ErrorAction SilentlyContinue
-$version=cargo metadata --format-version=1 --no-deps | jq '.packages[0].version'
-$version="$version".Replace("""","")
-Compress-Archive ./target/doc "./dist/rust-doc-$version.zip"
+Compress-Archive ./target/doc "./dist/rust-doc.zip"

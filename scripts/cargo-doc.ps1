2ca9c4196cde12ed1f7c74a5aee5ebe013b4d791
Glatzel <893016099@qq.com>
2025-02-07 20:26:22 +0800

    ci: fix release asset

--- scripts/cargo-doc.ps1
+++ scripts/cargo-doc.ps1
@@ -3,3 +3,9 @@ Set-Location ..
 & $PSScriptRoot/set-env.ps1
 
 pixi run cargo doc --no-deps --all
+
+Remove-Item ./dist/rust-doc-*.zip -Recurse -Force -ErrorAction SilentlyContinue
+New-Item ./dist -ItemType Directory -ErrorAction SilentlyContinue
+$version=cargo metadata --format-version=1 --no-deps | jq '.packages[0].version'
+$version="$version".Replace("""","")
+Compress-Archive ./target/doc "./dist/rust-doc-$version.zip"

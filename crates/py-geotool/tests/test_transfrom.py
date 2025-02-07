24b5ce053e97ef054dba6c82d7f176b56396649f
Glatzel <893016099@qq.com>
2025-02-07 09:21:55 +0800

    fix: python function return type for scalar array

--- crates/py-geotool/tests/test_transfrom.py
+++ crates/py-geotool/tests/test_transfrom.py
@@ -38,17 +38,22 @@ def test_datum_compense_scalar():
     ("x", "y"),
     [
         pytest.param(
+            np.array([469704.6693], np.float64),
+            np.array([2821940.796], np.float64),
+            id="ndarry1-ndarry1",
+        ),
+        pytest.param(
             np.array([469704.6693, 469704.6693], np.float64),
             np.array([2821940.796, 2821940.796], np.float64),
-            id="ndarry-ndarry",
+            id="ndarry2-ndarry2",
         ),
     ],
 )
 def test_datum_compense_vector(x, y):
-    expected_x = (469706.56912942487, 469706.56912942487)
-    expected_y = (2821763.831232311, 2821763.831232311)
+    expected_x = 469706.56912942487
+    expected_y = 2821763.831232311
 
     test_x, test_y = geotool.datum_compense(xc=x, yc=y, hb=400)
 
-    assert test_x == pytest.approx(expected_x)
-    assert test_y == pytest.approx(expected_y)
+    assert test_x[0] == pytest.approx(expected_x)
+    assert test_y[0] == pytest.approx(expected_y)

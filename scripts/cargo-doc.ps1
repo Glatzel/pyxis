339a8f52339ef574b4887d143f12c98fd7e3e906
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-02-07 23:13:08 +0800

    Update cargo-doc.ps1

--- scripts/cargo-doc.ps1
+++ scripts/cargo-doc.ps1
@@ -1,10 +1,14 @@
 Set-Location $PSScriptRoot
 Set-Location ..
-if(-not $env:CI){
+if($env:CI){
+    cargo doc --no-deps --all
+}
+else{
     & $PSScriptRoot/set-env.ps1
+    pixi run cargo doc --no-deps --all
 }
 
-pixi run cargo doc --no-deps --all
+
 
 Remove-Item ./dist/rust-doc.zip -Force -ErrorAction SilentlyContinue
 New-Item ./dist -ItemType Directory -ErrorAction SilentlyContinue

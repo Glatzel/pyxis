99bc05a5f61530ec5827f723bbad3ca367e6e61a
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-02-07 10:50:58 +0800

    ci: add doctest (#8)

--- scripts/test-rs.ps1
+++ scripts/test-rs.ps1
@@ -5,10 +5,18 @@ $dll_path = Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/bin
 $env:PATH = $env:PATH + ";$pkg_config;$dll_path"
 $env:PKG_CONFIG_PATH = Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/lib/pkgconfig
 
-if ($env:CI) {
-    pixi run cargo test
+if ( $env:CI ) {
+    Write-Output "::group::nextest"
     pixi run cargo llvm-cov nextest
-    pixi run cargo llvm-cov --all-features --workspace --lcov --output-path lcov.info
+    Write-Output "::endgroup::"
+
+    Write-Output "::group::doctest"
+    pixi run cargo llvm-cov --doc
+    Write-Output "::endgroup::"
+
+    Write-Output "::group::cov"
+    pixi run cargo llvm-cov --doctests --all-features --workspace --lcov --output-path lcov.info
+    Write-Output "::endgroup::"
 }
 else {
     pixi run cargo test

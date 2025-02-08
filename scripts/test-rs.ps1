be85cd8f7b21487e87971bd14f25e225f5eb1d80
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-02-08 15:32:34 +0800

    Update test-rs.ps1

--- scripts/test-rs.ps1
+++ scripts/test-rs.ps1
@@ -6,16 +6,16 @@ $env:PATH = $env:PATH + ";$pkg_config;$dll_path"
 $env:PKG_CONFIG_PATH = Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/lib/pkgconfig
 
 if ( $env:CI ) {
-    # Write-Output "::group::nextest"
-    # pixi run cargo llvm-cov nextest
-    # Write-Output "::endgroup::"
+    Write-Output "::group::nextest"
+    pixi run cargo llvm-cov nextest
+    Write-Output "::endgroup::"
 
     # Write-Output "::group::doctest"
     # pixi run cargo llvm-cov --doc
     # Write-Output "::endgroup::"
 
     Write-Output "::group::cov"
-    pixi run cargo llvm-cov --all-features --workspace --lcov --output-path lcov.info
+    pixi run cargo llvm-cov --doctests --all-features --workspace --lcov --output-path lcov.info
     Write-Output "::endgroup::"
 }
 else {

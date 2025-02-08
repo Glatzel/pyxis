2b88354a2a351dd87ffdc1da7f27efd995b6f2a0
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-02-08 16:03:10 +0800

    Update test_main.rs

--- crates/geotool-cli/tests/test_main.rs
+++ crates/geotool-cli/tests/test_main.rs
@@ -7,8 +7,8 @@ fn test_main_with_args() {
     let mut cmd = Command::cargo_bin("geotool").unwrap();
 
     // Pass in command-line arguments, for example: `program_name arg1 arg2`
-    cmd.arg("transform").arg("--help");
+    cmd.arg("--help");
 
     // Assert the output printed to stdout (modify expected output as needed)
-    cmd.assert().success();
+    cmd.assert().success().stdout(predicate::str::contains("transform"));
 }

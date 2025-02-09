f112936a2adf1829bbee8a295f3f1000fd011f1e
Glatzel <893016099@qq.com>
2025-02-09 14:19:05 +0800

    fix

--- scripts/cargo-machete.ps1
+++ scripts/cargo-machete.ps1
@@ -3,7 +3,9 @@ Set-Location $PSScriptRoot
 Set-Location ..
 $ErrorActionPreference = 'Stop'
 
-try{cargo machete --with-metadata}
+try{
+    cargo machete --with-metadata
+}
 catch{
     cargo install cargo-machete
     cargo machete --with-metadata

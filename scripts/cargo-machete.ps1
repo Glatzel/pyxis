3d3345668eaed1690537e9677e77d606602d2e3e
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-02-10 16:39:49 +0800

    perf: optimize test and coverage (#35)

--- scripts/cargo-machete.ps1
+++ scripts/cargo-machete.ps1
@@ -1,14 +1,4 @@
-$current_dir=Resolve-Path $PWD
 Set-Location $PSScriptRoot
 Set-Location ..
-$ErrorActionPreference = 'Stop'
 
-try{
-    cargo machete --with-metadata
-}
-catch{
-    cargo install cargo-machete
-    cargo machete --with-metadata
-}
-
-Set-Location $current_dir
+cargo machete --with-metadata

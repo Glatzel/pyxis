72edc0eefab642c87340e197e5a4c8b0b52a9f31
Glatzel <893016099@qq.com>
2025-02-11 16:56:57 +0800

    fix

--- scripts/cargo-doc.ps1
+++ scripts/cargo-doc.ps1
@@ -8,8 +8,6 @@ else{
     pixi run cargo doc --no-deps --all
 }
 
-
-
 Remove-Item ./dist/rust-doc.zip -Force -ErrorAction SilentlyContinue
 New-Item ./dist -ItemType Directory -ErrorAction SilentlyContinue
 Compress-Archive ./target/doc "./dist/rust-doc.zip"

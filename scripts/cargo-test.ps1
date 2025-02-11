f15e9698183148c01fde25ddab0d034d77fac524
Glatzel <893016099@qq.com>
2025-02-11 18:38:07 +0800

    fix

--- scripts/cargo-test.ps1
+++ scripts/cargo-test.ps1
@@ -1,10 +1,6 @@
 Set-Location $PSScriptRoot
 Set-Location ..
-$ErrorActionPreference = "Stop"
-$pkg_config = Resolve-Path .pixi/envs/dev/Library/bin
-$dll_path = Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/bin
-$env:PATH = $env:PATH + ";$pkg_config;$dll_path"
-$env:PKG_CONFIG_PATH = Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/lib/pkgconfig
+& $PSScriptRoot/set-env.ps1
 
 pixi run cargo +nightly llvm-cov --no-report --all-features --workspace nextest
 $code=$LASTEXITCODE

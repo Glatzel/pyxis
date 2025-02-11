559a41ce1e57441ea6571681708964df3253d9fc
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-02-11 18:43:38 +0800

    fix: cargo test never fail (#42)

--- scripts/cargo-test.ps1
+++ scripts/cargo-test.ps1
@@ -1,13 +1,11 @@
 Set-Location $PSScriptRoot
 Set-Location ..
-$pkg_config = Resolve-Path .pixi/envs/dev/Library/bin
-$dll_path = Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/bin
-$env:PATH = $env:PATH + ";$pkg_config;$dll_path"
-$env:PKG_CONFIG_PATH = Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/lib/pkgconfig
+& $PSScriptRoot/set-env.ps1
 
 pixi run cargo +nightly llvm-cov --no-report --all-features --workspace nextest
+$code=$LASTEXITCODE
 pixi run cargo +nightly llvm-cov --no-report --all-features --workspace --doc
-
+$code=$code + $LASTEXITCODE
 if ( $env:CI ) {
     pixi run cargo +nightly llvm-cov report
     pixi run cargo +nightly llvm-cov report --lcov --output-path lcov.info
@@ -15,3 +13,6 @@ if ( $env:CI ) {
 else {
     pixi run cargo +nightly llvm-cov report
 }
+$code = $code + $LASTEXITCODE
+Write-Output $code
+exit $code
--- scripts/set-env.ps1
+++ scripts/set-env.ps1
@@ -2,6 +2,7 @@ $current_dir = Resolve-Path $PWD
 Set-Location $PSScriptRoot
 Set-Location ..
 $pkg_config = Resolve-Path .pixi/envs/dev/Library/bin
-$env:PATH=$env:PATH+";$pkg_config"
+$dll_path = Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/bin
+$env:PATH = $env:PATH + ";$pkg_config;$dll_path"
 $env:PKG_CONFIG_PATH=Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/lib/pkgconfig
 Set-Location $current_dir

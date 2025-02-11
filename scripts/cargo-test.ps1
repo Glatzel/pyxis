74b2129c0da6765d6194f222edc57a539a3304b6
Glatzel <893016099@qq.com>
2025-02-11 18:23:17 +0800

    fix

--- scripts/cargo-test.ps1
+++ scripts/cargo-test.ps1
@@ -1,13 +1,15 @@
 Set-Location $PSScriptRoot
 Set-Location ..
+$ErrorActionPreference = "Stop"
 $pkg_config = Resolve-Path .pixi/envs/dev/Library/bin
 $dll_path = Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/bin
 $env:PATH = $env:PATH + ";$pkg_config;$dll_path"
 $env:PKG_CONFIG_PATH = Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows/lib/pkgconfig
 
 pixi run cargo +nightly llvm-cov --no-report --all-features --workspace nextest
+$code=$LASTEXITCODE
 pixi run cargo +nightly llvm-cov --no-report --all-features --workspace --doc
-
+$code=$code -and $LASTEXITCODE
 if ( $env:CI ) {
     pixi run cargo +nightly llvm-cov report
     pixi run cargo +nightly llvm-cov report --lcov --output-path lcov.info
@@ -15,5 +17,7 @@ if ( $env:CI ) {
 else {
     pixi run cargo +nightly llvm-cov report
 }
-
-if ($LASTEXITCODE -ne 0) { throw "test fail" }
+$code=$code -and $LASTEXITCODE
+if (-not $code){
+    exit 1
+}

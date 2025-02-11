c3fb13ccf6a3c8a57fb5a1d4535037860c57a954
Glatzel <893016099@qq.com>
2025-02-11 19:44:21 +0800

    fix

--- scripts/cargo-test.ps1
+++ scripts/cargo-test.ps1
@@ -2,17 +2,15 @@ Set-Location $PSScriptRoot
 Set-Location ..
 & $PSScriptRoot/set-env.ps1
 
-pixi run cargo +nightly llvm-cov --no-report --all-features --workspace nextest
-$code=$LASTEXITCODE
-pixi run cargo +nightly llvm-cov --no-report --all-features --workspace --doc
-$code=$code + $LASTEXITCODE
+pixi run cargo +nightly llvm-cov --no-report --all-features --workspace --branch nextest
+$code = $LASTEXITCODE
+pixi run cargo +nightly llvm-cov --no-report --all-features --workspace --branch --doc
+$code = $code + $LASTEXITCODE
 if ( $env:CI ) {
     pixi run cargo +nightly llvm-cov report
     pixi run cargo +nightly llvm-cov report --lcov --output-path lcov.info
 }
-else {
-    pixi run cargo +nightly llvm-cov report
-}
+else { pixi run cargo +nightly llvm-cov report }
 $code = $code + $LASTEXITCODE
 Write-Output $code
 exit $code

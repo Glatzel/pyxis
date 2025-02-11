8de6782bd53d72fda00339667f04488d622354b1
Glatzel <893016099@qq.com>
2025-02-11 16:43:19 +0800

    fix

--- scripts/cargo-clippy.ps1
+++ scripts/cargo-clippy.ps1
@@ -2,10 +2,11 @@ Set-Location $PSScriptRoot
 Set-Location ..
 
 if ($env:CI) {
+    cargo fmt --all -- --check
     cargo clippy --all-targets --all-features
 }
 else {
     & $PSScriptRoot/set-env.ps1
-    pixi run cargo fmt
-    pixi run cargo clippy --fix --all-targets --all-features
+    pixi run cargo fmt -- --order-imports
+    pixi run cargo clippy --fix --all-targets --all-features 
 }

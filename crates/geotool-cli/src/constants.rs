ff77f7f9630af3472a7f72f500a7e806748a2055
Glatzel <893016099@qq.com>
2025-02-12 18:54:11 +0800

    fix

--- crates/geotool-cli/src/constants.rs
+++
@@ -1,9 +0,0 @@
-use std::sync::LazyLock;
-
-use console::Style;
-// console style
-pub static TRACE_STYLE: LazyLock<Style> = LazyLock::new(|| Style::new().color256(99));
-pub static DEBUG_STYLE: LazyLock<Style> = LazyLock::new(|| Style::new().blue());
-pub static INFO_STYLE: LazyLock<Style> = LazyLock::new(|| Style::new().green());
-pub static WARN_STYLE: LazyLock<Style> = LazyLock::new(|| Style::new().yellow().bold());
-pub static ERROR_STYLE: LazyLock<Style> = LazyLock::new(|| Style::new().red().bold());

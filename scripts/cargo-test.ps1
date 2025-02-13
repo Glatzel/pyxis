b01604e8bc0ffaf1181606831b5f333066f47f7d
Glatzel <893016099@qq.com>
2025-02-13 15:06:05 +0800

    ci: show cargo test report

--- scripts/cargo-test.ps1
+++ scripts/cargo-test.ps1
@@ -2,15 +2,25 @@ Set-Location $PSScriptRoot
 Set-Location ..
 & $PSScriptRoot/set-env.ps1
 
+write-output "::group::nextest"
 pixi run cargo +nightly llvm-cov --no-report --all-features --workspace nextest
 $code = $LASTEXITCODE
+Write-Output "::endgroup::"
+
+write-output "::group::test"
 pixi run cargo +nightly llvm-cov --no-report --all-features --workspace --doc
 $code = $code + $LASTEXITCODE
-pixi run cargo +nightly llvm-cov report
+Write-Output "::endgroup::"
 
+write-output "::group::report"
+pixi run cargo +nightly llvm-cov report
+Write-Output "::endgroup::"
+
+write-output "::group::report lcov"
 if ( $env:CI ) {
     pixi run cargo +nightly llvm-cov report --lcov --output-path lcov.info
 }
+Write-Output "::endgroup::"
 
 $code = $code + $LASTEXITCODE
 Write-Output $code

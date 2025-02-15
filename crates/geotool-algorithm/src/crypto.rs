cd12576d25a5916ab9de18fd91fafb5069f8c2c7
Glatzel <893016099@qq.com>
2025-02-15 14:33:41 +0800

    fix

--- crates/geotool-algorithm/src/crypto.rs
+++ crates/geotool-algorithm/src/crypto.rs
@@ -12,6 +12,7 @@ pub enum CryptoSpace {
 const EARTH_R: f64 = 6378137.0;
 const _X_PI: f64 = PI * 3000.0 / 180.0;
 const EE: f64 = 0.006_693_421_622_965_943;
+
 fn out_of_china(lon: f64, lat: f64) -> bool {
     if !(72.004..=137.8347).contains(&lon) {
         return true;
@@ -21,6 +22,7 @@ fn out_of_china(lon: f64, lat: f64) -> bool {
     }
     false
 }
+
 fn transform(x: f64, y: f64) -> (f64, f64) {
     let xy = x * y;
     let abs_x = x.abs().sqrt();
@@ -45,6 +47,7 @@ fn transform(x: f64, y: f64) -> (f64, f64) {
 
     (lon, lat)
 }
+
 fn delta(lon: f64, lat: f64) -> (f64, f64) {
     let (d_lon, d_lat) = transform(lon - 105.0, lat - 35.0);
     let mut d_lat = d_lat;
@@ -57,12 +60,13 @@ fn delta(lon: f64, lat: f64) -> (f64, f64) {
     d_lon = (d_lon * 180.0) / (EARTH_R / sqrt_magic * (rad_lat).cos() * PI);
     (d_lon, d_lat)
 }
+
 /// Converts coordinates from `BD09` to `GCJ02` coordinate system.
 ///
 /// # Arguments
 ///
-/// - `lon`: Longitude in `BD09` coordinate system.
-/// - `lat`: Latitude in `BD09` coordinate system.
+/// - `bd09_lon`: Longitude in `BD09` coordinate system.
+/// - `bd09_lat`: Latitude in `BD09` coordinate system.
 ///
 /// # Returns
 ///
@@ -87,12 +91,13 @@ pub fn bd09_to_gcj02(bd09_lon: f64, bd09_lat: f64) -> (f64, f64) {
     let gcj02_lat = z * theta.sin();
     (gcj02_lon, gcj02_lat)
 }
+
 /// Converts coordinates from `GCJ02` to `WGS84` coordinate system.
 ///
 /// # Arguments
 ///
-/// - `lon`: Longitude in `GCJ02` coordinate system.
-/// - `lat`: Latitude in `GCJ02` coordinate system.
+/// - `gcj02_lon`: Longitude in `GCJ02` coordinate system.
+/// - `gcj02_lat`: Latitude in `GCJ02` coordinate system.
 ///
 /// # Returns
 ///
@@ -108,19 +113,20 @@ pub fn bd09_to_gcj02(bd09_lon: f64, bd09_lat: f64) -> (f64, f64) {
 /// assert_approx_eq!(f64, p.0, 121.0917077 , epsilon = 1e-5);
 /// assert_approx_eq!(f64, p.1, 30.6107779 , epsilon = 1e-5);
 /// ```
-pub fn gcj02_to_wgs84(lon: f64, lat: f64) -> (f64, f64) {
-    if out_of_china(lon, lat) {
-        return (lon, lat);
+pub fn gcj02_to_wgs84(gcj02_lon: f64, gcj02_lat: f64) -> (f64, f64) {
+    if out_of_china(gcj02_lon, gcj02_lat) {
+        return (gcj02_lon, gcj02_lat);
     }
-    let (d_lon, d_lat) = delta(lon, lat);
-    (lon - d_lon, lat - d_lat)
+    let (d_lon, d_lat) = delta(gcj02_lon, gcj02_lat);
+    (gcj02_lon - d_lon, gcj02_lat - d_lat)
 }
+
 /// Converts coordinates from `BD09` to `WGS84` coordinate system.
 ///
 /// # Arguments
 ///
-/// - `lon`: Longitude in `BD09` coordinate system.
-/// - `lat`: Latitude in `BD09` coordinate system.
+/// - `bd09_lon`: Longitude in `BD09` coordinate system.
+/// - `bd09_lat`: Latitude in `BD09` coordinate system.
 ///
 /// # Returns
 ///
@@ -136,10 +142,11 @@ pub fn gcj02_to_wgs84(lon: f64, lat: f64) -> (f64, f64) {
 /// assert_approx_eq!(f64, p.0, 121.09170577473259, epsilon = 1e-6);
 /// assert_approx_eq!(f64, p.1, 30.610767662599578, epsilon = 1e-6);
 /// ```
-pub fn bd09_to_wgs84(lon: f64, lat: f64) -> (f64, f64) {
-    let (gcj_lon, gcj_lat) = bd09_to_gcj02(lon, lat);
+pub fn bd09_to_wgs84(bd09_lon: f64, bd09_lat: f64) -> (f64, f64) {
+    let (gcj_lon, gcj_lat) = bd09_to_gcj02(bd09_lon, bd09_lat);
     gcj02_to_wgs84(gcj_lon, gcj_lat)
 }
+
 /// Converts coordinates from `GCJ02` to `BD09` coordinate system.
 ///
 /// # Arguments
@@ -168,6 +175,7 @@ pub fn gcj02_to_bd09(lon: f64, lat: f64) -> (f64, f64) {
     let dest_lat = z * (theta).sin() + 0.006;
     (dest_lon, dest_lat)
 }
+
 /// Converts coordinates from `WGS84` to `GCJ02` coordinate system.
 ///
 /// # Arguments
@@ -197,6 +205,7 @@ pub fn wgs84_to_gcj02(lon: f64, lat: f64) -> (f64, f64) {
     let (d_lon, d_lat) = delta(lon, lat);
     (lon + d_lon, lat + d_lat)
 }
+
 /// Converts coordinates from `BD09` to `WGS84` coordinate system.
 ///
 /// # Arguments
@@ -207,8 +216,8 @@ pub fn wgs84_to_gcj02(lon: f64, lat: f64) -> (f64, f64) {
 /// # Returns
 ///
 /// A tuple `(lon, lat)` representing the coordinates in the `WGS84` coordinate system:
-/// - `lon`: Longitude in the `WGS84` coordinate system.
-/// - `lat`: Latitude in the `WGS84` coordinate system.
+/// - `wgs84_lon`: Longitude in the `WGS84` coordinate system.
+/// - `wgs84_lat`: Latitude in the `WGS84` coordinate system.
 ///
 /// # Example
 /// ```
@@ -218,8 +227,8 @@ pub fn wgs84_to_gcj02(lon: f64, lat: f64) -> (f64, f64) {
 /// assert_approx_eq!(f64, p.0, 121.10271691314193, epsilon = 1e-5);
 /// assert_approx_eq!(f64, p.1, 30.614836298418275, epsilon = 1e-5);
 /// ```
-pub fn wgs84_to_bd09(lon: f64, lat: f64) -> (f64, f64) {
-    let (gcj_lon, gcj_lat) = wgs84_to_gcj02(lon, lat);
+pub fn wgs84_to_bd09(wgs84_lon: f64, wgs84_lat: f64) -> (f64, f64) {
+    let (gcj_lon, gcj_lat) = wgs84_to_gcj02(wgs84_lon, wgs84_lat);
     gcj02_to_bd09(gcj_lon, gcj_lat)
 }
 
@@ -253,14 +262,14 @@ pub fn gcj02_to_wgs84_exact(
         d_lon = tmp_lon - gcj_lon;
         d_lat = tmp_lat - gcj_lat;
 
-        // log message only under debug mode
+        // print message only under debug mode
         #[cfg(debug_assertions)]
         {
-            tracing::trace!("step: {i}");
-            tracing::trace!("wgs_lon: {wgs_lon}, wgs_lat: {wgs_lat}");
-            tracing::trace!("d_lon: {d_lon:.6e}, d_lat: {d_lat:.6e}");
-            tracing::trace!("p_lon: {p_lon}, p_lat: {p_lat}");
-            tracing::trace!("m_lon: {m_lon}, m_lat: {m_lat}");
+            println!("step: {i}");
+            println!("wgs_lon: {wgs_lon}, wgs_lat: {wgs_lat}");
+            println!("d_lon: {d_lon:.6e}, d_lat: {d_lat:.6e}");
+            println!("p_lon: {p_lon}, p_lat: {p_lat}");
+            println!("m_lon: {m_lon}, m_lat: {m_lat}");
         }
 
         if d_lat.abs() < threshold && d_lon.abs() < threshold {
@@ -277,15 +286,15 @@ pub fn gcj02_to_wgs84_exact(
             m_lat = wgs_lat;
         }
     }
-    // log message only under debug mode
+    // print message only under debug mode
     #[cfg(debug_assertions)]
     {
-        tracing::warn!("Exeed max iteration number: {max_iter}");
+        println!("Exeed max iteration number: {max_iter}");
     }
     ((m_lon + p_lon) / 2.0, (m_lat + p_lat) / 2.0)
 }
 
-// distance calculate the distance between point(lat_a, lon_a) and point(lat_b, lon_b), unit in meter.
+/// distance calculate the distance between point(lat_a, lon_a) and point(lat_b, lon_b), unit in meter.
 pub fn distance_geo(lon_a: f64, lat_a: f64, lon_b: f64, lat_b: f64) -> f64 {
     let arc_lat_a = lat_a * PI / 180.0;
     let arc_lat_b = lat_b * PI / 180.0;
@@ -298,17 +307,12 @@ pub fn distance_geo(lon_a: f64, lat_a: f64, lon_b: f64, lat_b: f64) -> f64 {
 
 #[cfg(test)]
 mod tests {
-    use tracing_subscriber::filter::LevelFilter;
-    use tracing_subscriber::layer::SubscriberExt;
-    use tracing_subscriber::util::SubscriberInitExt;
+
     #[test]
     fn test_gcj2wgs_exact() {
-        tracing_subscriber::registry()
-            .with(log_template::terminal_layer(LevelFilter::TRACE))
-            .init();
         let (wgs_lon, wgs_lat) =
             super::gcj02_to_wgs84_exact(121.09626935575027, 30.608604331756705, 1e-6, 30);
-        println!("{wgs_lon},{wgs_lat}");
+        println!("wgs_lon: {wgs_lon}, wgs_lat: {wgs_lat}");
         let d = super::distance_geo(wgs_lon, wgs_lat, 121.0917077, 30.6107779);
         println!(
             "delta_lon:{:.6e}, delta_lat:{:.6e}, delta_distance: {d}",

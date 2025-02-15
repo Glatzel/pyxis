4ac7cafc52f85365ce413385587d802336ef8ab1
Glatzel <893016099@qq.com>
2025-02-15 11:12:14 +0800

    perf: raise crypto precision

--- crates/geotool-algorithm/src/crypto.rs
+++ crates/geotool-algorithm/src/crypto.rs
@@ -5,24 +5,53 @@ pub enum CryptoSpace {
     BD09,
 }
 
+const EARTH_R: f64 = 6378137.0;
 const _X_PI: f64 = PI * 3000.0 / 180.0;
-const _A: f64 = 6378245.0;
-const _EE: f64 = 0.006_693_421_622_965_943;
-fn transform_lon(lon: f64, lat: f64) -> f64 {
-    let mut ret =
-        300.0 + lon + 2.0 * lat + 0.1 * lon * lon + 0.1 * lon * lat + 0.1 * lon.abs().sqrt();
-    ret += (20.0 * (6.0 * lon * PI).sin() + 20.0 * (2.0 * lon * PI).sin()) * 2.0 / 3.0;
-    ret += (20.0 * (lon * PI).sin() + 40.0 * (lon / 3.0 * PI).sin()) * 2.0 / 3.0;
-    ret += (150.0 * (lon / 12.0 * PI).sin() + 300.0 * (lon / 30.0 * PI).sin()) * 2.0 / 3.0;
-    ret
+const EE: f64 = 0.00669342162296594323;
+fn out_of_china(lon: f64, lat: f64) -> bool {
+    if lon < 72.004 || lon > 137.8347 {
+        return true;
+    }
+    if lat < 0.8293 || lat > 55.8271 {
+        return true;
+    }
+    false
 }
-fn transform_lat(lon: f64, lat: f64) -> f64 {
-    let mut ret =
-        -100.0 + 2.0 * lon + 3.0 * lat + 0.2 * lat * lat + 0.1 * lon * lat + 0.2 * lon.abs().sqrt();
-    ret += (20.0 * (6.0 * lon * PI).sin() + 20.0 * (2.0 * lon * PI).sin()) * 2.0 / 3.0;
-    ret += (20.0 * (lat * PI).sin() + 40.0 * (lat / 3.0 * PI).sin()) * 2.0 / 3.0;
-    ret += (160.0 * (lat / 12.0 * PI).sin() + 320.0 * (lat * PI / 30.0).sin()) * 2.0 / 3.0;
-    ret
+fn transform(x: f64, y: f64) -> (f64, f64) {
+    let xy = x * y;
+    let abs_x = x.abs().sqrt();
+    let x_pi = x * PI;
+    let y_pi = y * PI;
+    let d = 20.0 * (6.0 * x_pi).sin() + 20.0 * (2.0 * x_pi).sin();
+
+    let mut lat = d;
+    let mut lon = d;
+
+    lat += 20.0 * (y_pi).sin() + 40.0 * (y_pi / 3.0).sin();
+    lon += 20.0 * (x_pi).sin() + 40.0 * (x_pi / 3.0).sin();
+
+    lat += 160.0 * (y_pi / 12.0).sin() + 320.0 * (y_pi / 30.0).sin();
+    lon += 150.0 * (x_pi / 12.0).sin() + 300.0 * (x_pi / 30.0).sin();
+
+    lat *= 2.0 / 3.0;
+    lon *= 2.0 / 3.0;
+
+    lat += -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * xy + 0.2 * abs_x;
+    lon += 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * xy + 0.1 * abs_x;
+
+    (lon, lat)
+}
+fn delta(lon: f64, lat: f64) -> (f64, f64) {
+    let ( d_lon,d_lat) = transform(lon - 105.0, lat - 35.0);
+    let mut d_lat = d_lat;
+    let mut d_lon = d_lon;
+    let rad_lat = lat / 180.0 * PI;
+    let mut magic = (rad_lat).sin();
+    magic = 1.0 - EE * magic * magic;
+    let sqrt_magic = (magic).sqrt();
+    d_lat = (d_lat * 180.0) / ((EARTH_R * (1.0 - EE)) / (magic * sqrt_magic) * PI);
+    d_lon = (d_lon * 180.0) / (EARTH_R / sqrt_magic * (rad_lat).cos() * PI);
+    (d_lon, d_lat)
 }
 /// Converts coordinates from `BD09` to `GCJ02` coordinate system.
 ///
@@ -42,17 +71,17 @@ fn transform_lat(lon: f64, lat: f64) -> f64 {
 /// use float_cmp::assert_approx_eq;
 /// let p = (121.10271691314193, 30.614836298418275);
 /// let p = geotool_algorithm::bd09_to_gcj02(p.0, p.1);
-/// assert_approx_eq!(f64, p.0, 121.09626892329175, epsilon = 1e-6);
+/// assert_approx_eq!(f64, p.0, 12.121.09626892329175, epsilon = 1e-6);
 /// assert_approx_eq!(f64, p.1, 30.608594105135296, epsilon = 1e-6);
 /// ```
-pub fn bd09_to_gcj02(lon: f64, lat: f64) -> (f64, f64) {
-    let x = lon - 0.0065;
-    let y = lat - 0.006;
+pub fn bd09_to_gcj02(bd09_lon: f64, bd09_lat: f64) -> (f64, f64) {
+    let x = bd09_lon - 0.0065;
+    let y = bd09_lat - 0.006;
     let z = (x * x + y * y).sqrt() - 0.00002 * (y * _X_PI).sin();
     let theta = y.atan2(x) - 0.000003 * (x * _X_PI).cos();
-    let dest_lon = z * theta.cos();
-    let dest_lat = z * theta.sin();
-    (dest_lon, dest_lat)
+    let gcj02_lon = z * theta.cos();
+    let gcj02_lat = z * theta.sin();
+    (gcj02_lon, gcj02_lat)
 }
 /// Converts coordinates from `GCJ02` to `WGS84` coordinate system.
 ///
@@ -70,23 +99,17 @@ pub fn bd09_to_gcj02(lon: f64, lat: f64) -> (f64, f64) {
 /// # Example
 /// ```
 /// use float_cmp::assert_approx_eq;
-/// let p = (121.09626892329175, 30.608594105135296);
+/// let p = (121.09626935575027, 30.608604331756705);
 /// let p = geotool_algorithm::gcj02_to_wgs84(p.0, p.1);
-/// assert_approx_eq!(f64, p.0, 121.09170577473259, epsilon = 1e-6);
-/// assert_approx_eq!(f64, p.1, 30.610767662599578, epsilon = 1e-6);
+/// assert_approx_eq!(f64, p.0, 121.0917077 , epsilon = 1e-5);
+/// assert_approx_eq!(f64, p.1, 30.6107779 , epsilon = 1e-5);
 /// ```
 pub fn gcj02_to_wgs84(lon: f64, lat: f64) -> (f64, f64) {
-    let dlat = transform_lat(lon - 105.0, lat - 35.0);
-    let dlon = transform_lon(lon - 105.0, lat - 35.0);
-    let radlat = lat / 180.0 * PI;
-    let magic = radlat.sin();
-    let magic = 1.0 - _EE * magic * magic;
-    let sqrtmagic = magic.sqrt();
-    let dlat = (dlat * 180.0) / ((_A * (1.0 - _EE)) / (magic * sqrtmagic) * PI);
-    let dlon = (dlon * 180.0) / (_A / sqrtmagic * radlat.cos() * PI);
-    let mglat = lat + dlat;
-    let mglon = lon + dlon;
-    (lon * 2.0 - mglon, lat * 2.0 - mglat)
+    if out_of_china(lon, lat) {
+        return (lon, lat);
+    }
+    let (d_lng, d_lat) = delta(lon, lat);
+    (lon - d_lng, lat - d_lat)
 }
 /// Converts coordinates from `BD09` to `WGS84` coordinate system.
 ///
@@ -157,23 +180,18 @@ pub fn gcj02_to_bd09(lon: f64, lat: f64) -> (f64, f64) {
 /// # Example
 /// ```
 /// use float_cmp::assert_approx_eq;
-/// let p = (121.09170577473259, 30.610767662599578);
+/// let p = (121.0917077,30.6107779 );
 /// let p = geotool_algorithm::wgs84_to_gcj02(p.0, p.1);
-/// assert_approx_eq!(f64, p.0, 121.09626892329175, epsilon = 1e-5);
-/// assert_approx_eq!(f64, p.1, 30.608594105135296, epsilon = 1e-5);
+/// println!("{},{}",p.0,p.1);
+/// assert_approx_eq!(f64, p.0, 121.09626935575027, epsilon = 1e-6);
+/// assert_approx_eq!(f64, p.1, 30.608604331756705, epsilon = 1e-6);
 /// ```
 pub fn wgs84_to_gcj02(lon: f64, lat: f64) -> (f64, f64) {
-    let dlat = transform_lat(lon - 105.0, lat - 35.0);
-    let dlon = transform_lon(lon - 105.0, lat - 35.0);
-    let radlat = lat / 180.0 * PI;
-    let magic = radlat.sin();
-    let magic = 1.0 - _EE * magic * magic;
-    let sqrt_magic = (magic).sqrt();
-    let dlat = (dlat * 180.0) / ((_A * (1.0 - _EE)) / (magic * sqrt_magic) * PI);
-    let dlon = (dlon * 180.0) / (_A / sqrt_magic * radlat.cos() * PI);
-    let dest_lat = lat + dlat;
-    let dest_lon = lon + dlon;
-    (dest_lon, dest_lat)
+    if out_of_china(lon, lat) {
+        return (lon, lat);
+    }
+    let (d_lon, d_lat) = delta(lon, lat);
+    (lon + d_lon, lat + d_lat)
 }
 /// Converts coordinates from `BD09` to `WGS84` coordinate system.
 ///

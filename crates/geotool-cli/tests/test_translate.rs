8db05be37922247c892bb3b30ba4408d38696beb
Glatzel <893016099@qq.com>
2025-02-18 17:24:59 +0800

    add translate

---
+++ crates/geotool-cli/tests/test_translate.rs
@@ -0,0 +1,12 @@
+use assert_cmd::Command;
+use predicates::prelude::*; // For more readable assertions
+#[test]
+fn test_translate() {
+    Command::cargo_bin("geotool")
+        .unwrap()
+        .args(["transform", "-x", "0", "-y", "0", "-z", "0"])
+        .args(["translate", "-x", "1", "-y", "2", "-z", "3"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("x: 1, y: 2, z: 3"));
+}

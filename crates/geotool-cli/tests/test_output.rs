3cf2800dcd392ffb946f766996b9f38bc35807f3
Glatzel <893016099@qq.com>
2025-02-18 17:05:24 +0800

    fix

---
+++ crates/geotool-cli/tests/test_output.rs
@@ -0,0 +1,85 @@
+use assert_cmd::Command;
+use predicates::prelude::*; // For more readable assertions
+#[test]
+fn test_output_plain() {
+    Command::cargo_bin("geotool")
+        .unwrap()
+        .args([
+            "transform",
+            "-x",
+            "4760096.421921",
+            "-y",
+            "3744293.729449",
+            "-z",
+            "0",
+            "-o",
+            "plain",
+        ])
+        .args(["proj", "--from", "EPSG:2230", "--to", "EPSG:26946"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(
+            r#"
+x: 4760096.421921, y: 3744293.729449, z: 0
+|-- step: 1
+|-- method: proj
+|-- parameter:
+|       {
+|         "from": "EPSG:2230",
+|         "to": "EPSG:26946"
+|       }
+▼
+x: 1450880.2910605022, y: 1141263.0111604782, z: 0"#,
+        ));
+}
+#[test]
+fn test_json() {
+    Command::cargo_bin("geotool")
+        .unwrap()
+        .args([
+            "transform",
+            "-x",
+            "4760096.421921",
+            "-y",
+            "3744293.729449",
+            "-z",
+            "0",
+            "-o",
+            "json",
+        ])
+        .args(["proj", "--from", "EPSG:2230", "--to", "EPSG:26946"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(
+            r#"{
+  "name": "",
+  "record": [
+    {
+      "idx": 0,
+      "method": "input",
+      "output_x": 4760096.421921,
+      "output_x_name": "x",
+      "output_y": 3744293.729449,
+      "output_y_name": "y",
+      "output_z": 0.0,
+      "output_z_name": "z",
+      "parameter": {}
+    },
+    {
+      "idx": 1,
+      "method": "proj",
+      "output_x": 1450880.2910605022,
+      "output_x_name": "x",
+      "output_y": 1141263.0111604782,
+      "output_y_name": "y",
+      "output_z": 0.0,
+      "output_z_name": "z",
+      "parameter": {
+        "from": "EPSG:2230",
+        "to": "EPSG:26946"
+      }
+    }
+  ]
+}"#,
+        ));
+}

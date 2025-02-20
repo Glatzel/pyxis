998f575ac288095e4d665171d07279c1dea919bf
Glatzel <893016099@qq.com>
2025-02-20 19:03:42 +0800

    fix

--- scripts/cargo-bench.ps1
+++ scripts/cargo-bench.ps1
@@ -1,11 +1,5 @@
 Set-Location $PSScriptRoot
 Set-Location ..
 
-if ($env:CI) {
-    cargo +nightly bench
-}
-else {
-    & $PSScriptRoot/set-env.ps1
-    # pixi run cargo +nightly bench
-    pixi run cargo bench
-}
+& $PSScriptRoot/set-env.ps1
+pixi run cargo bench

21a5658b9699051c1edbf2569de8bd7a749305b5
Glatzel <893016099@qq.com>
2025-02-22 14:28:07 +0800

    test: modify some test precision assert

--- crates/geotool-algorithm/src/misc.rs
+++ crates/geotool-algorithm/src/misc.rs
@@ -20,8 +20,8 @@
 /// use float_cmp::assert_approx_eq;
 /// let p = (469704.6693, 2821940.796);
 /// let p = geotool_algorithm::datum_compense(p.0, p.1, 400.0, 6_378_137.0, 500_000.0, 0.0);
-/// assert_approx_eq!(f64, p.0, 469706.56912942, epsilon = 1e-6);
-/// assert_approx_eq!(f64, p.1, 2821763.83123231, epsilon = 1e-6);
+/// assert_approx_eq!(f64, p.0, 469706.56912942487, epsilon = 1e-17);
+/// assert_approx_eq!(f64, p.1, 2821763.831232311, epsilon = 1e-17);
 /// ```
 pub fn datum_compense(xc: f64, yc: f64, hb: f64, radius: f64, x0: f64, y0: f64) -> (f64, f64) {
     let ratio = hb / radius;
@@ -60,9 +60,9 @@ pub fn datum_compense(xc: f64, yc: f64, hb: f64, radius: f64, x0: f64, y0: f64) 
 /// let ellipsoid = Ellipsoid::from_semi_major_and_invf(6378137.0, 298.257223563);
 /// let (x, y, z) = geotool_algorithm::lbh2xyz(48.8566, 2.3522, 35.0, &ellipsoid);
 /// println!("{},{},{}", x, y, z);
-/// assert_approx_eq!(f64, x, 4192979.6198897623, epsilon = 1e-6);
-/// assert_approx_eq!(f64, y, 4799159.563725418, epsilon = 1e-6);
-/// assert_approx_eq!(f64, z, 260022.66015989496, epsilon = 1e-6);
+/// assert_approx_eq!(f64, x, 4192979.6198897623, epsilon = 1e-17);
+/// assert_approx_eq!(f64, y, 4799159.563725418, epsilon = 1e-17);
+/// assert_approx_eq!(f64, z, 260022.66015989496, epsilon = 1e-17);
 /// ```
 pub fn lbh2xyz(lon: f64, lat: f64, height: f64, ellipsoid: &crate::Ellipsoid) -> (f64, f64, f64) {
     // Constants from the ellipsoid
@@ -113,12 +113,12 @@ pub fn lbh2xyz(lon: f64, lat: f64, height: f64, ellipsoid: &crate::Ellipsoid) ->
 ///     260022.66015989496,
 ///     &ellipsoid,
 ///     1e-17,
-///     20
+///     25
 /// );
 /// println!("{},{},{}", x, y, z);
-/// assert_approx_eq!(f64, x, 48.8566, epsilon = 1e-6);
-/// assert_approx_eq!(f64, y, 2.3522, epsilon = 1e-6);
-/// assert_approx_eq!(f64, z, 35.0, epsilon = 1e-6);
+/// assert_approx_eq!(f64, x, 48.8566, epsilon = 1e-17);
+/// assert_approx_eq!(f64, y, 2.3522, epsilon = 1e-7);
+/// assert_approx_eq!(f64, z, 35.0, epsilon = 1e-17);
 /// ```
 pub fn xyz2lbh(
     x: f64,

92cd5fc682239742807390bac0f1b36080d79eab
Glatzel <893016099@qq.com>
2025-02-27 17:22:07 +0800

    fix

--- scripts/set-env.ps1
+++ scripts/set-env.ps1
@@ -2,5 +2,5 @@ $current_dir = Resolve-Path $PWD
 Set-Location $PSScriptRoot
 Set-Location ..
 
-$env:PKG_CONFIG_PATH=Resolve-Path vcpkg_deps/vcpkg_installed/x64-windows-static/lib/pkgconfig
+$env:PKG_CONFIG_PATH=Resolve-Path vcpkg_deps/vcpkg_installed/static/x64-windows/lib/pkgconfig
 Set-Location $current_dir
--- scripts/vcpkg-install.ps1
+++ scripts/vcpkg-install.ps1
@@ -4,10 +4,10 @@ Set-Location ..
 Set-Location vcpkg_deps
 $triplet=Resolve-Path ./triplet
 
-Write-Output "::group::static"
-&./vcpkg/vcpkg.exe install --overlay-triplets=$triplet --triplet x64-windows
+Write-Output "::group::dynamic"
+&./vcpkg/vcpkg.exe install --overlay-triplets=$triplet --triplet x64-windows --x-install-root ./vcpkg_installed/static
 Write-Output "::endgroup::"
 
-Write-Output "::group::dynamic"
-&./vcpkg/vcpkg.exe install --overlay-triplets=$triplet --triplet x64-windows-static
+Write-Output "::group::static"
+&./vcpkg/vcpkg.exe install --overlay-triplets=$triplet --triplet x64-windows-static --x-install-root ./vcpkg_installed/dynamic
 Write-Output "::endgroup::"

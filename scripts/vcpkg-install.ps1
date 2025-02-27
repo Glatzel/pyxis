4f06f1868c1033d76cf80f34a2cf34ceb58539f0
Glatzel <893016099@qq.com>
2025-02-27 16:08:28 +0800

    fix

--- scripts/vcpkg-install.ps1
+++ scripts/vcpkg-install.ps1
@@ -3,5 +3,11 @@ Set-Location ..
 
 Set-Location vcpkg_deps
 $triplet=Resolve-Path ./triplet
+
+Write-Output "::group::static"
 &./vcpkg/vcpkg.exe install --overlay-triplets=$triplet --triplet x64-windows
+Write-Output "::endgroup::"
+
+Write-Output "::group::dynamic"
 &./vcpkg/vcpkg.exe install --overlay-triplets=$triplet --triplet x64-windows-static
+Write-Output "::endgroup::"

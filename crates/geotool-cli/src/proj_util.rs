860707aeb6115661b8e5d2826133fefa58eda585
Glatzel <893016099@qq.com>
2025-02-27 22:06:43 +0800

    fix

--- crates/geotool-cli/src/proj_util.rs
+++ crates/geotool-cli/src/proj_util.rs
@@ -1,3 +1,4 @@
+#[cfg(target_os = "windows")]
 use std::io::Write;
 use std::path::PathBuf;
 #[cfg(target_os = "windows")]
@@ -6,11 +7,14 @@ const PROJ_DB: &[u8] = include_bytes!("proj.db");
 pub fn init_proj_builder() -> proj::ProjBuilder {
     let mut builder = proj::ProjBuilder::new();
     if let Ok(proj_data) = std::env::var("PROJ_DATA") {
+        tracing::info!("PROJ_DATA environment variable is found: {proj_data}");
         builder.set_search_paths(PathBuf::from(proj_data)).unwrap();
     } else {
+        tracing::info!("PROJ_DATA environment variable is not found");
         let exe_root = std::env::current_exe().unwrap();
         exe_root.parent().unwrap();
         if !exe_root.clone().join("proj.db").exists() {
+            tracing::info!("proj.db is not found. Use bundled file.");
             #[cfg(target_os = "windows")]
             {
                 let mut db_file = std::fs::File::create("proj.db").unwrap();

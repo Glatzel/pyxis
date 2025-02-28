ae0136d0521996ecfd190ec6e6472ba4b7c3b061
Glatzel <893016099@qq.com>
2025-02-28 15:14:11 +0800

    feat: new build type

--- scripts/build-cli.ps1
+++ scripts/build-cli.ps1
@@ -6,18 +6,41 @@ param (
 Set-Location $PSScriptRoot
 Set-Location ..
 & $PSScriptRoot/set-env.ps1
-Remove-Item dist/geotool -Recurse -ErrorAction SilentlyContinue
-
+Remove-Item dist/cli -Recurse -ErrorAction SilentlyContinue
+Remove-Item ./dist/geotool*.7z -Recurse -Force -ErrorAction SilentlyContinue
 Write-Host "Build in $config mode."
-if ($config -ne "debug") {
-    pixi run cargo build --profile $config --bin geotool
+function build-static {
+    Write-Output "::group::Build static"
+    if ($config -ne "debug") {
+        pixi run cargo build --profile $config --bin geotool --features static
+    }
+    else {
+        pixi run cargo build --bin geotool --features static
+    }
+    New-Item ./dist/cli/static -ItemType Directory -ErrorAction SilentlyContinue
+    Copy-Item "target/$config/geotool.exe" ./dist/cli/static/geotool.exe
+    7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on -sccUTF-8 -bb0 -bse0 -bsp2 `
+        "-wdist/cli/static" -mtc=on -mta=on "dist/geotool-windows-x64-self-contained.7z" "./dist/cli/static/geotool.exe"
+    Write-Output "::endgroup::"
 }
-else {
-    pixi run cargo build --bin geotool
+function build-dynamic {
+    Write-Output "::group::Build dynamic"
+    if ($config -ne "debug") {
+        pixi run cargo build --profile $config --bin geotool
+    }
+    else {
+        pixi run cargo build --bin geotool
+    }
+    New-Item ./dist/cli/dynamic -ItemType Directory -ErrorAction SilentlyContinue
+    Copy-Item "target/$config/geotool.exe" ./dist/cli/dynamic/geotool.exe
+    7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on -sccUTF-8 -bb0 -bse0 -bsp2 `
+        "-wdist/cli/dynamic" -mtc=on -mta=on "dist/geotool-windows-x64.7z" "./dist/cli/dynamic/geotool.exe"
+
+    Copy-Item ./vcpkg_deps/vcpkg_installed/dynamic/x64-windows/bin/*.dll ./dist/cli/dynamic
+    Copy-Item ./vcpkg_deps/vcpkg_installed/dynamic/x64-windows/share/proj/proj.db ./dist/cli/dynamic
+    7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on -sccUTF-8 -bb0 -bse0 -bsp2 `
+        "-wdist/cli/dynamic" -mtc=on -mta=on "dist/geotool-windows-x64-proj.7z" "./dist/cli/dynamic/*"
+    Write-Output "::endgroup::"
 }
-Set-Location $PSScriptRoot
-Set-Location ..
-Remove-Item ./dist/geotool*.zip -Recurse -Force -ErrorAction SilentlyContinue
-New-Item ./dist -ItemType Directory -ErrorAction SilentlyContinue
-Copy-Item "target/$config/geotool.exe" ./dist/geotool.exe
-7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on -sccUTF-8 -bb0 -bse0 -bsp2 "-wdist" -mtc=on -mta=on "dist\geotool.7z" "dist\geotool.exe"
+build-static
+build-dynamic

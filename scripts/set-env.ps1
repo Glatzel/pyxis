71f6625af2d04d86fcdac662967a835f9ad3faab
Glatzel <893016099@qq.com>
2025-02-28 15:56:03 +0800

    fix

--- scripts/set-env.ps1
+++ scripts/set-env.ps1
@@ -1,7 +1,18 @@
-$current_dir = Resolve-Path $PWD
-Set-Location $PSScriptRoot
-Set-Location ..
+$os = (Get-OperatingSystem).OS
 
-$env:PKG_CONFIG_PATH=Resolve-Path vcpkg_deps/vcpkg_installed/static/x64-windows-static/lib/pkgconfig
-Set-Location $current_dir
-Copy-Item ./vcpkg_deps/vcpkg_installed/static/x64-windows-static/share/proj/proj.db ./crates/geotool-cli/src
+switch -Wildcard ($os) {
+    "*Windows*" {
+        $current_dir = Resolve-Path $PWD
+        Set-Location $PSScriptRoot
+        Set-Location ..
+
+        $env:PKG_CONFIG_PATH = Resolve-Path vcpkg_deps/vcpkg_installed/static/x64-windows-static/lib/pkgconfig
+        Set-Location $current_dir
+        Copy-Item ./vcpkg_deps/vcpkg_installed/static/x64-windows-static/share/proj/proj.db ./crates/geotool-cli/src
+
+        break
+    }
+    default {
+    
+    }
+}

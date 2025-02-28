4bf4591a4a82d5dce5de44a284d072a0a937fa23
Glatzel <893016099@qq.com>
2025-02-28 16:24:48 +0800

    fix

--- scripts/build-cli.ps1
+++ scripts/build-cli.ps1
@@ -9,35 +9,40 @@ Set-Location ..
 Remove-Item dist/cli -Recurse -ErrorAction SilentlyContinue
 Remove-Item ./dist/geotool*.7z -Recurse -Force -ErrorAction SilentlyContinue
 Write-Host "Build in $config mode."
-
 Write-Output "::group::Build static"
-if ($config -ne "debug") {
-    pixi run cargo build --profile $config --bin geotool --features static
-}
-else {
-    pixi run cargo build --bin geotool --features static
-}
-New-Item ./dist/cli/static -ItemType Directory -ErrorAction SilentlyContinue
-Copy-Item "target/$config/geotool.exe" ./dist/cli/static/geotool.exe
-7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on -sccUTF-8 -bb0 -bse0 -bsp2 `
-    "-wdist/cli/static" -mtc=on -mta=on "dist/geotool-windows-x64-self-contained.7z" "./dist/cli/static/geotool.exe"
-Write-Output "::endgroup::"
+switch -Wildcard ($Env:OS) {
+    "*Windows*" {
+        if ($config -ne "debug") {
+            pixi run cargo build --profile $config --bin geotool --features static
+        }
+        else {
+            pixi run cargo build --bin geotool --features static
+        }
+        New-Item ./dist/cli/static -ItemType Directory -ErrorAction SilentlyContinue
+        Copy-Item "target/$config/geotool.exe" ./dist/cli/static/geotool.exe
+        7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on -sccUTF-8 -bb0 -bse0 -bsp2 `
+            "-wdist/cli/static" -mtc=on -mta=on "dist/geotool-windows-x64-self-contained.7z" "./dist/cli/static/geotool.exe"
+        Write-Output "::endgroup::"
 
-Write-Output "::group::Build dynamic"
-if ($config -ne "debug") {
-    pixi run cargo build --profile $config --bin geotool
-}
-else {
-    pixi run cargo build --bin geotool
-}
-New-Item ./dist/cli/dynamic -ItemType Directory -ErrorAction SilentlyContinue
-Copy-Item "target/$config/geotool.exe" ./dist/cli/dynamic/geotool.exe
-7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on -sccUTF-8 -bb0 -bse0 -bsp2 `
-    "-wdist/cli/dynamic" -mtc=on -mta=on "dist/geotool-windows-x64.7z" "./dist/cli/dynamic/geotool.exe"
+        Write-Output "::group::Build dynamic"
+        if ($config -ne "debug") {
+            pixi run cargo build --profile $config --bin geotool
+        }
+        else {
+            pixi run cargo build --bin geotool
+        }
+        New-Item ./dist/cli/dynamic -ItemType Directory -ErrorAction SilentlyContinue
+        Copy-Item "target/$config/geotool.exe" ./dist/cli/dynamic/geotool.exe
+        7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on -sccUTF-8 -bb0 -bse0 -bsp2 `
+            "-wdist/cli/dynamic" -mtc=on -mta=on "dist/geotool-windows-x64.7z" "./dist/cli/dynamic/geotool.exe"
 
-Copy-Item ./vcpkg_deps/vcpkg_installed/dynamic/x64-windows/bin/*.dll ./dist/cli/dynamic
-Copy-Item ./vcpkg_deps/vcpkg_installed/dynamic/x64-windows/share/proj/proj.db ./dist/cli/dynamic
-7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on -sccUTF-8 -bb0 -bse0 -bsp2 `
-    "-wdist/cli/dynamic" -mtc=on -mta=on "dist/geotool-windows-x64-proj.7z" "./dist/cli/dynamic/*"
-Write-Output "::endgroup::"
-
+        Copy-Item ./vcpkg_deps/vcpkg_installed/dynamic/x64-windows/bin/*.dll ./dist/cli/dynamic
+        Copy-Item ./vcpkg_deps/vcpkg_installed/dynamic/x64-windows/share/proj/proj.db ./dist/cli/dynamic
+        7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on -sccUTF-8 -bb0 -bse0 -bsp2 `
+            "-wdist/cli/dynamic" -mtc=on -mta=on "dist/geotool-windows-x64-proj.7z" "./dist/cli/dynamic/*"
+        Write-Output "::endgroup::"
+    }
+    "*Linux*" {
+        
+    }
+}
--- scripts/cargo-test.ps1
+++ scripts/cargo-test.ps1
@@ -2,38 +2,74 @@ Set-Location $PSScriptRoot
 Set-Location ..
 & $PSScriptRoot/set-env.ps1
 New-Item ./target/llvm-cov-target/debug -ItemType Directory -ErrorAction SilentlyContinue
-if ($env:OS -like "*Windows*")
-{
-    Copy-Item "./vcpkg_deps/vcpkg_installed/static/x64-windows-static/share/proj/proj.db" ./target/llvm-cov-target/debug
+switch -Wildcard ($Env:OS) {
+    "*Windows*" {
+        Copy-Item "./vcpkg_deps/vcpkg_installed/static/x64-windows-static/share/proj/proj.db" ./target/llvm-cov-target/debug
+        Write-Output "::group::nextest"
+        pixi run cargo +nightly llvm-cov --no-report --all-features --workspace --branch nextest
+        $code = $LASTEXITCODE
+        Write-Output "::endgroup::"
+
+        Write-Output "::group::doctest"
+        pixi run cargo +nightly llvm-cov --no-report --all-features --workspace --branch --doc
+        $code = $code + $LASTEXITCODE
+        Write-Output "::endgroup::"
+
+        Write-Output "::group::report"
+        pixi run cargo +nightly llvm-cov report
+        Write-Output "::endgroup::"
+
+        Write-Output "::group::lcov"
+        if ( $env:CI ) {
+            pixi run cargo +nightly llvm-cov report --lcov --output-path lcov.info
+        }
+        Write-Output "::endgroup::"
+
+        Write-Output "::group::result"
+        $code = $code + $LASTEXITCODE
+        if ($code -ne 0) {
+            Write-Output "Test failed."
+        }
+        else {
+            Write-Output "Test successed."
+        }
+        Write-Output "::endgroup::"
+        exit $code
+    }
+    "*Linux*" {
+        Write-Output "::group::nextest"
+        cargo +nightly llvm-cov --no-report --all-features --workspace --branch nextest
+        $code = $LASTEXITCODE
+        Write-Output "::endgroup::"
+
+        Write-Output "::group::doctest"
+        cargo +nightly llvm-cov --no-report --all-features --workspace --branch --doc
+        $code = $code + $LASTEXITCODE
+        Write-Output "::endgroup::"
+
+        Write-Output "::group::report"
+        cargo +nightly llvm-cov report
+        Write-Output "::endgroup::"
+
+        Write-Output "::group::lcov"
+        if ( $env:CI ) {
+            cargo +nightly llvm-cov report --lcov --output-path lcov.info
+        }
+        Write-Output "::endgroup::"
+
+        Write-Output "::group::result"
+        $code = $code + $LASTEXITCODE
+        if ($code -ne 0) {
+            Write-Output "Test failed."
+        }
+        else {
+            Write-Output "Test successed."
+        }
+        Write-Output "::endgroup::"
+        exit $code
+    }
+    default {
+        Write-Error "Unsupported system $Env:OS"
+        exit 1
+    }
 }
-
-Write-Output "::group::nextest"
-pixi run cargo +nightly llvm-cov --no-report --all-features --workspace --branch nextest
-$code = $LASTEXITCODE
-Write-Output "::endgroup::"
-
-Write-Output "::group::doctest"
-pixi run cargo +nightly llvm-cov --no-report --all-features --workspace --branch --doc
-$code = $code + $LASTEXITCODE
-Write-Output "::endgroup::"
-
-Write-Output "::group::report"
-pixi run cargo +nightly llvm-cov report
-Write-Output "::endgroup::"
-
-Write-Output "::group::lcov"
-if ( $env:CI ) {
-    pixi run cargo +nightly llvm-cov report --lcov --output-path lcov.info
-}
-Write-Output "::endgroup::"
-
-Write-Output "::group::result"
-$code = $code + $LASTEXITCODE
-if ($code -ne 0) {
-    Write-Output "Test failed."
-}
-else {
-    Write-Output "Test successed."
-}
-Write-Output "::endgroup::"
-exit $code

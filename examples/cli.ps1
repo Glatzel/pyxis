98b981af0e795e6b966ba12d5a00c4b7534f7ba3
Glatzel <893016099@qq.com>
2025-02-28 17:24:05 +0800

    fix

--- examples/cli.ps1
+++ examples/cli.ps1
@@ -1,9 +1,10 @@
 Set-Location $PSScriptRoot
 Set-Location ..
 Set-Location dist/cli/static
-
+if ($IsWindows) { $geotool = "geotool.exe" }
+elseif($IsLinux){ $geotool = "geotool"}
 # Zhengyong expressway Dehua east interchange
-& ./geotool.exe -v `
+& "./$geotool" -v `
     transform -n "Zhengyong expressway Dehua east interchange" -x 469704.6693 -y 2821940.796 -z 0 -o plain `
     datum-compense --hb 400 -r 6378137 --x0 500000 --y0 0 `
     proj --from "+proj=tmerc +lat_0=0 +lon_0=118.5 +k=1 +x_0=500000 +y_0=0 +ellps=GRS80 +units=m +no_defs +type=crs" `
@@ -11,21 +12,21 @@ Set-Location dist/cli/static
 Write-Output ""
 
 # Jiaxing bump station
-& ./geotool.exe `
+& "./$geotool" `
     transform -n "Jiaxing bump station" -x 121.091701 -y 30.610765 -z 0 -o json `
     crypto --from "wgs84" --to "gcj02"
 Write-Output ""
 
 # Tian'anmen national flag
-& ./geotool.exe `
-transform -n "Tian'anmen national flag" -x 116.3913318 -y 39.9055625 -z 0 -o plain `
-crypto --from "wgs84" --to "gcj02"
+& "./$geotool" `
+    transform -n "Tian'anmen national flag" -x 116.3913318 -y 39.9055625 -z 0 -o plain `
+    crypto --from "wgs84" --to "gcj02"
 Write-Output ""
 
 # proj crate
-& ./geotool.exe `
-transform -n "proj crate" -x 4760096.421921 -y 3744293.729449 -z 0 -o plain `
-proj --from "EPSG:2230" --to "EPSG:26946"
+& "./$geotool" `
+    transform -n "proj crate" -x 4760096.421921 -y 3744293.729449 -z 0 -o plain `
+    proj --from "EPSG:2230" --to "EPSG:26946"
 
 Set-Location $PSScriptRoot
 Set-Location ..

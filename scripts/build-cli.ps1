5e6a418e0dddc4c919ba79aad3c7ac7068ce0c4c
Glatzel <893016099@qq.com>
2025-02-28 19:17:08 +0800

    fix

--- scripts/build-cli.ps1
+++ scripts/build-cli.ps1
@@ -88,7 +88,7 @@ elseif ($IsLinux) {
     Write-Output "::endgroup::"
 
     Write-Output "::group::Pack geotool-linux-x64-proj.7z"
-    Copy-Item ./vcpkg_deps/vcpkg_installed/dynamic/x64-linux/bin/*.so ./dist/cli/dynamic
+    Copy-Item ./vcpkg_deps/vcpkg_installed/dynamic/x64-linux/lib/*.so ./dist/cli/dynamic
     Copy-Item ./vcpkg_deps/vcpkg_installed/dynamic/x64-linux/share/proj/proj.db ./dist/cli/dynamic
     New-Item ./dist/cli/dynamic -ItemType Directory -ErrorAction SilentlyContinue
     7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on -sccUTF-8 -bb0 -bse0 -bsp2 `

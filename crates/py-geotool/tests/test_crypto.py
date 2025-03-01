f4b25d64fe1c69fb79e0ec80da772101061bff27
Glatzel <893016099@qq.com>
2025-03-01 11:49:51 +0800

    fix

--- crates/py-geotool/tests/test_crypto.py
+++ crates/py-geotool/tests/test_crypto.py
@@ -1,4 +1,5 @@
 import geotool
+import numpy as np
 import pytest
 
 bd09 = (121.10271732371203, 30.61484572185035)
@@ -21,7 +22,31 @@ wgs84 = (121.0917077, 30.6107779)
 )
 @pytest.mark.parametrize("exact", [True, False])
 def test_convert(src, dst, exact, input, expected):
-    lon, lat = geotool.crypto(input[0], input[1], src, dst,exact)
+    lon, lat = geotool.crypto(input[0], input[1], src, dst, exact)
 
     assert lon == pytest.approx(expected[0])
     assert lat == pytest.approx(expected[1])
+
+
+bd09_array = (np.array([121.10271732371203, 121.10271732371203]), np.array([30.61484572185035, 30.61484572185035]))
+gcj02_array = (np.array([121.10271732371203, 121.10271732371203]), np.array([30.61484572185035, 30.61484572185035]))
+wgs84_array = (np.array([121.10271732371203, 121.10271732371203]), np.array([30.61484572185035, 30.61484572185035]))
+
+
+@pytest.mark.parametrize(
+    ("src", "dst", "input", "expected"),
+    [
+        ("WGS84", "BD09", wgs84_array, bd09),
+        ("WGS84", "GCJ02", wgs84_array, gcj02),
+        ("GCJ02", "BD09", gcj02_array, bd09),
+        ("GCJ02", "WGS84", gcj02_array, wgs84),
+        ("BD09", "GCJ02", bd09_array, gcj02),
+        ("BD09", "WGS84", bd09_array, wgs84),
+    ],
+)
+@pytest.mark.parametrize("exact", [True, False])
+def test_convert_array(src, dst, exact, input, expected):
+    lon, lat = geotool.crypto(input[0], input[1], src, dst, exact)
+
+    assert lon[0] == pytest.approx(expected[0])
+    assert lat[0] == pytest.approx(expected[1])

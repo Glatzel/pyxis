94b83d75282852f4caefbc91ce57e327332ebbd7
Glatzel <893016099@qq.com>
2025-03-01 14:39:37 +0800

    fix

--- crates/py-geotool/doc/source/reference/crypto.rst
+++ crates/py-geotool/doc/source/reference/crypto.rst
@@ -5,3 +5,9 @@ Crypto
    :toctree: ./api/crypto
 
    crypto
+   bd09_to_gcj02
+   bd09_to_wgs84
+   gcj02_to_bd09
+   gcj02_to_wgs84
+   wgs84_to_bd09
+   wgs84_to_gcj02
--- scripts/py-sphinx.ps1
+++ scripts/py-sphinx.ps1
@@ -6,8 +6,10 @@ Set-Location -Path crates/py-geotool
 Remove-Item -Path "./doc/source/reference/api" -Recurse -ErrorAction SilentlyContinue
 Remove-Item -Path "./doc/build" -Recurse -ErrorAction SilentlyContinue
 pixi run sphinx-build -M html ./doc/source ./doc/build --fail-on-warning
-
+$code = $LASTEXITCODE
 Remove-Item $PSScriptRoot/../dist/python-doc.zip -Force -ErrorAction SilentlyContinue
 New-Item $PSScriptRoot/../dist -ItemType Directory -ErrorAction SilentlyContinue
 Compress-Archive ./doc/build "$PSScriptRoot/../dist/python-doc.zip"
 7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on -sccUTF-8 -bb0 -bse0 -bsp2 "-wdoc/build" -mtc=on -mta=on "$PSScriptRoot/../dist/python-doc.7z" "./doc/build/*"
+
+exit $code

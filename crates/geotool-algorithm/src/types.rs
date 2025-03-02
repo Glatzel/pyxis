b0a5e76461fe27501b4abb7b2cc3d912778da2b9
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-03-02 18:08:39 +0800

    perf: improve const ellipsoid performance (#129)

--- crates/geotool-algorithm/src/types.rs
+++ crates/geotool-algorithm/src/types.rs
@@ -27,23 +27,61 @@ pub trait ConstEllipsoid<T>
 where
     T: GeoFloat,
 {
-    fn krasovsky1940() -> LazyLock<Ellipsoid<T>>;
-    fn wgs84() -> LazyLock<Ellipsoid<T>>;
+    fn cgcs2000() -> &'static Ellipsoid<T>;
+    fn iag1975() -> &'static Ellipsoid<T>;
+    fn grs1980() -> &'static Ellipsoid<T>;
+    fn krasovsky1940() -> &'static Ellipsoid<T>;
+    fn wgs84() -> &'static Ellipsoid<T>;
 }
-
+//f32
+static GRS1980_F32: LazyLock<Ellipsoid<f32>> =
+    LazyLock::new(|| Ellipsoid::from_semi_major_and_invf(6378137.0, 298.257_23));
+static IAG1975_F32: LazyLock<Ellipsoid<f32>> =
+    LazyLock::new(|| Ellipsoid::from_semi_major_and_invf(6378140.0, 298.257));
+static KRASOVSKY1940_F32: LazyLock<Ellipsoid<f32>> =
+    LazyLock::new(|| Ellipsoid::from_semi_major_and_invf(6378245.0, 298.3));
+static WGS84_F32: LazyLock<Ellipsoid<f32>> =
+    LazyLock::new(|| Ellipsoid::from_semi_major_and_invf(6378137.0, 298.257_23));
 impl ConstEllipsoid<f32> for f32 {
-    fn krasovsky1940() -> LazyLock<Ellipsoid<f32>> {
-        LazyLock::new(|| Ellipsoid::from_semi_major_and_invf(6378245.0, 298.3))
+    fn cgcs2000() -> &'static Ellipsoid<f32> {
+        &GRS1980_F32
     }
-    fn wgs84() -> LazyLock<Ellipsoid<f32>> {
-        LazyLock::new(|| Ellipsoid::from_semi_major_and_invf(6378137.0, 298.257_23))
+    fn iag1975() -> &'static Ellipsoid<f32> {
+        &IAG1975_F32
+    }
+    fn grs1980() -> &'static Ellipsoid<f32> {
+        &GRS1980_F32
+    }
+    fn krasovsky1940() -> &'static Ellipsoid<f32> {
+        &KRASOVSKY1940_F32
+    }
+    fn wgs84() -> &'static Ellipsoid<f32> {
+        &WGS84_F32
     }
 }
+//f64
+static GRS1980_F64: LazyLock<Ellipsoid<f64>> =
+    LazyLock::new(|| Ellipsoid::from_semi_major_and_invf(6378137.0, 298.257222101));
+static IAG1975_F64: LazyLock<Ellipsoid<f64>> =
+    LazyLock::new(|| Ellipsoid::from_semi_major_and_invf(6378140.0, 298.257));
+static KRASOVSKY1940_F64: LazyLock<Ellipsoid<f64>> =
+    LazyLock::new(|| Ellipsoid::from_semi_major_and_invf(6378245.0, 298.3));
+static WGS84_F64: LazyLock<Ellipsoid<f64>> =
+    LazyLock::new(|| Ellipsoid::from_semi_major_and_invf(6378137.0, 298.257223563));
 impl ConstEllipsoid<f64> for f64 {
-    fn krasovsky1940() -> LazyLock<Ellipsoid<f64>> {
-        LazyLock::new(|| Ellipsoid::from_semi_major_and_invf(6378245.0, 298.3))
+    fn cgcs2000() -> &'static Ellipsoid<f64> {
+        &GRS1980_F64
     }
-    fn wgs84() -> LazyLock<Ellipsoid<f64>> {
-        LazyLock::new(|| Ellipsoid::from_semi_major_and_invf(6378137.0, 298.257223563))
+    fn iag1975() -> &'static Ellipsoid<f64> {
+        &IAG1975_F64
+    }
+    fn grs1980() -> &'static Ellipsoid<f64> {
+        &GRS1980_F64
+    }
+    fn krasovsky1940() -> &'static Ellipsoid<f64> {
+        &KRASOVSKY1940_F64
+    }
+    fn wgs84() -> &'static Ellipsoid<f64> {
+        &WGS84_F64
     }
 }

d839599c1d21d1f2291b2a6954da15b18ec3cf97
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-03-03 21:57:02 +0800

    build: simplify scripts (#141)

--- scripts/cargo-test.ps1
+++ scripts/cargo-test.ps1
@@ -2,72 +2,34 @@ Set-Location $PSScriptRoot
 Set-Location ..
 New-Item ./target/llvm-cov-target/debug -ItemType Directory -ErrorAction SilentlyContinue
 & $PSScriptRoot/set-env.ps1
-if ($IsWindows) {
-    Copy-Item "./vcpkg_deps/vcpkg_installed/static/x64-windows-static/share/proj/proj.db" ./target/llvm-cov-target/debug
-    Write-Output "::group::nextest"
-    pixi run cargo +nightly llvm-cov --no-report --all-features --workspace --branch nextest
-    $code = $LASTEXITCODE
-    Write-Output "::endgroup::"
 
-    Write-Output "::group::doctest"
-    pixi run cargo +nightly llvm-cov --no-report --all-features --workspace --branch --doc
-    $code = $code + $LASTEXITCODE
-    Write-Output "::endgroup::"
+Write-Output "::group::nextest"
+pixi run cargo +nightly llvm-cov --no-report --all-features --workspace --branch nextest
+$code = $LASTEXITCODE
+Write-Output "::endgroup::"
 
-    Write-Output "::group::report"
-    pixi run cargo +nightly llvm-cov report
-    Write-Output "::endgroup::"
+Write-Output "::group::doctest"
+pixi run cargo +nightly llvm-cov --no-report --all-features --workspace --branch --doc
+$code = $code + $LASTEXITCODE
+Write-Output "::endgroup::"
 
-    Write-Output "::group::lcov"
-    if ( $env:CI ) {
-        pixi run cargo +nightly llvm-cov report --lcov --output-path lcov.info
-    }
-    Write-Output "::endgroup::"
+Write-Output "::group::report"
+pixi run cargo +nightly llvm-cov report
+Write-Output "::endgroup::"
 
-    Write-Output "::group::result"
-    $code = $code + $LASTEXITCODE
-    if ($code -ne 0) {
-        Write-Output "Test failed."
-    }
-    else {
-        Write-Output "Test successed."
-    }
-    Write-Output "::endgroup::"
-    exit $code
+Write-Output "::group::lcov"
+if ( $env:CI ) {
+    pixi run cargo +nightly llvm-cov report --lcov --output-path lcov.info
 }
-elseif ($IsLinux) {
-    Write-Output "::group::nextest"
-    cargo +nightly llvm-cov --no-report --all-features --workspace --branch nextest
-    $code = $LASTEXITCODE
-    Write-Output "::endgroup::"
+Write-Output "::endgroup::"
 
-    Write-Output "::group::doctest"
-    cargo +nightly llvm-cov --no-report --all-features --workspace --branch --doc
-    $code = $code + $LASTEXITCODE
-    Write-Output "::endgroup::"
-
-    Write-Output "::group::report"
-    cargo +nightly llvm-cov report
-    Write-Output "::endgroup::"
-
-    Write-Output "::group::lcov"
-    if ( $env:CI ) {
-        cargo +nightly llvm-cov report --lcov --output-path lcov.info
-    }
-    Write-Output "::endgroup::"
-
-    Write-Output "::group::result"
-    $code = $code + $LASTEXITCODE
-    if ($code -ne 0) {
-        Write-Output "Test failed."
-    }
-    else {
-        Write-Output "Test successed."
-    }
-    Write-Output "::endgroup::"
-    exit $code
+Write-Output "::group::result"
+$code = $code + $LASTEXITCODE
+if ($code -ne 0) {
+    Write-Output "Test failed."
 }
 else {
-    Write-Error "Unsupported system $os"
-    exit 1
+    Write-Output "Test successed."
 }
+Write-Output "::endgroup::"
+exit $code
--- scripts/set-env.ps1
+++ scripts/set-env.ps1
@@ -5,6 +5,7 @@ param (
 $current_dir = Resolve-Path $PWD
 Set-Location $PSScriptRoot
 Set-Location ..
+pixi install
 if ($IsWindows) {
     if ($link -eq 'static') {
         $env:PKG_CONFIG_PATH = Resolve-Path vcpkg_deps/vcpkg_installed/static/x64-windows-static/lib/pkgconfig
@@ -12,7 +13,7 @@ if ($IsWindows) {
     else {
         $env:PKG_CONFIG_PATH = Resolve-Path vcpkg_deps/vcpkg_installed/dynamic/x64-windows/lib/pkgconfig
     }
-    Copy-Item ./vcpkg_deps/vcpkg_installed/static/x64-windows-static/share/proj/proj.db ./crates/geotool-cli/src
+    Copy-Item .pixi/envs/default/Library/share/proj/proj.db ./crates/geotool-cli/src/proj.db
 }
 if ($IsLinux) {
     pixi install

2830cdd0cd60acb6f0170b8b6802cdb73b765788
Glatzel <893016099@qq.com>
2025-03-05 12:59:43 +0800

    fix

--- scripts/build-cuda.ps1
+++ scripts/build-cuda.ps1
@@ -6,7 +6,6 @@ param (
 Set-Location $PSScriptRoot
 Set-Location ..
 if($IsWindows){
-   
     $env:PATH="$env:PATH;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.8\bin"
 }
 if ($config -ne "debug") {

0b52895c6b168abf99a0fbfff5ec8dd1841faaaf
Glatzel <893016099@qq.com>
2025-03-05 13:05:14 +0800

    fix

--- scripts/build-cuda.ps1
+++ scripts/build-cuda.ps1
@@ -5,9 +5,12 @@ param (
 
 Set-Location $PSScriptRoot
 Set-Location ..
+
+# add nvcc to path
 if($IsWindows){
     $env:PATH="$env:PATH;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.8\bin"
 }
+
 if ($config -ne "debug") {
     pixi run cargo build --profile $config -p pyxis-cuda
 }

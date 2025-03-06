12bba8094c071380afccb620d5724da2428ce962
Glatzel <893016099@qq.com>
2025-03-06 10:45:44 +0800

    fix

--- crates/pyxis-cuda/src/crypto_cuda.rs
+++ crates/pyxis-cuda/src/crypto_cuda.rs
@@ -201,4 +201,38 @@ mod test {
         assert_approx_eq!(f64, lon[0], pyxis::crypto::GCJ02_LON, epsilon = 1e-17);
         assert_approx_eq!(f64, lat[0], pyxis::crypto::GCJ02_LAT, epsilon = 1e-17);
     }
+    #[test]
+    fn test_wgs84_to_bd09_cuda() {
+        let mut lon: Vec<f64> = vec![pyxis::crypto::WGS84_LON, pyxis::crypto::WGS84_LON];
+        let mut lat: Vec<f64> = vec![pyxis::crypto::WGS84_LAT, pyxis::crypto::WGS84_LAT];
+        let ctx = crate::PyxisCudaContext::new();
+        let mut dlon = ctx.from_slice(&lon);
+        let mut dlat = ctx.from_slice(&lat);
+        ctx.wgs84_to_bd09_cuda(&mut dlon, &mut dlat);
+        dlon.copy_to(&mut lon).unwrap();
+        dlat.copy_to(&mut lat).unwrap();
+
+        println!("lon:{:?}", lon);
+        println!("lat:{:?}", lat);
+        assert_approx_eq!(f64, lon[0], pyxis::crypto::BD09_LON, epsilon = 1e-14);
+        assert_approx_eq!(f64, lat[0], pyxis::crypto::BD09_LAT, epsilon = 1e-14);
+    }
+    #[test]
+    fn test_bd09_to_wgs84_cuda() {
+        let mut lon: Vec<f64> = vec![pyxis::crypto::BD09_LON, pyxis::crypto::BD09_LON];
+        let mut lat: Vec<f64> = vec![pyxis::crypto::BD09_LAT, pyxis::crypto::BD09_LAT];
+        let expect_wgs = pyxis::crypto::bd09_to_wgs84(lon[0], lat[0]);
+        let ctx = crate::PyxisCudaContext::new();
+        let mut dlon = ctx.from_slice(&lon);
+        let mut dlat = ctx.from_slice(&lat);
+        ctx.bd09_to_wgs84_cuda(&mut dlon, &mut dlat);
+        dlon.copy_to(&mut lon).unwrap();
+        dlat.copy_to(&mut lat).unwrap();
+
+        println!("yc:{:?}", lon);
+        println!("yc:{:?}", lat);
+
+        assert_approx_eq!(f64, lon[0], expect_wgs.0, epsilon = 1e-14);
+        assert_approx_eq!(f64, lat[0], expect_wgs.1, epsilon = 1e-14);
+    }
 }

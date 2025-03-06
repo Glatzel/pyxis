3ebbbbf0d317377cecf06c4429d1098c09fe97ba
Glatzel <893016099@qq.com>
2025-03-06 09:53:56 +0800

    fix

--- crates/pyxis-cuda/src_cuda/crypto.cpp
+++ crates/pyxis-cuda/src_cuda/crypto.cpp
@@ -6,10 +6,31 @@
 #endif
 #include <cmath>
 #define M_PI 3.14159265358979323846
+CUDA_DEVICE void transform(double x, double y, double &lon, double &lat)
+{
+    double xy = x * y;
+    double abs_x = sqrt(abs(x));
+    double x_pi = x * M_PI;
+    double y_pi = y * M_PI;
+    double d = (20.0) * sin((6.0) * x_pi) + (20.0) * sin(2.0 * x_pi);
+
+    lat = d;
+    lon = d;
+
+    lat += (20.0) * sin(y_pi) + (40.0) * sin(y_pi / (3.0)) + (160.0) * sin(y_pi / (12.0)) + (320.0) * sin(y_pi / (30.0));
+    lon += (20.0) * sin(x_pi) + (40.0) * sin(x_pi / (3.0)) + (150.0) * sin(x_pi / (12.0)) + (300.0) * sin(x_pi / (30.0));
+
+    lat *= (2.0) / (3.0);
+    lon *= (2.0) / (3.0);
+
+    lat += (-100.0) + 2.0 * x + (3.0) * y + (0.2) * y * y + (0.1) * xy + (0.2) * abs_x;
+    lon +=
+        (300.0) + x + 2.0 * y + (0.1) * x * x + (0.1) * xy + (0.1) * abs_x;
+}
 CUDA_DEVICE void bd09_to_gcj02(double bd09_lon, double bd09_lat,
                                double &gcj02_lon, double &gcj02_lat)
 {
-    double x_pi=M_PI * 3000.0 / 180.0;
+    double x_pi = M_PI * 3000.0 / 180.0;
     double x = bd09_lon - 0.0065;
     double y = bd09_lat - 0.006;
     double z = sqrt(x * x + y * y) - 0.00002 * sin(y * x_pi);
@@ -21,7 +42,7 @@ CUDA_DEVICE void gcj02_to_bd09(double gcj02_lon, double gcj02_lat,
                                double &bd09_lon, double &bd09_lat)
 
 {
-    double x_pi=M_PI * 3000.0 / 180.0;
+    double x_pi = M_PI * 3000.0 / 180.0;
     double z = sqrt(gcj02_lon * gcj02_lon + gcj02_lat * gcj02_lat) + 0.00002 * sin(gcj02_lat * x_pi);
     double theta = atan2(gcj02_lat, gcj02_lon) + 0.000003 * cos(gcj02_lon * x_pi);
     bd09_lon = z * cos(theta) + 0.0065;

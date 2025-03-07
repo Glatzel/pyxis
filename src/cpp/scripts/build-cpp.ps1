f044cf8da6d2478d92bf15ad14311c0fcb138475
Glatzel <893016099@qq.com>
2025-03-07 12:40:34 +0800

    fix

--- src/cpp/scripts/build-cpp.ps1
+++ src/cpp/scripts/build-cpp.ps1
@@ -1,21 +1,16 @@
-param(
-    $config,
-    $install = "../../dist/pyxis-cpp"
-)
-
+param($config)
+Set-Location $PSScriptRoot
+Set-Location ..
 if ($config) {
     $config = "-DCMAKE_BUILD_TYPE=Release"
 }
-if ($install) {
-    if (-not (Test-Path $install)) {
-        New-Item $install -ItemType Directory
-    }
-    $install = Resolve-Path $install
-    $install = "$install".Replace('\', '/')
-    $install = "-DCMAKE_INSTALL_PREFIX=$install"
-}
-Set-Location $PSScriptRoot
-Set-Location ..
+# create install dir
+$install = "../../dist/pyxis-cpp"
+Remove-Item $install -Recurse -ErrorAction SilentlyContinue
+New-Item $install -ItemType Directory -ErrorAction SilentlyContinue
+$install = Resolve-Path $install
+$install = "$install".Replace('\', '/')
+$install = "-DCMAKE_INSTALL_PREFIX=$install"
 
 cmake . -B build $install $config
 if ($install) {
@@ -24,8 +19,6 @@ if ($install) {
 else {
     cmake --build build
 }
-Set-Location $PSScriptRoot
-Set-Location ..
 if ($IsWindows) {
     7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on `
         "../../dist/pyxis-cpp-windows-x64.7z" "../../dist/pyxis-cpp/"

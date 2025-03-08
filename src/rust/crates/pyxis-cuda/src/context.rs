757dfba38e0c50083265b2d024bb46bc57a53acd
Glatzel <893016099@qq.com>
2025-03-08 22:09:25 +0800

    fix

--- src/rust/crates/pyxis-cuda/src/context.rs
+++ src/rust/crates/pyxis-cuda/src/context.rs
@@ -28,15 +28,15 @@ impl Default for PyxisCudaContext {
 
 impl PyxisCudaContext {
     /// Create a new module manager
-    pub fn new(size_limit: usize, count_size: usize) -> Self {
+    pub fn new(size_limit: usize, count_limit: usize) -> Self {
         Self {
             _ctx: cust::quick_init().unwrap(),
             stream: Stream::new(StreamFlags::NON_BLOCKING, 1i32.into()).unwrap(),
             module_cache: RefCell::new(HashMap::new()),
             lru: RefCell::new(VecDeque::new()),
             total_size: RefCell::new(0),
-            size_limit: size_limit,
-            count_limit: count_size,
+            size_limit,
+            count_limit,
         }
     }
 }
@@ -44,18 +44,21 @@ impl PyxisCudaContext {
 impl PyxisCudaContext {
     /// Get the active module or load it if not already loaded
     pub(crate) fn get_module(&self, ptx: &PyxisPtx) -> Ref<Module> {
+        // add module to cache if it is not exist in cache
         if !self.module_cache.borrow().contains_key(ptx.name) {
             self.add_module(ptx);
         }
 
+        // set this module as newest
         self.lru.borrow_mut().push_front(ptx.name);
         Ref::map(self.module_cache.borrow(), |m| &m.get(ptx.name).unwrap().0)
     }
     fn add_module(&self, ptx: &PyxisPtx) {
-        // clear extra module
+        // clear last module if reach count limit
         if self.count_limit > 0 && self.lru.borrow().len() + 1 > self.count_limit {
             self.remove_last_module();
         }
+        // clear modules until total size smaller than limit
         while *self.total_size.borrow() + ptx.size > self.size_limit {
             self.remove_last_module();
         }

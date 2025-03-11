6c3ced9f96dada24d78750e8a5436c659647e2c8
Glatzel <893016099@qq.com>
2025-03-11 13:28:08 +0800

    fix

--- src/rattler/pyxis-cli/ci-pyxis-cli.ps1
+++ src/rattler/pyxis-cli/ci-pyxis-cli.ps1
@@ -1,3 +1,8 @@
+param (
+    [ValidateSet("dist", "release", "debug")]
+    [string]$config = "debug"
+)
+
 Set-Location $PSScriptRoot
 . ../scripts/utils.ps1
 & "../../rust/scripts/build-cli.ps1" -config dist

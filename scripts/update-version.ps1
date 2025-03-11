b4c3b2bfa15673e7155c96ec7f8c08404a4da1d8
Glatzel <893016099@qq.com>
2025-03-11 15:18:59 +0800

    fix

--- scripts/update-version.ps1
+++ scripts/update-version.ps1
@@ -18,7 +18,7 @@ $cargoTomlPath = "./src/rust/Cargo.toml"
 Write-Host "Updated Rust version to $version"
 
 # Update the version in rattler
-foreach ($pkg in ("pyxis-cli")) {
+foreach ($pkg in ("pyxis-cli","pyxis-py")) {
     $recipe_path = "./src/rattler/$pkg/recipe.yaml"
     (Get-Content -Path $recipe_path) -replace '^  version: .*', "  version: $version" | Set-Content -Path $recipe_path
     Write-Host "Updated ratter $pkg version to $version"

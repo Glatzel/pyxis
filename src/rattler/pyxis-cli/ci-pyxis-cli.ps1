7ab41a420d64c1f6957d74cd734796dbe319871f
Glatzel <893016099@qq.com>
2025-03-11 11:49:44 +0800

    fix

--- src/rattler/pyxis-cli/ci-pyxis-cli.ps1
+++ src/rattler/pyxis-cli/ci-pyxis-cli.ps1
@@ -1,15 +1,6 @@
 Set-Location $PSScriptRoot
-
+. ../scripts/utils.ps1
 & "../../rust/scripts/build-cli.ps1" -config dist
-
-Write-Output "::group::build"
-pixi run rattler-build build
-Write-Output "::endgroup::"
-
-Write-Output "::group::test"
-    if ($IsWindows) {
-        foreach ($conda_file in Get-ChildItem "./output/win-64/*.conda") {
-            pixi run rattler-build test --package-file $conda_file
-        }
-    }
-Write-Output "::endgroup::"
+Set-Location $PSScriptRoot
+build_pkg
+test_pkg

2f3eb051a3c2a4e5bb54eea1957c05512b1dee03
Glatzel <893016099@qq.com>
2025-03-11 14:31:11 +0800

    fix

--- src/rust/scripts/py-develop.ps1
+++ src/rust/scripts/py-develop.ps1
@@ -8,8 +8,8 @@ Set-Location ..
 Set-Location crates/pyxis-py
 Remove-Item pyxis/pyxis.pyd -ErrorAction SilentlyContinue
 
-pixi run cargo build  $config -p pyxis-py
-pixi run maturin develop $config
+pixi run cargo build --profile  $config -p pyxis-py
+pixi run maturin develop --profile $config
 
 Set-Location $PSScriptRoot
 Set-Location ../../../

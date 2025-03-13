7fb36505c56add52ae3d286b594a1bd6c9963067
Glatzel <893016099@qq.com>
2025-03-13 11:49:47 +0800

    fix

--- scripts/build-cpp.ps1
+++ scripts/build-cpp.ps1
@@ -12,9 +12,10 @@ $install = "$ROOT/dist/pyxis-cpp"
 Remove-Item $install -Recurse -ErrorAction SilentlyContinue
 New-Item $install -ItemType Directory -ErrorAction SilentlyContinue
 $install = "-DCMAKE_INSTALL_PREFIX=$install"
-New-Item ./build -ItemType Directory -ErrorAction SilentlyContinue
+New-Item $ROOT/build -ItemType Directory -ErrorAction SilentlyContinue
 Set-Location $ROOT/build
 # build
+write-output Get-ChildItem $ROOT
 cmake -S .. -B build $install $config -DBUILD_CPP=ON
 cmake --build build --target install
 

e9d1aefc68205e58541d49bed2b8259a870b74b4
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-03-14 20:31:43 +0800

    ci: isolate jobs (#208)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- python/rattler/ci-pyxis-py.ps1
+++ python/rattler/ci-pyxis-py.ps1
@@ -4,7 +4,6 @@ param (
 )
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot
-. ../scripts/utils.ps1
 
 & "$ROOT/python/scripts/build-cuda.ps1"
 & "$ROOT/python/scripts/py-develop.ps1" -config $config
@@ -12,5 +11,5 @@ Set-Location $PSScriptRoot
 & "$ROOT/python/scripts/build-python-whl.ps1" -config $config
 
 Set-Location $PSScriptRoot
-build_pkg
+pixi run rattler-build build
 Set-Location $ROOT
--- rattler/scripts/utils.ps1
+++
@@ -1,5 +0,0 @@
-function build_pkg {
-    Write-Output "::group::build"
-    pixi run rattler-build build
-    Write-Output "::endgroup::"
-}

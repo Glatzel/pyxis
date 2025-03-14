8fec65110368f5becf6b0377b06837d81b520de1
Glatzel <893016099@qq.com>
2025-03-14 20:13:46 +0800

    ci: isolate jobs

--- rust/rattler/ci-pyxis-cli.ps1
+++ rust/rattler/ci-pyxis-cli.ps1
@@ -4,13 +4,12 @@ param (
 )
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot
-. ../scripts/utils.ps1
 
 & "$ROOT/rust/scripts/build-rust-cli.ps1" -config $config
 & "$ROOT/rust/crates/pyxis-cli/examples/cli.ps1"
 
 Set-Location $PSScriptRoot
-build_pkg
+pixi run rattler-build build
 New-Item $ROOT/dist -ItemType Directory -ErrorAction SilentlyContinue
 Copy-Item ./output/**.conda $ROOT/dist
 Set-Location $ROOT

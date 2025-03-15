6ff949727e65468c6ae07ec5d95b8da519826d39
Glatzel <893016099@qq.com>
2025-03-15 10:14:58 +0800

    fix

--- rust/scripts/set-env.ps1
+++ rust/scripts/set-env.ps1
@@ -9,17 +9,17 @@ pixi install
 if ($IsWindows) {
     # set libproj find path for pkgconfig
     if ($link -eq 'static') {
-        $env:PKG_CONFIG_PATH = "$ROOT/vcpkg/installed/static/x64-windows-static/lib/pkgconfig"
+        $env:PKG_CONFIG_PATH = Resolve-Path "./vcpkg/vcpkg/installed/static/x64-windows-static/lib/pkgconfig"
     }
     else {
-        $env:PKG_CONFIG_PATH = "$ROOT/vcpkg/installed/dynamic/x64-windows/lib/pkgconfig"
+        $env:PKG_CONFIG_PATH = Resolve-Path "./vcpkg/vcpkg/installed/dynamic/x64-windows/lib/pkgconfig"
     }
     # copy proj.db to cli src
-    Copy-Item $ROOT/rust/.pixi/envs/default/Library/share/proj/proj.db ./crates/pyxis-cli/src/proj.db
+    Copy-Item ./.pixi/envs/default/Library/share/proj/proj.db ./crates/pyxis-cli/src/proj.db
 }
 if ($IsLinux) {
     # copy proj.db to cli src
     pixi install
-    Copy-Item $ROOT/rust/.pixi/envs/default/share/proj/proj.db ./crates/pyxis-cli/src/proj.db
+    Copy-Item ./.pixi/envs/default/share/proj/proj.db ./crates/pyxis-cli/src/proj.db
 }
 Set-Location $current_dir

59e3a138c34f5c7689e531f3ee4d10ed505e8bb9
Glatzel <893016099@qq.com>
2025-03-15 14:23:36 +0800

    fix

--- scripts/create-ci-link.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location $PSScriptRoot/..
-foreach ($lang in ("cpp", "csharp", "cuda", "python", "rust")) {
-    copy-Item "./$lang/.github/workflows/ci.yml" "./.github/workflows/$lang-ci.yml"
-}

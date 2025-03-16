720f030a339a9a9e9a1c1988d826c19c37fbd11d
Glatzel <32577755+Glatzel@users.noreply.github.com>
2025-03-16 20:10:22 +0800

    feat: simplify build scripts (#234)

--- python/scripts/maturin-develop.ps1
+++ python/scripts/maturin-develop.ps1
@@ -7,7 +7,6 @@ $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 Remove-Item pyxis/pyxis.pyd -ErrorAction SilentlyContinue
 
-pixi run cargo build --profile  $config -p pyxis-py
 pixi run maturin develop --profile $config
 
 Set-Location $ROOT

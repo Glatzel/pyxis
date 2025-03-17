7d2fb3f02123a46c127c527c7fb2f3248c7c0050
Glatzel <893016099@qq.com>
2025-03-17 17:47:34 +0800

    fix

--- python/tests/test_pyxis_cuda/test_crypto_cuda.py
+++ python/tests/test_pyxis_cuda/test_crypto_cuda.py
@@ -25,37 +25,13 @@ wgs84_array = (
     np.array([30.6107779, 30.6107779], np.float64),
 )
 
-
-# @pytest.mark.parametrize(
-#     ("src", "dst", "input", "expected"),
-#     [
-#         (COORD_CRYPTO_SPACE.WGS84, COORD_CRYPTO_SPACE.BD09, wgs84_array, bd09),
-#         (COORD_CRYPTO_SPACE.WGS84, COORD_CRYPTO_SPACE.GCJ02, wgs84_array, gcj02),
-#         (COORD_CRYPTO_SPACE.GCJ02, COORD_CRYPTO_SPACE.BD09, gcj02_array, bd09),
-#         (COORD_CRYPTO_SPACE.GCJ02, COORD_CRYPTO_SPACE.WGS84, gcj02_array, wgs84),
-#         (COORD_CRYPTO_SPACE.BD09, COORD_CRYPTO_SPACE.GCJ02, bd09_array, gcj02),
-#         (COORD_CRYPTO_SPACE.BD09, COORD_CRYPTO_SPACE.WGS84, bd09_array, wgs84),
-#     ],
-# )
-# @pytest.mark.parametrize("exact", [True, False])
-# def test_simple(src, dst, input, expected, exact):
-#     import cupy as cp
-
-#     in_lon = deepcopy(input[0])
-#     in_lat = deepcopy(input[1])
-#     module = pyxis.pyxis_cuda.CryptoCuda()
-#     lon, lat = module.crypto_exact("double", cp.asarray(in_lon), cp.asarray(in_lat), src, dst, 1e-17, 100)
-#     assert cp.asnumpy(lon)[0] == pytest.approx(expected[0])
-#     assert cp.asnumpy(lat)[0] == pytest.approx(expected[1])
-
-
 @pytest.mark.cuda
 @pytest.mark.parametrize(
     ("src", "dst", "input", "expected"),
     [
         (COORD_CRYPTO_SPACE.GCJ02, COORD_CRYPTO_SPACE.WGS84, gcj02_array, wgs84),
-        # (COORD_CRYPTO_SPACE.BD09, COORD_CRYPTO_SPACE.GCJ02, bd09_array, gcj02),
-        # (COORD_CRYPTO_SPACE.BD09, COORD_CRYPTO_SPACE.WGS84, bd09_array, wgs84),
+        (COORD_CRYPTO_SPACE.BD09, COORD_CRYPTO_SPACE.GCJ02, bd09_array, gcj02),
+        (COORD_CRYPTO_SPACE.BD09, COORD_CRYPTO_SPACE.WGS84, bd09_array, wgs84),
     ],
 )
 def test_exact(src, dst, input, expected):

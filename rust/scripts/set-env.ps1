b96b97a5cd61a38c1631e1ff6180e2f824fa8292
Glatzel <893016099@qq.com>
2025-04-19 13:51:51 +0800

    fix

--- rust/scripts/set-env.ps1
+++ rust/scripts/set-env.ps1
@@ -2,6 +2,8 @@ $ROOT = git rev-parse --show-toplevel
 $current_dir = Resolve-Path $PWD
 Set-Location $PSScriptRoot/..
 pixi install
+$pkg_config_exe = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/bin
+$env:Path = "$pkg_config_exe;$env:Path"
 if ($IsWindows) {
     $env:PKG_CONFIG_PATH = Resolve-Path "./.pixi/envs/default/proj/x64-windows-static/lib/pkgconfig"
     Copy-Item ./.pixi/envs/default/proj/x64-windows-static/share/proj/proj.db ./crates/pyxis-cli/src/proj.db

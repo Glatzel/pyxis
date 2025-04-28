a7bd1e1761085c7b7a266aec4c1f45073b4bb025
Glatzel <893016099@qq.com>
2025-04-28 21:20:36 +0800

    fix

--- rust/scripts/proj-bindings.ps1
+++ rust/scripts/proj-bindings.ps1
@@ -2,6 +2,5 @@ $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 $env:RUSTFLAGS = "-Dwarnings"
 & $PSScriptRoot/set-env.ps1
-$env:OUT_DIR=resolve-path "$PSScriptRoot/../crates/proj-sys/src"
 cargo build  --all-features -p proj-sys
 Set-Location $ROOT

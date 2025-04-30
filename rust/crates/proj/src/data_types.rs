a609b9b4a3050f5666b92730e31963dc27571896
Glatzel <893016099@qq.com>
2025-04-30 21:07:09 +0800

    fix

--- rust/crates/proj/src/data_types.rs
+++ rust/crates/proj/src/data_types.rs
@@ -2,17 +2,59 @@
 pub struct Proj {
     pj: *mut proj_sys::PJ,
 }
+impl Drop for Proj {
+    fn drop(&mut self) {
+        unsafe { proj_sys::proj_destroy(self.pj) };
+    }
+}
+// region:Coordinate transformation
+///https://proj.org/en/stable/development/reference/functions.html#coordinate-transformation
+impl Proj {
+    ///https://proj.org/en/stable/development/reference/functions.html#c.proj_trans
+    pub fn trans(&self) {}
+}
+struct _ProjDirection {}
+
+pub struct Context {
+    ctx: *mut proj_sys::PJ_CONTEXT,
+}
+// region:Threading contexts
+impl Context {
+    /// #References
+    /// https://proj.org/en/stable/development/reference/functions.html#c.proj_context_create
+    pub fn new() -> Self {
+        Self {
+            ctx: unsafe { proj_sys::proj_context_create() },
+        }
+    }
+}
+impl Clone for Context {
+    /// #References
+    /// https://proj.org/en/stable/development/reference/functions.html#c.proj_context_clone
+    fn clone(&self) -> Self {
+        Self {
+            ctx: unsafe { proj_sys::proj_context_clone(self.ctx) },
+        }
+    }
+}
+impl Drop for Context {
+    /// #References
+    /// https://proj.org/en/stable/development/reference/functions.html#c.proj_context_destroy
+    fn drop(&mut self) {
+        unsafe { proj_sys::proj_context_destroy(self.ctx) };
+    }
+}
 // region:Transformation setup
 ///https://proj.org/en/stable/development/reference/functions.html#transformation-setup
-impl Proj {
+impl Context {
     ///https://proj.org/en/stable/development/reference/functions.html#c.proj_create
-    pub fn new(ctx: Context, definition: &str) -> Self {
-        Self {
-            pj: unsafe { proj_sys::proj_create(ctx.ctx, definition.as_ptr() as *const i8) },
+    pub fn proj_create(&self, definition: &str) -> Proj {
+        Proj {
+            pj: unsafe { proj_sys::proj_create(self.ctx, definition.as_ptr() as *const i8) },
         }
     }
     ///https://proj.org/en/stable/development/reference/functions.html#c.proj_create_argv
-    pub fn from_argv(ctx: Context, definition: &[&str]) -> Self {
+    pub fn proj_create_argv(&self, definition: &[&str]) -> Proj {
         let len = definition.len();
         let mut ptrs: Vec<*mut i8> = Vec::with_capacity(len);
         for s in definition {
@@ -20,16 +62,16 @@ impl Proj {
             let ptr = c_str.as_ptr() as *mut i8; // Convert to *mut i8
             ptrs.push(ptr);
         }
-        Self {
-            pj: unsafe { proj_sys::proj_create_argv(ctx.ctx, len as i32, ptrs.as_mut_ptr()) },
+        Proj {
+            pj: unsafe { proj_sys::proj_create_argv(self.ctx, len as i32, ptrs.as_mut_ptr()) },
         }
     }
     ///https://proj.org/en/stable/development/reference/functions.html#c.proj_create_crs_to_crs
-    pub fn from_crs_to_crs(ctx: Context, source_crs: &str, target_crs: &str, area: Area) -> Self {
-        Self {
+    pub fn proj_create_crs_to_crs(&self, source_crs: &str, target_crs: &str, area: Area) -> Proj {
+        Proj {
             pj: unsafe {
                 proj_sys::proj_create_crs_to_crs(
-                    ctx.ctx,
+                    self.ctx,
                     source_crs.as_ptr() as *const i8,
                     target_crs.as_ptr() as *const i8,
                     area.area,
@@ -38,8 +80,8 @@ impl Proj {
         }
     }
     ///https://proj.org/en/stable/development/reference/functions.html#c.proj_create_crs_to_crs_from_pj
-    pub fn from_crs_to_crs_pj(
-        ctx: Context,
+    pub fn proj_create_crs_to_crs_from_pj(
+        &self,
         source_crs: Proj,
         target_crs: Proj,
         area: Area,
@@ -48,7 +90,7 @@ impl Proj {
         allow_ballpark: Option<bool>,
         only_best: Option<bool>,
         force_over: Option<bool>,
-    ) -> Self {
+    ) -> Proj {
         let mut options: Vec<*const i8> = Vec::with_capacity(5);
         if let Some(authority) = authority {
             options.push(format!("AUTHORITY={}", authority).as_ptr() as *mut i8);
@@ -75,10 +117,10 @@ impl Proj {
                 format!("FORCE_OVER={}", if force_over { "YES" } else { "NO" }).as_ptr() as *mut i8,
             );
         }
-        Self {
+        Proj {
             pj: unsafe {
                 proj_sys::proj_create_crs_to_crs_from_pj(
-                    ctx.ctx,
+                    self.ctx,
                     source_crs.pj,
                     target_crs.pj,
                     area.area,
@@ -92,47 +134,7 @@ impl Proj {
         unimplemented!()
     }
 }
-impl Drop for Proj {
-    fn drop(&mut self) {
-        unsafe { proj_sys::proj_destroy(self.pj) };
-    }
-}
-// region:Coordinate transformation
-///https://proj.org/en/stable/development/reference/functions.html#coordinate-transformation
-impl Proj {
-    ///https://proj.org/en/stable/development/reference/functions.html#c.proj_trans
-    pub fn trans(&self) {}
-}
-struct _ProjDirection {}
 
-pub struct Context {
-    ctx: *mut proj_sys::PJ_CONTEXT,
-}
-impl Context {
-    /// #References
-    /// https://proj.org/en/stable/development/reference/functions.html#c.proj_context_create
-    pub fn new() -> Self {
-        Self {
-            ctx: unsafe { proj_sys::proj_context_create() },
-        }
-    }
-}
-impl Clone for Context {
-    /// #References
-    /// https://proj.org/en/stable/development/reference/functions.html#c.proj_context_clone
-    fn clone(&self) -> Self {
-        Self {
-            ctx: unsafe { proj_sys::proj_context_clone(self.ctx) },
-        }
-    }
-}
-impl Drop for Context {
-    /// #References
-    /// https://proj.org/en/stable/development/reference/functions.html#c.proj_context_destroy
-    fn drop(&mut self) {
-        unsafe { proj_sys::proj_context_destroy(self.ctx) };
-    }
-}
 pub struct Area {
     area: *mut proj_sys::PJ_AREA,
 }

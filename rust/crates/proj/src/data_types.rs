07cbe39932b11d2ea342088cb96ebf73193e2242
Glatzel <893016099@qq.com>
2025-05-01 12:48:33 +0800

    fix

--- rust/crates/proj/src/data_types.rs
+++ rust/crates/proj/src/data_types.rs
@@ -1,10 +1,12 @@
 mod error_codes;
 mod info_structures;
+mod iso_19111;
 mod list_structures;
 mod projection_derivatives;
 mod transformation_objects;
 
 pub use info_structures::*;
+pub use iso_19111::*;
 pub use list_structures::*;
 pub use projection_derivatives::*;
 pub use transformation_objects::*;

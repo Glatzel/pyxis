6a60d9f73f112c6a131b2b333209679fa8871593
Glatzel <893016099@qq.com>
2025-05-02 20:59:19 +0800

    fix

--- rust/crates/pyxis-cli/src/proj_util.rs
+++ rust/crates/pyxis-cli/src/proj_util.rs
@@ -11,12 +11,12 @@ pub fn init_proj_builder() -> miette::Result<proj::PjContext> {
         clerk::info!("PROJ_DATA environment variable is found: {proj_data}");
         ctx.set_search_paths(&[&PathBuf::from(proj_data)])?;
     } else {
-        clerk::info!("PROJ_DATA environment variable is not found");
+        clerk::debug!("PROJ_DATA environment variable is not found");
         let exe_path = std::env::current_exe().into_diagnostic()?;
         clerk::debug!("current_exe: {}", exe_path);
         let exe_root = exe_path.parent().unwrap();
         if !exe_root.join("proj.db").exists() {
-            clerk::warn!("proj.db is not found.");
+            clerk::warn!("proj.db is not found. Try to use bundled proj.db");
             let mut db_file = std::fs::File::create(exe_root.join("proj.db")).into_diagnostic()?;
             db_file.write_all(PROJ_DB).into_diagnostic()?;
             clerk::info!("Write to: {}", exe_root.join("proj.db").to_str().unwrap());

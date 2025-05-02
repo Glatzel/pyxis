ae579cf50059001f407269cf0ca030f0d03c8a89
Glatzel <893016099@qq.com>
2025-05-02 11:42:58 +0800

    fix

--- rust/crates/proj/src/functions/setting_custom_io_functions.rs
+++ rust/crates/proj/src/functions/setting_custom_io_functions.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 impl crate::PjContext {
     pub fn set_fileapi(&self) {
         unimplemented!()
@@ -8,8 +10,16 @@ impl crate::PjContext {
     pub fn set_file_finder(&self) {
         unimplemented!()
     }
-    pub fn set_search_paths(&self) {
-        unimplemented!()
+    pub fn set_search_paths(&self, paths: &[&Path]) -> miette::Result<&Self> {
+        let len = paths.len();
+        let paths: Vec<*const i8> = paths
+            .iter()
+            .map(|p| (crate::string_to_c_char(&p.to_string_lossy()).unwrap()) as *const i8)
+            .collect();
+        unsafe {
+            proj_sys::proj_context_set_search_paths(self.ctx, len as i32, paths.as_ptr());
+        };
+        Ok(self)
     }
     pub fn set_ca_bundle_path(&self) {
         unimplemented!()

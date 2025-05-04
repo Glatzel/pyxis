a64f9847526b74c63616bce0ce633a96471c87c4
Glatzel <893016099@qq.com>
2025-05-04 19:29:42 +0800

    fix

--- rust/crates/proj/src/functions.rs
+++ rust/crates/proj/src/functions.rs
@@ -3,27 +3,21 @@ mod area;
 mod cleanup;
 mod coordinate_transformation;
 mod custom_io;
-#[cfg(feature = "distances")]
 mod distances;
 mod error_reporting;
-#[cfg(feature = "info")]
 mod info;
-#[cfg(feature = "iso19111")]
 mod iso19111;
-#[cfg(feature = "lists")]
 mod lists;
-#[cfg(feature = "network")]
 mod network;
 mod threading_contexts;
 mod transformation_setup;
 mod various;
 
-#[cfg(feature = "distances")]
+
 pub use distances::*;
-#[cfg(feature = "info")]
+
 pub use info::*;
 // pub use iso19111::*;
 #[cfg(feature = "unsuggested")]
 pub use cleanup::*;
-#[cfg(feature = "lists")]
 pub use lists::*;

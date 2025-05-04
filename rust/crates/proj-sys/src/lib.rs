1f1b31408b00c7424a29ae4c0037f5406bf57bf1
Glatzel <chenzelin9794@gmail.com>
2025-05-04 16:55:34 +0800

    build: update proj bindings (#303)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- rust/crates/proj-sys/src/lib.rs
+++ rust/crates/proj-sys/src/lib.rs
@@ -1,9 +1,5 @@
 #![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
-#![no_std]
-
+#[cfg(not(feature = "bindgen"))]
+include!("bindings.rs");
 #[cfg(feature = "bindgen")]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-#[cfg(not(feature = "bindgen"))]
-mod bindings;
-#[cfg(not(feature = "bindgen"))]
-pub use bindings::*;

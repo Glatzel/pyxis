72516de55560a66668d0797f73d9d3e122e476f3
Glatzel <893016099@qq.com>
2025-05-04 19:08:31 +0800

    fix

--- rust/crates/proj/src/functions/cleanup.rs
+++ rust/crates/proj/src/functions/cleanup.rs
@@ -1,5 +1,6 @@
 /// # References
 ///<https://proj.org/en/stable/development/reference/functions.html#c.proj_cleanup>
+#[cfg(feature = "unsuggested")]
 pub fn cleanup() {
     unsafe { proj_sys::proj_cleanup() };
 }

c0a588dc3e5a7dabe857361eed1c0a1137ec452e
Glatzel <893016099@qq.com>
2025-05-04 16:25:44 +0800

    fmt

--- rust/crates/proj-sys/src/lib.rs
+++ rust/crates/proj-sys/src/lib.rs
@@ -1,8 +1,4 @@
-#![allow(
-    non_camel_case_types,
-    non_upper_case_globals,
-    non_snake_case
-)]
+#![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
 #![no_std]
 
 #[cfg(feature = "bindgen")]

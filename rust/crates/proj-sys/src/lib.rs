6e2e5344edeef6340d179646964b1a91bddcea1a
Glatzel <893016099@qq.com>
2025-05-04 21:59:55 +0800

    fix

--- rust/crates/proj-sys/src/lib.rs
+++ rust/crates/proj-sys/src/lib.rs
@@ -1,5 +1,7 @@
 #![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
-#[cfg(not(feature = "bindgen"))]
-include!("bindings.rs");
+#[cfg(all(not(feature = "bindgen"), target_os = "windows"))]
+include!("bindings-win.rs");
+#[cfg(all(not(feature = "bindgen"), target_os = "linux"))]
+include!("bindings-linux.rs");
 #[cfg(feature = "bindgen")]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));

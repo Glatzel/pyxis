309dec8dd4ac2fab6835fd22ef7e22036cfd3930
Glatzel <893016099@qq.com>
2025-05-05 08:33:25 +0800

    fix

--- rust/crates/proj/src/functions/transformation_setup.rs
+++ rust/crates/proj/src/functions/transformation_setup.rs
@@ -1,3 +1,5 @@
+use std::ffi::CString;
+
 use miette::IntoDiagnostic;
 
 use crate::check_result;
@@ -8,7 +10,7 @@ impl crate::PjContext {
     /// # References
     ///<https://proj.org/en/stable/development/reference/functions.html#c.proj_create>
     pub fn create(&self, definition: &str) -> miette::Result<crate::Pj> {
-        let definition = std::ffi::CString::new(definition).into_diagnostic()?;
+        let definition = CString::new(definition).into_diagnostic()?;
         let pj = crate::Pj {
             pj: unsafe { proj_sys::proj_create(self.ctx, definition.as_ptr()) },
         };
@@ -21,7 +23,7 @@ impl crate::PjContext {
         let len = argv.len();
         let mut ptrs: Vec<*mut i8> = Vec::with_capacity(len);
         for s in argv {
-            ptrs.push(std::ffi::CString::new(*s).into_diagnostic()?.into_raw());
+            ptrs.push(CString::new(*s).into_diagnostic()?.into_raw());
         }
         let pj = crate::Pj {
             pj: unsafe { proj_sys::proj_create_argv(self.ctx, len as i32, ptrs.as_mut_ptr()) },
@@ -37,8 +39,8 @@ impl crate::PjContext {
         target_crs: &str,
         area: &crate::PjArea,
     ) -> miette::Result<crate::Pj> {
-        let source_crs = std::ffi::CString::new(source_crs).into_diagnostic()?;
-        let target_crs = std::ffi::CString::new(target_crs).into_diagnostic()?;
+        let source_crs = CString::new(source_crs).into_diagnostic()?;
+        let target_crs = CString::new(target_crs).into_diagnostic()?;
         let pj = crate::Pj {
             pj: unsafe {
                 proj_sys::proj_create_crs_to_crs(
@@ -58,7 +60,7 @@ impl crate::PjContext {
         &self,
         source_crs: crate::Pj,
         target_crs: crate::Pj,
-        area: crate::PjArea,
+        area: &crate::PjArea,
         authority: Option<&str>,
         accuracy: Option<f64>,
         allow_ballpark: Option<bool>,
@@ -67,28 +69,47 @@ impl crate::PjContext {
     ) -> miette::Result<crate::Pj> {
         let mut options: Vec<*const i8> = Vec::with_capacity(5);
         if let Some(authority) = authority {
-            options.push(format!("AUTHORITY={}", authority).as_ptr() as *mut i8);
+            options.push(
+                CString::new(format!("AUTHORITY={}", authority))
+                    .into_diagnostic()?
+                    .as_ptr(),
+            );
         }
         if let Some(accuracy) = accuracy {
-            options.push(format!("ACCURACY={}", accuracy).as_ptr() as *mut i8);
+            options.push(
+                CString::new(format!("ACCURACY={}", accuracy))
+                    .into_diagnostic()?
+                    .as_ptr(),
+            );
         }
         if let Some(allow_ballpark) = allow_ballpark {
             options.push(
-                format!(
+                CString::new(format!(
                     "ALLOW_BALLPARK={}",
                     if allow_ballpark { "YES" } else { "NO" }
-                )
-                .as_ptr() as *mut i8,
+                ))
+                .into_diagnostic()?
+                .as_ptr(),
             );
         }
         if let Some(only_best) = only_best {
             options.push(
-                format!("ONLY_BEST={}", if only_best { "YES" } else { "NO" }).as_ptr() as *mut i8,
+                CString::new(format!(
+                    "ONLY_BEST={}",
+                    if only_best { "YES" } else { "NO" }
+                ))
+                .into_diagnostic()?
+                .as_ptr(),
             );
         }
         if let Some(force_over) = force_over {
             options.push(
-                format!("FORCE_OVER={}", if force_over { "YES" } else { "NO" }).as_ptr() as *mut i8,
+                CString::new(format!(
+                    "FORCE_OVER={}",
+                    if force_over { "YES" } else { "NO" }
+                ))
+                .into_diagnostic()?
+                .as_ptr(),
             );
         }
         let pj = crate::Pj {
@@ -137,6 +158,23 @@ mod test {
     fn test_create_crs_to_crs() -> miette::Result<()> {
         let ctx = crate::PjContext::default();
         ctx.create_crs_to_crs("EPSG:4326", "EPSG:4978", &crate::PjArea::default())?;
+        Ok(())
+    }
+    #[test]
+    fn test_create_crs_to_crs_from_pj() -> miette::Result<()> {
+        let ctx = crate::PjContext::default();
+        let pj1 = ctx.create("EPSG:4326")?;
+        let pj2 = ctx.create("EPSG:4978")?;
+        ctx.create_crs_to_crs_from_pj(
+            pj1,
+            pj2,
+            &crate::PjArea::default(),
+            Some("any"),
+            Some(0.001),
+            Some(true),
+            Some(true),
+            Some(true),
+        )?;
         Ok(())
     }
 }

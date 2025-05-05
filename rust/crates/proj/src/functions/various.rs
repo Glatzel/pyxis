19751a16e4d2b48d914e57ebce9b09d203345d02
Glatzel <893016099@qq.com>
2025-05-05 14:25:59 +0800

    fix

--- rust/crates/proj/src/functions/various.rs
+++ rust/crates/proj/src/functions/various.rs
@@ -3,17 +3,53 @@ use std::ffi::CString;
 
 use miette::IntoDiagnostic;
 
-use crate::{array4_to_pj_coord, check_result};
+use crate::check_result;
 
 /// # Various
 impl crate::Pj {
-    pub fn roundtrip(&self) { unimplemented!() }
-    pub fn factors<T>(&self, coord: T) -> miette::Result<crate::PjFactors>
-    where
-        T: crate::IPjCoord,
-    {
-        let factor =
-            unsafe { proj_sys::proj_factors(self.pj, array4_to_pj_coord(coord.to_array4())?) };
+    ///Measure internal consistency of a given transformation. The function
+    /// performs n round trip transformations starting in either the forward or
+    /// reverse direction. Returns the euclidean distance of the starting point
+    /// coo and the resulting coordinate after n iterations back and forth.
+    ///
+    /// # References
+    ///<https://proj.org/en/stable/development/reference/functions.html#c.proj_roundtrip>
+    #[cfg(any(feature = "unrecommended", test))]
+    pub fn roundtrip(
+        &self,
+        dir: &crate::PjDirection,
+        n: i32,
+        coord: &mut crate::PjCoord,
+    ) -> miette::Result<f64> {
+        let distance = unsafe { proj_sys::proj_roundtrip(self.pj, i32::from(dir), n, coord) };
+        check_result!(self);
+        Ok(distance)
+    }
+
+    ///Calculate various cartographic properties, such as scale factors,
+    /// angular distortion and meridian convergence. Depending on the underlying
+    /// projection values will be calculated either numerically (default) or
+    /// analytically.
+    ///
+    ///Starting with PROJ 8.2, the P object can be a projected CRS, for example
+    /// instantiated from a EPSG CRS code. The factors computed will be those of
+    /// the map projection implied by the transformation from the base
+    /// geographic CRS of the projected CRS to the projected CRS. Starting with
+    /// PROJ 9.6, to improve performance on repeated calls on a projected CRS
+    /// object, the above steps will modify the internal state of the provided P
+    /// object, and thus calling this function concurrently from multiple
+    /// threads on the same P object will no longer be supported.
+
+    ///The input geodetic coordinate lp should be such that lp.lam is the
+    /// longitude in radian, and lp.phi the latitude in radian (thus
+    /// independently of the definition of the base CRS, if P is a projected
+    /// CRS).
+    ///
+    /// # References
+    ///<https://proj.org/en/stable/development/reference/functions.html#c.proj_factors>
+    #[cfg(any(feature = "unrecommended", test))]
+    pub fn factors(&self, coord: crate::PjCoord) -> miette::Result<crate::PjFactors> {
+        let factor = unsafe { proj_sys::proj_factors(self.pj, coord) };
         match self.errno() {
             crate::PjErrorCode::ProjSuccess => (),
             crate::PjErrorCode::CoordTransfmOutsideProjectionDomain => (),
@@ -109,11 +145,27 @@ pub fn rtodms2(r: f64, pos: char, neg: char) -> miette::Result<String> {
 
 #[cfg(test)]
 mod test {
+    use crate::IPjCoord;
+
+    #[test]
+    fn test_roundtrip() -> miette::Result<()> {
+        let ctx = crate::PjContext::default();
+        let pj = ctx.create_crs_to_crs("+proj=tmerc +lat_0=0 +lon_0=75 +k=1 +x_0=13500000 +y_0=0 +ellps=GRS80 +units=m +no_defs +type=crs","EPSG:4326",  &crate::PjArea::default())?;
+        let mut coord =
+            crate::array4_to_pj_coord((5877537.151800396, 4477291.358855194).to_array4())?;
+        let distance = pj.roundtrip(&crate::PjDirection::PjFwd, 10000, &mut coord)?;
+        println!("{:?}", unsafe { coord.xy.x });
+        println!("{:?}", unsafe { coord.xy.y });
+        assert_eq!(distance, 0.023350762947799957);
+        Ok(())
+    }
     #[test]
     fn test_factors() -> miette::Result<()> {
         let ctx = crate::PjContext::default();
         let pj = ctx.create_crs_to_crs("EPSG:4326", "EPSG:3857", &crate::PjArea::default())?;
-        let factor = pj.factors((12.0f64.to_radians(), 55.0f64.to_radians()))?;
+        let factor = pj.factors(crate::array4_to_pj_coord(
+            (12.0f64.to_radians(), 55.0f64.to_radians()).to_array4(),
+        )?)?;
 
         println!("{:?}", factor);
 
@@ -135,11 +187,14 @@ mod test {
 
         Ok(())
     }
+
     #[test]
     fn test_factors_fail() -> miette::Result<()> {
         let ctx = crate::PjContext::default();
         let pj = ctx.create("EPSG:4326")?;
-        let factor = pj.factors((12.0f64.to_radians(), 55.0f64.to_radians()));
+        let factor = pj.factors(crate::array4_to_pj_coord(
+            (12.0f64.to_radians(), 55.0f64.to_radians()).to_array4(),
+        )?);
         assert!(factor.is_err());
         Ok(())
     }

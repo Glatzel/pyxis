0fcc47341b3ddced410ec388cb1ec77258e53b6b
Glatzel <893016099@qq.com>
2025-05-05 14:40:30 +0800

    fix

--- rust/crates/proj/src/extension.rs
+++ rust/crates/proj/src/extension.rs
@@ -1,9 +1,13 @@
 mod conversion;
 mod error_handling;
 mod logging;
+#[cfg(test)]
+mod test_utils;
 mod traits;
 mod utils;
 
 pub(crate) use logging::*;
+#[cfg(test)]
+pub(crate) use test_utils::*;
 pub use traits::*;
 pub(crate) use utils::*;
---
+++ rust/crates/proj/src/extension/test_utils.rs
@@ -0,0 +1,11 @@
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+pub(crate) fn init_ctx() -> crate::PjContext {
+    tracing_subscriber::registry()
+        .with(clerk::terminal_layer(LevelFilter::TRACE, true))
+        .init();
+    let ctx = crate::PjContext::default();
+    ctx.set_log_level(crate::PjLogLevel::Trace);
+    ctx
+}

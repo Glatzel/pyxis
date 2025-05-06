7d086e770c6557a5dc11bb0364bb19a5e535b3d1
Glatzel <893016099@qq.com>
2025-05-06 09:07:10 +0800

    fix

--- rust/crates/proj/src/lib.rs
+++ rust/crates/proj/src/lib.rs
@@ -1,10 +1,8 @@
 pub mod data_types;
 mod extension;
 mod functions;
-mod version;
+pub mod version;
 
 pub use data_types::{Pj, PjArea, PjContext, PjDirection, PjLogLevel};
 pub use extension::*;
-#[allow(unused_imports)]
 pub use functions::*;
-pub use version::*;

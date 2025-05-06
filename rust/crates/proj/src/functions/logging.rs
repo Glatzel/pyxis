4697c6fbb96459410de62ef15fbd4b1feb0ca4bf
Glatzel <893016099@qq.com>
2025-05-06 08:01:28 +0800

    fix

--- rust/crates/proj/src/functions/logging.rs
+++ rust/crates/proj/src/functions/logging.rs
@@ -2,8 +2,10 @@
 impl crate::PjContext {
     /// # References
     /// <https://proj.org/en/stable/development/reference/functions.html#c.proj_log_level>
+    #[deprecated(note = "No plan. Use `crate::extension::logging` instead.")]
     fn _proj_log_level() { unimplemented!() }
     /// # References
     /// <https://proj.org/en/stable/development/reference/functions.html#c.proj_log_func>
+    #[deprecated(note = "No plan. Use `crate::extension::logging` instead.")]
     fn _proj_log_func() { unimplemented!() }
 }

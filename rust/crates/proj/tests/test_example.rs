e3b05c52c2435503cc64a1e2ebf153fa4ed01a84
Glatzel <893016099@qq.com>
2025-05-07 10:21:54 +0800

    fix

--- rust/crates/proj/tests/test_example.rs
+++ rust/crates/proj/tests/test_example.rs
@@ -5,10 +5,10 @@ use assert_cmd::Command;
 fn test_example_convert() {
     let root = PathBuf::from(std::env::var("CARGO_WORKSPACE_DIR").unwrap());
     let mut exe = root.clone();
-    exe.push("target/llvm-cov-target/debug/examples/convert.exe");
+    exe.push("target/llvm-cov-target/debug/examples/convert");
     if !exe.exists() {
         exe = root.clone();
-        exe.push("target/debug/examples/convert.exe")
+        exe.push("target/debug/examples/convert")
     }
     Command::new(exe).current_dir(root).assert().success();
 }

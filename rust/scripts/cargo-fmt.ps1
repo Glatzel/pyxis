6a6b511ecbdd947bfaa07e62329a93ad8d981d30
Glatzel <chenzelin9794@gmail.com>
2025-05-08 02:27:01 +0800

    ci: merge some check (#334)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- rust/scripts/cargo-fmt.ps1
+++ rust/scripts/cargo-fmt.ps1
@@ -3,13 +3,5 @@ $PSNativeCommandUseErrorActionPreference = $true
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 & $PSScriptRoot/set-env.ps1
-
-if ($env:CI) {
-    cargo +nightly fmt --all -- --check
-}
-else {
-    cargo +nightly fmt --all
-}
-
-
+cargo +nightly fmt --all --check
 Set-Location $ROOT

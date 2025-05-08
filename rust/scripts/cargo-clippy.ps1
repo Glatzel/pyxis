b6f84a80915742bb0cbb71555be79ac215600aec
Glatzel <893016099@qq.com>
2025-05-08 16:39:59 +0800

    fix

--- rust/scripts/cargo-clippy.ps1
+++ rust/scripts/cargo-clippy.ps1
@@ -2,14 +2,13 @@ $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 & $PSScriptRoot/set-env.ps1
 
-if ($env:CI) {
-    cargo +stable clippy --fix --all-features -p pyxis -p pyxis-cli -p proj -- -D warnings
-    cargo +stable clippy --all-features -p pyxis -p pyxis-cli -p proj -- -D warnings
-}
-else {
-   
-   
-} 
-cargo +stable clippy --fix --all -- -D warnings
-cargo +stable clippy --all -- -D warnings
+# if ($env:CI) {
+#     cargo +stable clippy --fix --all-features -p pyxis -p pyxis-cli -p proj -- -D warnings
+#     cargo +stable clippy --all-features -p pyxis -p pyxis-cli -p proj -- -D warnings
+# }
+# else {
+    cargo +stable clippy --fix --all -- -D warnings
+    cargo +stable clippy --all -- -D warnings
+# } 
+
 Set-Location $ROOT

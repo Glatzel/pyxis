1cc8f24879c584a8758d277639f838f195906a15
Glatzel <893016099@qq.com>
2025-05-08 14:05:55 +0800

    fix

--- rust/scripts/cargo-clippy.ps1
+++ rust/scripts/cargo-clippy.ps1
@@ -5,10 +5,10 @@ Set-Location $PSScriptRoot/..
 & $PSScriptRoot/set-env.ps1
 
 if ($env:CI) {
-    cargo +stable clippy --all-features -p pyxis -p pyxis-cli -p proj
+    cargo +stable clippy --fix --all-features -p pyxis -p pyxis-cli -p proj -- -D warnings
 }
 else {
-    cargo clippy --fix --all-targets --all-features
+    cargo clippy --fix --all -- -D warnings
 }
 
 Set-Location $ROOT

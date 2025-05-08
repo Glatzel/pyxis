2d75b8bd55fb47ac03877bd2b41c9b6b7847a909
Glatzel <893016099@qq.com>
2025-05-08 16:59:19 +0800

    fix

--- python/scripts/cargo-clippy1.ps1
+++ python/scripts/cargo-clippy1.ps1
@@ -1,5 +1,5 @@
-$ROOT = git rev-parse --show-toplevel
-Set-Location $PSScriptRoot/..
-cargo +stable clippy --fix --all -- -D warnings
-cargo +stable clippy --all -- -D warnings
-Set-Location $ROOT
+# $ROOT = git rev-parse --show-toplevel
+# Set-Location $PSScriptRoot/..
+# cargo +stable clippy --fix --all -- -D warnings
+# cargo +stable clippy --all -- -D warnings
+# Set-Location $ROOT

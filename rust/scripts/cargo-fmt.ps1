8581ae7ca35e52132d7d0e8dc2e7b36ed20b2ad2
Glatzel <893016099@qq.com>
2025-05-08 13:59:18 +0800

    fix

--- rust/scripts/cargo-fmt.ps1
+++ rust/scripts/cargo-fmt.ps1
@@ -1,8 +1,5 @@
-$ErrorActionPreference = "Stop"
-$PSNativeCommandUseErrorActionPreference = $true
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 & $PSScriptRoot/set-env.ps1
-cargo +nightly fmt --all
 cargo +nightly fmt --all --check
 Set-Location $ROOT

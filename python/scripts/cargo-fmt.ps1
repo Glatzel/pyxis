e22d26ddf882d7fbed3559fabdf6f78028cd937e
Glatzel <893016099@qq.com>
2025-05-08 17:45:15 +0800

    fix

--- python/scripts/cargo-fmt.ps1
+++ python/scripts/cargo-fmt.ps1
@@ -1,4 +1,4 @@
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
-cargo +nightly fmt --all
+cargo +nightly fmt --all -- --config-path $ROOT
 Set-Location $ROOT
--- rust/scripts/cargo-fmt.ps1
+++ rust/scripts/cargo-fmt.ps1
@@ -1,5 +1,5 @@
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 & $PSScriptRoot/set-env.ps1
-cargo +nightly fmt --all --check
+cargo +nightly fmt --all --check -- --config-path $ROOT
 Set-Location $ROOT

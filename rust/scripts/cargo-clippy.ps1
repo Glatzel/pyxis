dae3aaca03a1f962ad7539b6d9ebef61613f18df
Glatzel <chenzelin9794@gmail.com>
2025-05-11 01:00:26 +0800

    Update cargo-clippy.ps1

--- rust/scripts/cargo-clippy.ps1
+++ rust/scripts/cargo-clippy.ps1
@@ -1,5 +1,6 @@
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 & $PSScriptRoot/set-env.ps1
+$env:RUST_BACKTRACE=1
 pixi run -e all cargo +stable clippy --fix --all -- -D warnings
 Set-Location $ROOT

2508bec96e91fcb66a1d530d664d54c15afd2564
Glatzel <chenzelin9794@gmail.com>
2025-05-11 01:11:42 +0800

    Update build-rust-cuda.ps1

--- rust/scripts/build-rust-cuda.ps1
+++ rust/scripts/build-rust-cuda.ps1
@@ -5,6 +5,7 @@ param (
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 $env:RUST_BACKTRACE=1
+$env:CARGO_PROFILE_DEVELOP_BUILD_OVERRIDE_DEBUG="true"
 pixi run -e gpu cargo build --profile $config -p pyxis-cuda
 Set-Location $PSScriptRoot
 Set-Location $ROOT

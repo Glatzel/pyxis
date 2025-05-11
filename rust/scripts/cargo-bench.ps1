b4e931bfd9cbb5e250d1375f9ef17760ba6678fe
Glatzel <893016099@qq.com>
2025-05-11 17:12:50 +0800

    fix

--- rust/scripts/cargo-bench.ps1
+++ rust/scripts/cargo-bench.ps1
@@ -3,6 +3,6 @@ param($filter)
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 & $PSScriptRoot/set-env.ps1
-cargo bench -p pyxis -p pyxis-cuda -- $filter
+cargo bench --all -- $filter
 Set-Location $PSScriptRoot
 Set-Location $ROOT

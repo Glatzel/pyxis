3677ea189c2bec12f6c4aa493e7374627b82278a
Glatzel <893016099@qq.com>
2025-05-11 10:07:35 +0800

    fix

--- cuda/scripts/build-cuda.ps1
+++ cuda/scripts/build-cuda.ps1
@@ -15,6 +15,19 @@ $install = "-DCMAKE_INSTALL_PREFIX=$install"
 # create ptx output dir
 New-Item ./build/ptx -ItemType Directory -ErrorAction SilentlyContinue
 
+# find visual studio
+if (-not $env:CI) {
+    $path = pixi run vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+    if ($path) {
+        $path = join-path $path 'Common7\Tools\vsdevcmd.bat'
+        if (test-path $path) {
+            cmd /s /c """$path"" $args && set" | where { $_ -match '(\w+)=(.*)' } | foreach {
+                $null = new-item -force -path "Env:\$($Matches[1])" -value $Matches[2]
+            }
+        }
+    }
+}
+
 # build
 pixi run cmake -B build $install $config -DBUILD_CUDA=ON
 pixi run cmake --build build --target install

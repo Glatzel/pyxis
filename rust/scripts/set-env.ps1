593fcc923cb01b0e8b20e3e05397a5d2104640a2
Glatzel <chenzelin9794@gmail.com>
2025-05-11 13:36:51 +0800

    Update set-env.ps1

--- rust/scripts/set-env.ps1
+++ rust/scripts/set-env.ps1
@@ -18,8 +18,8 @@ if ($IsWindows) {
     }
 
     $pkg_config_exe = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/bin
-    $nvcc_path = Resolve-Path $PSScriptRoot/../.pixi/envs/gpu/Library/bin
-    $env:CUDA_ROOT = Resolve-Path $PSScriptRoot/../.pixi/envs/gpu/Library
+    $nvcc_path = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/bin
+    $env:CUDA_ROOT = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library
     $env:PATH = "$cl_path;$nvcc_path;$pkg_config_exe;$env:PATH"
     $env:PKG_CONFIG_PATH = Resolve-Path "./.pixi/envs/default/proj/x64-windows-static/lib/pkgconfig"
     Copy-Item ./.pixi/envs/default/proj/x64-windows-static/share/proj/proj.db ./crates/pyxis-cli/src/proj.db
@@ -32,8 +32,8 @@ if ($IsMacOS) {
 }
 if ($IsLinux) {
     $pkg_config_exe = Resolve-Path $PSScriptRoot/../.pixi/envs/default/bin
-    $nvcc_path = Resolve-Path $PSScriptRoot/../.pixi/envs/gpu/bin
-    $env:CUDA_LIBRARY_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/gpu
+    $nvcc_path = Resolve-Path $PSScriptRoot/../.pixi/envs/default/bin
+    $env:CUDA_LIBRARY_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default
     $env:PATH = "$nvcc_path" + ":" + "$pkg_config_exe" + ":" + "$env:PATH"
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-linux-release/lib/pkgconfig
     Copy-Item ./.pixi/envs/default/proj/x64-linux-release/share/proj/proj.db ./crates/pyxis-cli/src/proj.db

993582c69bb2093fd02a0d90f94b64c6fc234df6
Glatzel <893016099@qq.com>
2025-05-11 17:38:38 +0800

    fix

--- rust-cuda/scripts/cargo-clippy.ps1
+++ rust-cuda/scripts/cargo-clippy.ps1
@@ -1,6 +1,5 @@
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 & $PSScriptRoot/setup.ps1
-$env:RUST_BACKTRACE=1
 cargo +stable clippy --fix --all -- -D warnings
 Set-Location $ROOT

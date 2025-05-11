fcc45590093f9dbab6e778ea2980db023f199e48
Glatzel <893016099@qq.com>
2025-05-11 17:02:52 +0800

    fix

--- rust/scripts/set-env.ps1
+++ rust/scripts/set-env.ps1
@@ -8,23 +8,9 @@ else {
 }
 
 if ($IsWindows) {
-    # find visual studio
-    $path = pixi run vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
-    $cl_path = join-path $path 'VC\Tools\MSVC\14.43.34808\bin\Hostx64\x64;'
-    $env:INCLUDE = join-path $path  'VC\Tools\MSVC\14.43.34808\include'
-
-    # find cuda
-    $nvcc_path = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/bin
-    $lib_path = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/lib
-    $lib_path = "$lib_path;"
-    $env:CUDA_ROOT = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library
-
-    # pkg-config
     $pkg_config_exe = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/bin
-    $pkg_config_exe = "$pkg_config_exe;"
     $env:PKG_CONFIG_PATH = Resolve-Path "./.pixi/envs/default/proj/x64-windows-static/lib/pkgconfig"
-
-    $env:PATH = "$env:PATH" + "$lib_path" + "$cl_path" + "$nvcc_path;$pkg_config_exe"
+    $env:PATH = "$pkg_config_exe;$env:PATH"
     Copy-Item ./.pixi/envs/default/proj/x64-windows-static/share/proj/proj.db ./crates/pyxis-cli/src/proj.db
 }
 if ($IsMacOS) {
@@ -35,9 +21,7 @@ if ($IsMacOS) {
 }
 if ($IsLinux) {
     $pkg_config_exe = Resolve-Path $PSScriptRoot/../.pixi/envs/default/bin
-    $nvcc_path = Resolve-Path $PSScriptRoot/../.pixi/envs/default/bin
-    $env:CUDA_LIBRARY_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default
-    $env:PATH = "$nvcc_path" + ":" + "$pkg_config_exe" + ":" + "$env:PATH"
+    $env:PATH = "$pkg_config_exe" + ":" + "$env:PATH"
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-linux-release/lib/pkgconfig
     Copy-Item ./.pixi/envs/default/proj/x64-linux-release/share/proj/proj.db ./crates/pyxis-cli/src/proj.db
 }

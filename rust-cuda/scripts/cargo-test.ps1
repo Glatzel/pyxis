559ad47e2a66dd1f93346423ca384942beb3483e
Glatzel <893016099@qq.com>
2025-05-11 17:54:46 +0800

    fix

--- rust-cuda/scripts/cargo-test.ps1
+++ rust-cuda/scripts/cargo-test.ps1
@@ -4,12 +4,11 @@ Set-Location $PSScriptRoot/..
 & $PSScriptRoot/setup.ps1
 pixi run cargo +nightly llvm-cov nextest --no-report --all --branch --no-fail-fast
 $code = $LASTEXITCODE
-pixi run cargo +nightly llvm-cov --no-report --all $package --branch --no-fail-fast --doc
+pixi run cargo +nightly llvm-cov --no-report --all --branch --no-fail-fast --doc
 $code = $code + $LASTEXITCODE
 cargo +nightly llvm-cov report
 cargo +nightly llvm-cov report --lcov --output-path lcov.info
 $code = $code + $LASTEXITCODE
-Copy-Item ./target/lcov.info ./
 if ($code -ne 0) {
     Write-Host "Test failed." -ForegroundColor Red
 }

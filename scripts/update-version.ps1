6c5a91a8e7188a89070258a93ea3f5ce9c7f6383
Glatzel <chenzelin9794@gmail.com>
2025-05-12 08:03:22 +0800

    feat(rust): inital proj network (#364)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- scripts/update-version.ps1
+++ scripts/update-version.ps1
@@ -1,38 +1,46 @@
 Set-Location $PSScriptRoot/..
-$version= "0.0.19"
+$version = "0.0.20"
 
-# Update cpp version
+# cpp
 $cmakeListsPath = "./cpp/CMakeLists.txt"
 (Get-Content -Path $cmakeListsPath) -replace 'project\(.* VERSION .*', "project(PyxisCppProject VERSION $version)" | Set-Content -Path $cmakeListsPath
-Write-Host "Updated CPP version to $version"
+Write-Host "Updated cpp version to $version"
 
 
-# Update csharp version
+# csharp
 $csconfig = "./csharp/Directory.Build.props"
 (Get-Content -Path $csconfig) -replace '<Version>.*</Version>', "<Version>$version</Version>" | Set-Content -Path $csconfig
 Write-Host "Updated csharp version to $version"
 
-# Update cuda version
+# cuda
 $cmakeListsPath = "./cuda/CMakeLists.txt"
 (Get-Content -Path $cmakeListsPath) -replace 'project\(.* VERSION .*', "project(PyxisCppProject VERSION $version)" | Set-Content -Path $cmakeListsPath
 Write-Host "Updated cuda version to $version"
 
-# Update python version
+# python
 $cargoTomlPath = "./python/Cargo.toml"
 (Get-Content -Path $cargoTomlPath) -replace '^version = .*', "version = `"$version`"" | Set-Content -Path $cargoTomlPath
 Write-Host "Updated python version to $version"
 
-# Update the version in Cargo.toml
-$cargoTomlPath = "./rust/Cargo.toml"
-(Get-Content -Path $cargoTomlPath) -replace '^version = .*', "version = `"$version`"" | Set-Content -Path $cargoTomlPath
-Write-Host "Updated Rust version to $version"
-
-# Update python rattler version
 $recipe_path = "./python/rattler/recipe.yaml"
 (Get-Content -Path $recipe_path) -replace '^  version: .*', "  version: $version" | Set-Content -Path $recipe_path
-Write-Host "Updated ratter cli version to $version"
+Write-Host "Updated python rattler version to $version"
 
-# Update rust rattler version
+# python-cuda
+$recipe_path = "./python-cuda/rattler/recipe.yaml"
+(Get-Content -Path $recipe_path) -replace '^  version: .*', "  version: $version" | Set-Content -Path $recipe_path
+Write-Host "Updated python-cuda rattler version to $version"
+
+# rust
+$cargoTomlPath = "./rust/Cargo.toml"
+(Get-Content -Path $cargoTomlPath) -replace '^version = .*', "version = `"$version`"" | Set-Content -Path $cargoTomlPath
+Write-Host "Updated rust version to $version"
+
 $recipe_path = "./rust/rattler/recipe.yaml"
 (Get-Content -Path $recipe_path) -replace '^  version: .*', "  version: $version" | Set-Content -Path $recipe_path
-Write-Host "Updated ratter py version to $version"
+Write-Host "Updated rust ratter version to $version"
+
+# rust-cuda
+$cargoTomlPath = "./rust-cuda/Cargo.toml"
+(Get-Content -Path $cargoTomlPath) -replace '^version = .*', "version = `"$version`"" | Set-Content -Path $cargoTomlPath
+Write-Host "Updated rust-cuda version to $version"

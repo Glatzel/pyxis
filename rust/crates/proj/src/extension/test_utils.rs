0da62f649f0898f5c5bf15ea1f2efe81c89a3621
Glatzel <chenzelin9794@gmail.com>
2025-05-12 17:36:32 +0800

    fix(rust): clerk null macro (#371)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- rust/crates/proj/src/extension/test_utils.rs
+++ rust/crates/proj/src/extension/test_utils.rs
@@ -3,6 +3,7 @@ use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
 use crate::data_types::PjLogLevel;
+
 pub(crate) fn new_test_ctx() -> miette::Result<crate::PjContext> {
     tracing_subscriber::registry()
         .with(clerk::terminal_layer(LevelFilter::TRACE, true))

5b51b46b5cb80cf1bbae46621f3be2b3f45a66bc
Glatzel <893016099@qq.com>
2025-05-12 22:39:10 +0800

    fix

--- rust/crates/pyxis/benches/datum_compensate.rs
+++ rust/crates/pyxis/benches/datum_compensate.rs
@@ -3,7 +3,7 @@ use criterion::{Criterion, black_box, criterion_group, criterion_main};
 fn bench_datum_compensate(c: &mut Criterion) {
     let processor = pyxis::DatumCompensate::new(400.0, 6_378_137.0, 500_000.0, 0.0);
     c.bench_function("datum_compensate", |b| {
-        b.iter(|| processor.transfrom(black_box(469704.6693f64), black_box(2821940.796f64)))
+        b.iter(|| processor.transform(black_box(469704.6693f64), black_box(2821940.796f64)))
     });
 }
 

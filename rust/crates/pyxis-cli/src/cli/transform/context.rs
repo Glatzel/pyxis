24b11feda26f27c3bfc013e4057a6c9b263e42ef
Glatzel <893016099@qq.com>
2025-05-12 22:38:50 +0800

    fix

--- rust/crates/pyxis-cli/src/cli/transform/context.rs
+++ rust/crates/pyxis-cli/src/cli/transform/context.rs
@@ -51,7 +51,7 @@ impl ContextTransform {
 
     pub fn datum_compensate(&mut self, hb: f64, r: f64, x0: f64, y0: f64) {
         let processor = pyxis::DatumCompensate::new(hb, r, x0, y0);
-        (self.x, self.y) = processor.datum_compensate(self.x, self.y);
+        (self.x, self.y) = processor.transfrom(self.x, self.y);
     }
     pub fn migrate2d(
         &mut self,
--- rust/crates/pyxis/src/datum_compensate.rs
+++ rust/crates/pyxis/src/datum_compensate.rs
@@ -1,5 +1,32 @@
 use crate::GeoFloat;
-
+/// Converts projected XY coordinates from the height compensation plane to
+/// the sea level plane.
+///
+/// # Arguments
+///
+/// - `xc`, `yc`: Coordinates on the height compensation plane (in meters).
+/// - `hb`: Elevation of the height compensation plane (in meters).
+/// - `radius`: Radius of the Earth (in meters).
+/// - `x0`, `y0`: Coordinate system origin (in meters).
+///
+/// # Returns
+///
+/// A tuple containing the projected XY coordinates of the sea level plane
+/// (in meters).
+///
+///
+/// # References
+/// - 杨元兴. (2008). 抵偿高程面的选择与计算. 城市勘测 (02), 72-74.
+///
+/// # Examples
+/// ```
+/// use float_cmp::assert_approx_eq;
+/// let p = (469704.6693, 2821940.796);
+/// let processor = pyxis::DatumCompensate::new(400.0, 6_378_137.0, 500_000.0, 0.0);
+/// let p = processor.transfrom(p.0, p.1);
+/// assert_approx_eq!(f64, p.0, 469706.56912942487, epsilon = 1e-17);
+/// assert_approx_eq!(f64, p.1, 2821763.831232311, epsilon = 1e-17);
+/// ```
 pub struct DatumCompensate<T: GeoFloat> {
     x0: T,
     y0: T,
@@ -11,35 +38,8 @@ impl<T: GeoFloat> DatumCompensate<T> {
         let factor = ratio / (T::ONE + ratio);
         Self { x0, y0, factor }
     }
-    /// Converts projected XY coordinates from the height compensation plane to
-    /// the sea level plane.
-    ///
-    /// # Arguments
-    ///
-    /// - `xc`, `yc`: Coordinates on the height compensation plane (in meters).
-    /// - `hb`: Elevation of the height compensation plane (in meters).
-    /// - `radius`: Radius of the Earth (in meters).
-    /// - `x0`, `y0`: Coordinate system origin (in meters).
-    ///
-    /// # Returns
-    ///
-    /// A tuple containing the projected XY coordinates of the sea level plane
-    /// (in meters).
-    ///
-    ///
-    /// # References
-    /// - 杨元兴. (2008). 抵偿高程面的选择与计算. 城市勘测 (02), 72-74.
-    ///
-    /// # Examples
-    /// ```
-    /// use float_cmp::assert_approx_eq;
-    /// let p = (469704.6693, 2821940.796);
-    /// let processor = pyxis::DatumCompensate::new(400.0, 6_378_137.0, 500_000.0, 0.0);
-    /// let p = processor.datum_compensate(p.0, p.1);
-    /// assert_approx_eq!(f64, p.0, 469706.56912942487, epsilon = 1e-17);
-    /// assert_approx_eq!(f64, p.1, 2821763.831232311, epsilon = 1e-17);
-    /// ```
-    pub fn datum_compensate(&self, xc: T, yc: T) -> (T, T)
+
+    pub fn transfrom(&self, xc: T, yc: T) -> (T, T)
     where
         T: GeoFloat,
     {

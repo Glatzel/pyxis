373b08b1cc4af5da929229c9b5dee003d91274d9
Glatzel <chenzelin9794@gmail.com>
2025-05-12 22:55:08 +0800

    refactor(python,rust,rust-cuda): datum compensate (#372)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- rust/crates/pyxis/src/datum_compensate.rs
+++ rust/crates/pyxis/src/datum_compensate.rs
@@ -1,28 +1,6 @@
 use crate::GeoFloat;
-pub trait IDatumCompensateParams<T: GeoFloat> {
-    fn x0(&self) -> T;
-    fn y0(&self) -> T;
-    fn factor(&self) -> T;
-}
-pub struct DatumCompensateParams<T: GeoFloat> {
-    x0: T,
-    y0: T,
-    factor: T,
-}
-impl<T: GeoFloat> DatumCompensateParams<T> {
-    pub fn new(hb: T, radius: T, x0: T, y0: T) -> Self {
-        let ratio = hb / radius;
-        let factor = ratio / (T::ONE + ratio);
-        Self { x0, y0, factor }
-    }
-}
-impl<T: GeoFloat> IDatumCompensateParams<T> for DatumCompensateParams<T> {
-    fn x0(&self) -> T { self.x0 }
-    fn y0(&self) -> T { self.y0 }
-    fn factor(&self) -> T { self.factor }
-}
-/// Converts projected XY coordinates from the height compensation plane to the
-/// sea level plane.
+/// Converts projected XY coordinates from the height compensation plane to
+/// the sea level plane.
 ///
 /// # Arguments
 ///
@@ -33,8 +11,8 @@ impl<T: GeoFloat> IDatumCompensateParams<T> for DatumCompensateParams<T> {
 ///
 /// # Returns
 ///
-/// A tuple containing the projected XY coordinates of the sea level plane (in
-/// meters).
+/// A tuple containing the projected XY coordinates of the sea level plane
+/// (in meters).
 ///
 ///
 /// # References
@@ -44,16 +22,29 @@ impl<T: GeoFloat> IDatumCompensateParams<T> for DatumCompensateParams<T> {
 /// ```
 /// use float_cmp::assert_approx_eq;
 /// let p = (469704.6693, 2821940.796);
-/// let params = pyxis::DatumCompensateParams::new(400.0, 6_378_137.0, 500_000.0, 0.0);
-/// let p = pyxis::datum_compensate(p.0, p.1, &params);
+/// let processor = pyxis::DatumCompensate::new(400.0, 6_378_137.0, 500_000.0, 0.0);
+/// let p = processor.transform(p.0, p.1);
 /// assert_approx_eq!(f64, p.0, 469706.56912942487, epsilon = 1e-17);
 /// assert_approx_eq!(f64, p.1, 2821763.831232311, epsilon = 1e-17);
 /// ```
-pub fn datum_compensate<T>(xc: T, yc: T, params: &impl IDatumCompensateParams<T>) -> (T, T)
-where
-    T: GeoFloat,
-{
-    let xc = xc - params.factor() * (xc - params.x0());
-    let yc = yc - params.factor() * (yc - params.y0());
-    (xc, yc)
+pub struct DatumCompensate<T: GeoFloat> {
+    x0: T,
+    y0: T,
+    factor: T,
+}
+impl<T: GeoFloat> DatumCompensate<T> {
+    pub fn new(hb: T, radius: T, x0: T, y0: T) -> Self {
+        let ratio = hb / radius;
+        let factor = ratio / (T::ONE + ratio);
+        Self { x0, y0, factor }
+    }
+
+    pub fn transform(&self, xc: T, yc: T) -> (T, T)
+    where
+        T: GeoFloat,
+    {
+        let xc = xc - self.factor * (xc - self.x0);
+        let yc = yc - self.factor * (yc - self.y0);
+        (xc, yc)
+    }
 }

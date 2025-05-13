4dee4ba59c1e038d8de5ba4ad281608163f28d75
Glatzel <893016099@qq.com>
2025-05-13 12:29:17 +0800

    fix

--- python-cuda/scripts/build-cuda.ps1
+++ python-cuda/scripts/build-cuda.ps1
@@ -1,8 +1,7 @@
 $ROOT = git rev-parse --show-toplevel
-$install=Resolve-Path  $PSScriptRoot/../src/pyxis_cuda
-& $ROOT/cuda/scripts/build-cuda.ps1 -install $install
 Set-Location $PSScriptRoot/..
 Remove-Item ./src/pyxis_cuda/ptx -Recurse -Force -ErrorAction SilentlyContinue
 New-Item ./src/pyxis_cuda/ptx -ItemType Directory -ErrorAction SilentlyContinue
-Copy-Item $ROOT/cuda/dist/ptx/* ./src/pyxis_cuda/ptx/
+$install = Resolve-Path  $PSScriptRoot/../src/pyxis_cuda
+& $ROOT/cuda/scripts/build-cuda.ps1 -install $install
 Set-Location $ROOT

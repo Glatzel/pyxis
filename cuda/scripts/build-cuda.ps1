19d4d8ac09b42636a5b5664f4eb4618ba7532e69
Glatzel <893016099@qq.com>
2025-05-13 13:08:22 +0800

    fix

--- cuda/scripts/build-cuda.ps1
+++ cuda/scripts/build-cuda.ps1
@@ -6,7 +6,6 @@ Set-Location $PSScriptRoot/..
 $config = "-DCMAKE_BUILD_TYPE=Release"
 
 # create install dir
-Remove-Item $install -Recurse -ErrorAction SilentlyContinue
 New-Item $install -ItemType Directory -ErrorAction SilentlyContinue
 $install = Resolve-Path $install
 $install = "$install".Replace('\', '/')
@@ -17,12 +16,12 @@ Remove-Item ./build -Recurse -Force -ErrorAction SilentlyContinue
 New-Item ./build/ptx -ItemType Directory -ErrorAction SilentlyContinue
 
 # build
-if ($install -eq "./dist") {
-    if ($IsWindows) { pixi run cmake -G "Visual Studio 17 2022" -B build $install $config -DBUILD_CUDA=ON }
-    else { pixi run cmake -B build $install $config -DBUILD_CUDA=ON }
-    pixi run cmake --build build --target install
+if ($IsWindows) { pixi run cmake -G "Visual Studio 17 2022" -B build $install $config -DBUILD_CUDA=ON }
+else { pixi run cmake -B build $install $config -DBUILD_CUDA=ON }
+pixi run cmake --build build --target install
 
-    # pack output files
+# pack output files
+if ($install -eq "./dist") {
     if ($IsWindows) {
         7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on `
             "./dist/pyxis-cuda-windows-x64.7z" "./dist/"

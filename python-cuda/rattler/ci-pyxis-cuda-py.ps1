6d6bd8e65e4bc8b9e7c5118257156fe931acef00
Glatzel <893016099@qq.com>
2025-05-13 12:23:23 +0800

    fix

--- python-cuda/rattler/ci-pyxis-cuda-py.ps1
+++ python-cuda/rattler/ci-pyxis-cuda-py.ps1
@@ -5,6 +5,10 @@ param (
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 $ROOT = git rev-parse --show-toplevel
+
+Set-Location $PSScriptRoot
+& "$PSScriptRoot/../scripts/build-cuda.ps1"
+
 Set-Location $PSScriptRoot
 pixi run rattler-build build
 Set-Location $ROOT

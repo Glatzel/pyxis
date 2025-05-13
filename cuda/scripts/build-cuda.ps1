881f77d64e771c10bfa9b6ef9cfe39cc0bd7d8c9
Glatzel <893016099@qq.com>
2025-05-13 12:45:32 +0800

    fix

--- cuda/scripts/build-cuda.ps1
+++ cuda/scripts/build-cuda.ps1
@@ -17,16 +17,18 @@ Remove-Item ./build -Recurse -Force -ErrorAction SilentlyContinue
 New-Item ./build/ptx -ItemType Directory -ErrorAction SilentlyContinue
 
 # build
-if ($IsWindows) { pixi run cmake -G "Visual Studio 17 2022" -B build $install $config -DBUILD_CUDA=ON }
-else { pixi run cmake -B build $install $config -DBUILD_CUDA=ON }
-pixi run cmake --build build --target install
+if ($install -eq "./dist") {
+    if ($IsWindows) { pixi run cmake -G "Visual Studio 17 2022" -B build $install $config -DBUILD_CUDA=ON }
+    else { pixi run cmake -B build $install $config -DBUILD_CUDA=ON }
+    pixi run cmake --build build --target install
 
-# pack output files
-if ($IsWindows) {
-    7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on `
-        "./dist/pyxis-cuda-windows-x64.7z" "./dist/"
-}if ($IsLinux) {
-    7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on `
-        "./dist/pyxis-cuda-linux-x64.7z" "./dist/"
+    # pack output files
+    if ($IsWindows) {
+        7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on `
+            "./dist/pyxis-cuda-windows-x64.7z" "./dist/"
+    }if ($IsLinux) {
+        7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on `
+            "./dist/pyxis-cuda-linux-x64.7z" "./dist/"
+    }
 }
 Set-Location $ROOT

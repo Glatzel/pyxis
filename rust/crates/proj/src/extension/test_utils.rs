9a89d106e26da49d72d5e9e95aca40368bd64346
Glatzel <893016099@qq.com>
2025-05-13 13:15:39 +0800

    fix

--- rust/crates/proj/src/extension/test_utils.rs
+++ rust/crates/proj/src/extension/test_utils.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::path::PathBuf;
 
+use miette::IntoDiagnostic;
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
@@ -15,28 +16,29 @@ pub(crate) fn new_test_ctx() -> miette::Result<crate::PjContext> {
     ctx.set_log_level(PjLogLevel::Trace)?;
     // PROJ_DATA
     let workspace_root = env::var("CARGO_WORKSPACE_DIR").unwrap();
-    let default_proj_data = match env::var("CARGO_CFG_TARGET_OS").unwrap().as_str() {
-        "windows" => dunce::canonicalize(format!(
+    let default_proj_data = if cfg!(target_os = "windows") {
+        dunce::canonicalize(format!(
             "{workspace_root}/.pixi/envs/default/proj/x64-windows-static/share/proj"
         ))
-        .unwrap()
+        .into_diagnostic()?
         .to_string_lossy()
-        .to_string(),
-        "linux" => dunce::canonicalize(format!(
+        .to_string()
+    } else if cfg!(target_os = "linux") {
+        dunce::canonicalize(format!(
             "{workspace_root}/.pixi/envs/default/proj/x64-linux-release/share/proj"
         ))
-        .unwrap()
+        .into_diagnostic()?
         .to_string_lossy()
-        .to_string(),
-        "macos" => dunce::canonicalize(format!(
+        .to_string()
+    } else if cfg!(target_os = "macos") {
+        dunce::canonicalize(format!(
             "{workspace_root}/.pixi/envs/default/proj/arm64-osx-release/share/proj"
         ))
-        .unwrap()
+        .into_diagnostic()?
         .to_string_lossy()
-        .to_string(),
-        other => {
-            panic!("Unsupported OS: {}", other)
-        }
+        .to_string()
+    } else {
+        panic!("Unsupported OS")
     };
     ctx.set_search_paths(&[&PathBuf::from(default_proj_data)])?;
     Ok(ctx)

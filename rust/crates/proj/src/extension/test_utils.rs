cbbb2cca26fd928af092ff4e429748ece6780a20
Glatzel <893016099@qq.com>
2025-05-13 12:42:24 +0800

    fix

--- rust/crates/proj/src/extension/test_utils.rs
+++ rust/crates/proj/src/extension/test_utils.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
@@ -10,5 +12,30 @@ pub(crate) fn new_test_ctx() -> miette::Result<crate::PjContext> {
         .init();
     let ctx = crate::PjContext::default();
     ctx.set_log_level(PjLogLevel::Trace)?;
+    // PROJ_DATA
+    let default_proj_data = match env::var("CARGO_CFG_TARGET_OS").unwrap().as_str() {
+        "windows" => {
+            dunce::canonicalize("../../.pixi/envs/default/proj/x64-windows-static/share/proj")
+                .unwrap()
+                .to_string_lossy()
+                .to_string()
+        }
+        "linux" => {
+            dunce::canonicalize("../../.pixi/envs/default/proj/x64-linux-release/share/proj")
+                .unwrap()
+                .to_string_lossy()
+                .to_string()
+        }
+        "macos" => {
+            dunce::canonicalize("../../.pixi/envs/default/proj/arm64-osx-release/share/proj")
+                .unwrap()
+                .to_string_lossy()
+                .to_string()
+        }
+        other => {
+            panic!("Unsupported OS: {}", other)
+        }
+    };
+    ctx.set_search_paths(&[&PathBuf::from(default_proj_data)]);
     Ok(ctx)
 }

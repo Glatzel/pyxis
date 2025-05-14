66741b1f31eca7880ef5f0e1614e9b4b97283f43
Glatzel <893016099@qq.com>
2025-05-14 10:47:51 +0800

    f

--- rust-cuda/build.rs
+++ rust-cuda/build.rs
@@ -18,7 +18,7 @@ fn main() {
             .to_string();
 
         let cl_paths =
-            glob("C:\\Program Files\\Microsoft Visual Studio\\2022\\*/VC\\Tools\\MSVC\\*\\bin\\Hostx64\\x64")
+            glob("C:\\Program Files\\Microsoft Visual Studio\\2022\\*\\VC\\Tools\\MSVC\\*\\bin\\Hostx64\\x64")
                 .expect("Failed to read glob pattern")
                 .filter_map(Result::ok)
                 .collect::<Vec<std::path::PathBuf>>();

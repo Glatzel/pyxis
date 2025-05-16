3b23578259cc4ac401df73b06a6433140fe12b16
Glatzel <893016099@qq.com>
2025-05-16 11:36:42 +0800

    f

--- rattler/build-pyxis.ps1
+++ rattler/build-pyxis.ps1
@@ -1,11 +1,11 @@
-New-Item $env:PREFIX/bin/pyxis -ItemType Directory -ErrorAction SilentlyContinue
-New-Item $env:PREFIX/bin/pyxis/bin -ItemType Directory -ErrorAction SilentlyContinue
+New-Item $env:PREFIX/pyxis -ItemType Directory -ErrorAction SilentlyContinue
+New-Item $env:PREFIX/pyxis/bin -ItemType Directory -ErrorAction SilentlyContinue
 $ROOT = git rev-parse --show-toplevel
 if ($IsWindows) {
-    Copy-Item "$ROOT/rust/dist/cli/pyxis.exe" "$env:PREFIX/bin/pyxis/pyxis.exe"
+    Copy-Item "$ROOT/rust/dist/cli/pyxis.exe" "$env:PREFIX/pyxis/bin/pyxis.exe"
 }
 if ($IsLinux -or $IsMacOS) {
-    Copy-Item "$ROOT/rust/dist/cli/pyxis" "$env:PREFIX/bin/pyxis/pyxis"
+    Copy-Item "$ROOT/rust/dist/cli/pyxis" "$env:PREFIX/pyxis/bin/pyxis"
 }
-Copy-Item "$ROOT/cpp/dist/*" "$env:PREFIX/bin/pyxis/pyxis/" -Recurse
-Copy-Item "$ROOT/cuda/dist/*" "$env:PREFIX/bin/pyxis/pyxis/" -Recurse
+Copy-Item "$ROOT/cpp/dist/*" "$env:PREFIX/pyxis" -Recurse
+Copy-Item "$ROOT/cuda/dist/*" "$env:PREFIX/pyxis" -Recurse

210ac9dc78d4bbc89c973774845ce3fbaedbcfef
Glatzel <893016099@qq.com>
2025-05-28 21:26:06 +0800

    ff

--- rust/scripts/cargo-clippy.ps1
+++ rust/scripts/cargo-clippy.ps1
@@ -1,5 +1,3 @@
-$ErrorActionPreference = "Stop"
-$PSNativeCommandUseErrorActionPreference = $true
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 cargo +stable clippy --fix --all -- -Dwarnings

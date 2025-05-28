1d5df87ae16f21feac0d7be5ba92a6a1c0b826f2
Glatzel <893016099@qq.com>
2025-05-28 21:46:18 +0800

    f

--- pre-commit/pre-commit.ps1
+++ pre-commit/pre-commit.ps1
@@ -4,7 +4,7 @@ if ($update) {
     pixi run --manifest-path ./pre-commit/pixi.toml pre-commit autoupdate
 }
 if ($v) {
-    pixi run --manifest-path ./pre-commit/pixi.toml pre-commit-run --color=always --show-diff-on-failure -v --debug
+    pixi run --manifest-path ./pre-commit/pixi.toml pre-commit-run --color=always --show-diff-on-failure -v
 }
 else {
     pixi run --manifest-path ./pre-commit/pixi.toml pre-commit-run --color=always --show-diff-on-failure

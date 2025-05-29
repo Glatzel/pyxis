0deb8c43338285eb6f8ce76d2c0a459219669a88
Glatzel <893016099@qq.com>
2025-05-29 18:00:39 +0800

    f

--- rust/crates/proj/src/data_types/list_structures.rs
+++ rust/crates/proj/src/data_types/list_structures.rs
@@ -1,30 +1,38 @@
 crate::create_readonly_struct!(
     Operations,
+    "Description a PROJ operation"
+    "# References"
     "<https://proj.org/en/stable/development/reference/datatypes.html#c.PJ_OPERATIONS>",
-    {id: String},
+    {id: String,"Operation keyword."},
     // PJ *(*proj)(PJ *);
-    {descr: String}
+    {descr: String,"Description of operation."}
 );
 crate::create_readonly_struct!(
     Ellps,
+    "Description of ellipsoids defined in PROJ"
+    "# References"
     "<https://proj.org/en/stable/development/reference/datatypes.html#c.PJ_OPERATIONS>",
-    {id: String},
-    {major: String},
-    {ell: String},
-    {name: String}
+    {id: String,"Keyword name of the ellipsoid."},
+    {major: String,"Semi-major axis of the ellipsoid, or radius in case of a sphere."},
+    {ell: String,"Elliptical parameter, e.g. `rf=298.257` or `b=6356772.2`."},
+    {name: String,"Name of the ellipsoid"}
 );
 crate::create_readonly_struct!(
     Units,
+    "Distance units defined in PROJ."
+    "# References"
     "<https://proj.org/en/stable/development/reference/datatypes.html#c.PJ_OPERATIONS>",
-    {id: String},
-    {to_meter: String},
-    {name: String},
-    {factor: f64}
+    {id: String,"Keyword for the unit."},
+    {to_meter: String,"Text representation of the factor that converts a given unit to meters"},
+    {name: String,"Name of the unit."},
+    {factor: f64,"Conversion factor that converts the unit to meters."}
 );
 
 crate::create_readonly_struct!(
     PrimeMeridians,
+    "Hard-coded prime meridians defined in PROJ. Note that the structure is no longer updated, and some values may conflict with other sources."
+    "# References"
     "<https://proj.org/en/stable/development/reference/datatypes.html#c.PJ_OPERATIONS>",
-    {id: String},
-    {defn: String}
+    {id: String,"Keyword for the prime meridian"},
+    {defn: String,"Offset from Greenwich in DMS format."}
 );

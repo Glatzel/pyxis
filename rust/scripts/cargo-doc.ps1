f29e1cbd769d67ecb3c6ceddca000bef598ccfd3
Glatzel <893016099@qq.com>
2025-05-29 18:12:56 +0800

    f

--- rust/scripts/cargo-doc.ps1
+++ rust/scripts/cargo-doc.ps1
@@ -3,7 +3,7 @@ $PSNativeCommandUseErrorActionPreference = $true
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 $env:RUSTDOCFLAGS="--html-in-header katex.html"
-cargo doc --no-deps --all-features -p pyxis -p proj
+cargo doc --no-deps --all-features -p pyxis -p proj --Dwarnings
 
 Remove-Item ./dist/rust-doc.7z -Force -ErrorAction SilentlyContinue
 New-Item ./dist -ItemType Directory -ErrorAction SilentlyContinue

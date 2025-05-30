dc56e6db166cbd86d5c3994597afb35d0d2b50ae
Glatzel <893016099@qq.com>
2025-05-30 14:59:14 +0800

    f

--- python/scripts/pytest.ps1
+++ python/scripts/pytest.ps1
@@ -1,7 +1,6 @@
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 
-# only run cuda test in local machine
 $python_root = Resolve-Path $PSScriptRoot/..
 $env:PYTHONPATH = "$env:PYTHONPATH;$python_root"
 

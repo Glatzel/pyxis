e20d709c8664c7574499e24fd6adaad2bf5553b9
Glatzel <893016099@qq.com>
2025-05-30 15:32:51 +0800

    f

--- rust-cuda/scripts/setup.ps1
+++ rust-cuda/scripts/setup.ps1
@@ -3,6 +3,7 @@ pixi install
 
 if ($IsWindows) {
     $env:CUDA_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library
+    $env:LIB = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/lib
 }
 if ($IsLinux) {
     $env:CUDA_LIBRARY_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default

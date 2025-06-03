1d52345f8467f5a2daa4b8fefc925917f8f8865a
Glatzel <chenzelin9794@gmail.com>
2025-06-03 20:00:50 +0800

    test(rust): add iso19111 advanced functions test (#455)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- rust/crates/proj/src/functions/logging.rs
+++ rust/crates/proj/src/functions/logging.rs
@@ -6,7 +6,7 @@ impl crate::Context {
     ///
     /// # References
     /// * <https://proj.org/en/stable/development/reference/functions.html#c.proj_log_level>
-    pub fn _log_level(&self) { unimplemented!("Use other function to instead.") }
+    fn _log_level(&self) { unimplemented!("Use other function to instead.") }
     /// # See Also
     ///
     /// * [`Self::set_log_fn`]

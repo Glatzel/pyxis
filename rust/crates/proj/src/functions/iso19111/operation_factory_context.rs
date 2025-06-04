1a1a9d8a2a91494a15ee0a41fda883edaa277f52
Glatzel <893016099@qq.com>
2025-06-04 08:22:31 +0800

    f

--- rust/crates/proj/src/functions/iso19111/operation_factory_context.rs
+++ rust/crates/proj/src/functions/iso19111/operation_factory_context.rs
@@ -21,52 +21,61 @@ impl Context {
 impl OperationFactoryContext<'_> {
     ///# References
     ///
-    /// <>
-    pub fn operation_factory_context_set_desired_accuracy(&self) { todo!() }
+    /// <https://proj.org/en/stable/development/reference/functions.html#c.proj_operation_factory_context_set_desired_accuracy>
+    pub fn operation_factory_context_set_desired_accuracy(&self, accuracy: f64) -> &Self {
+        unsafe {
+            proj_sys::proj_operation_factory_context_set_desired_accuracy(
+                self.ctx.ptr,
+                self.ptr,
+                accuracy,
+            );
+        }
+        self
+    }
     ///# References
     ///
     /// <>
-    pub fn operation_factory_context_set_area_of_interest(&self) { todo!() }
+    pub fn operation_factory_context_set_area_of_interest(&self) -> &Self { todo!() }
     ///# References
     ///
     /// <>
-    pub fn operation_factory_context_set_area_of_interest_name(&self) { todo!() }
+    pub fn operation_factory_context_set_area_of_interest_name(&self) -> &Self { todo!() }
     ///# References
     ///
     /// <>
-    pub fn operation_factory_context_set_crs_extent_use(&self) { todo!() }
+    pub fn operation_factory_context_set_crs_extent_use(&self) -> &Self { todo!() }
     ///# References
     ///
     /// <>
-    pub fn operation_factory_context_set_spatial_criterion(&self) { todo!() }
+    pub fn operation_factory_context_set_spatial_criterion(&self) -> &Self { todo!() }
     ///# References
     ///
     /// <>
-    pub fn operation_factory_context_set_grid_availability_use(&self) { todo!() }
+    pub fn operation_factory_context_set_grid_availability_use(&self) -> &Self { todo!() }
     ///# References
     ///
     /// <>
-    pub fn operation_factory_context_set_use_proj_alternative_grid_names(&self) { todo!() }
+    pub fn operation_factory_context_set_use_proj_alternative_grid_names(&self) -> &Self { todo!() }
     ///# References
     ///
     /// <>
-    pub fn operation_factory_context_set_allow_use_intermediate_crs(&self) { todo!() }
+    pub fn operation_factory_context_set_allow_use_intermediate_crs(&self) -> &Self { todo!() }
     ///# References
     ///
     /// <>
-    pub fn operation_factory_context_set_allowed_intermediate_crs(&self) { todo!() }
+    pub fn operation_factory_context_set_allowed_intermediate_crs(&self) -> &Self { todo!() }
     ///# References
     ///
     /// <>
-    pub fn operation_factory_context_set_discard_superseded(&self) { todo!() }
+    pub fn operation_factory_context_set_discard_superseded(&self) -> &Self { todo!() }
     ///# References
     ///
     /// <>
-    pub fn operation_factory_context_set_allow_ballpark_transformations(&self) { todo!() }
+    pub fn operation_factory_context_set_allow_ballpark_transformations(&self) -> &Self { todo!() }
     ///# References
     ///
     /// <>
-    pub fn create_operations(&self) { todo!() }
+    pub fn create_operations(&self) -> &Self { todo!() }
 }
 impl Drop for OperationFactoryContext<'_> {
     fn drop(&mut self) {

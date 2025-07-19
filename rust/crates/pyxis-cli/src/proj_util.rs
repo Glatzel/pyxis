bd9649c4b360cf802371da11479931042d7be3fb
Glatzel <893016099@qq.com>
2025-07-19 10:20:44 +0800

    分

--- rust/crates/pyxis-cli/src/proj_util.rs
+++ rust/crates/pyxis-cli/src/proj_util.rs
@@ -8,25 +8,6 @@ use proj::Context;
 // const PROJ_DB: &[u8] = include_bytes!(concat!(env!("PROJ_DATA"), "/proj.db"));
 pub fn init_proj_builder() -> miette::Result<Arc<Context>> {
     let ctx = proj::Context::new();
-
-    // setup logging
     ctx.set_log_level(proj::LogLevel::Trace)?;
-
-    // // search for proj.db
-    // if let Ok(proj_data) = std::env::var("PROJ_DATA") {
-    //     clerk::info!("PROJ_DATA environment variable is found: {proj_data}");
-    //     ctx.set_search_paths(&[&PathBuf::from(proj_data)])?;
-    // } else {
-    //     clerk::debug!("PROJ_DATA environment variable is not found");
-    //     let exe_path = std::env::current_exe().into_diagnostic()?;
-    //     let exe_root = exe_path.parent().unwrap();
-    //     if !exe_root.join("proj.db").exists() {
-    //         clerk::warn!("proj.db is not found. Try to use bundled proj.db");
-    //         let mut db_file = std::fs::File::create(exe_root.join("proj.db")).into_diagnostic()?;
-    //         db_file.write_all(PROJ_DB).into_diagnostic()?;
-    //         clerk::info!("Write to: {}", exe_root.join("proj.db").to_str().unwrap());
-    //     }
-    //     ctx.set_search_paths(&[exe_root])?;
-    // }
     Ok(ctx)
 }

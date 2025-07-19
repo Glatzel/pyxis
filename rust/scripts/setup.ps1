f3de06f25291fe458f838a7a2972e37eab244814
Glatzel <893016099@qq.com>
2025-07-19 20:20:57 +0800

    f

--- rust/scripts/setup.ps1
+++ rust/scripts/setup.ps1
@@ -2,13 +2,13 @@ Set-Location $PSScriptRoot/..
 pixi install
 if ($IsWindows) {
     $env:PROJ_LIB_DIR = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-windows-static/lib
-    $env:PROJ_INCLUDE_DIR = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-windows-static/include/proj
+    $env:PROJ_INCLUDE_DIR = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-windows-static/include
 }
 if ($IsMacOS) {
     $env:PROJ_LIB_DIR = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-osx-release/lib
-    $env:PROJ_INCLUDE_DIR = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-osx-release/include/proj
+    $env:PROJ_INCLUDE_DIR = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-osx-release/include
 }
 if ($IsLinux) {
     $env:PROJ_LIB_DIR = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-linux-release/lib
-    $env:PROJ_INCLUDE_DIR = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-linux-release/include/proj
+    $env:PROJ_INCLUDE_DIR = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-linux-release/include
 }

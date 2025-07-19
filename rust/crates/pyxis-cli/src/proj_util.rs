99d31a33329820896974a9d7d849dde74273cfb9
Glatzel <893016099@qq.com>
2025-07-19 10:20:54 +0800

    f

--- rust/crates/pyxis-cli/src/proj_util.rs
+++ rust/crates/pyxis-cli/src/proj_util.rs
@@ -5,7 +5,6 @@ use std::sync::Arc;
 use miette::IntoDiagnostic;
 use proj::Context;
 
-// const PROJ_DB: &[u8] = include_bytes!(concat!(env!("PROJ_DATA"), "/proj.db"));
 pub fn init_proj_builder() -> miette::Result<Arc<Context>> {
     let ctx = proj::Context::new();
     ctx.set_log_level(proj::LogLevel::Trace)?;

4fb8c37cefaf4bf5438b05ea9df369abdec6b3d6
Glatzel <893016099@qq.com>
2025-07-20 13:46:12 +0800

    use pixi

--- rust/scripts/cross-build-linux-aarch64.ps1
+++ rust/scripts/cross-build-linux-aarch64.ps1
@@ -1,20 +1,13 @@
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
-rustup target add aarch64-unknown-linux-gnu
-Set-Location $PSScriptRoot/..
-sudo apt-get update
-sudo apt-get install --assume-yes curl git build-essential cmake pkg-config zip unzip tar qemu-user-static g++-aarch64-linux-gnu cmake ninja-build
+if ($IsLinux) {
+  pixi global install proj -c https://repo.prefix.dev/glatzel --platform linux-aarch64
 
-# Install vcpkg
-git clone https://github.com/microsoft/vcpkg.git
-Set-Location ./vcpkg && ./bootstrap-vcpkg.sh
-
-# Use vcpkg to install proj for aarch64
-./vcpkg install proj --triplet arm64-linux-release
-
-# Set PKG_CONFIG_PATH to vcpkg's pkgconfig directory
-$p = resolve-path ./installed/arm64-linux-release/lib/pkgconfig
-$env:PKG_CONFIG_PATH = "$p" + ":" + "$env:PKG_CONFIG_PATH"
-$env:PKG_CONFIG_ALLOW_CROSS = 1
-Set-Location ..
-cargo build --target aarch64-unknown-linux-gnu --all-features
+  # Set PKG_CONFIG_PATH to vcpkg's pkgconfig directory
+  $p = resolve-path ~/.pixi/envs/proj/proj/arm64-linux-release/lib/pkgconfig
+  $env:PKG_CONFIG_PATH = "$p" + ":" + "$env:PKG_CONFIG_PATH"
+  $env:PKG_CONFIG_ALLOW_CROSS = 1
+  Set-Location ..
+  cargo build --target aarch64-unknown-linux-gnu --all-features
+}
+else { exit 1 }

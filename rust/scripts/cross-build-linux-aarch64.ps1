24ffbeadcff7e5b1b701ec3f6bcfd162036fbb9c
Glatzel <893016099@qq.com>
2025-07-20 11:23:27 +0800

    更新 cross-build-linux-aarch64.ps1

--- rust/scripts/cross-build-linux-aarch64.ps1
+++ rust/scripts/cross-build-linux-aarch64.ps1
@@ -5,13 +5,14 @@ Set-Location $PSScriptRoot/..
  sudo  apt-get install --assume-yes curl git build-essential cmake pkg-config zip unzip tar qemu-user-static g++-aarch64-linux-gnu cmake ninja-build
 
   # Install vcpkg
-  git clone https://github.com/microsoft/vcpkg.git /vcpkg
-  cd /vcpkg && ./bootstrap-vcpkg.sh
+  git clone https://github.com/microsoft/vcpkg.git
+  cd ./vcpkg && ./bootstrap-vcpkg.sh
 
   # Use vcpkg to install proj for aarch64
-  cd /vcpkg && ./vcpkg install proj --triplet arm64-linux-release
+   ./vcpkg install proj --triplet arm64-linux-release
 
   # Set PKG_CONFIG_PATH to vcpkg's pkgconfig directory
-  export PKG_CONFIG_PATH=/vcpkg/installed/arm64-linux-release/lib/pkgconfig:$PKG_CONFIG_PATH
-
+$p=resolve-path ./installed/arm64-linux-release/lib/pkgconfig
+  $env:PKG_CONFIG_PATH="$p"+":"+"$env:PKG_CONFIG_PATH"
+cd ..
 cargo build --target aarch64-unknown-linux-gnu --all-features

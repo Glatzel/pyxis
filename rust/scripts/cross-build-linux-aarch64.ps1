25361c1d8fe7a2f8e7a159b7ac32830866dc0bb8
Glatzel <893016099@qq.com>
2025-07-20 10:12:03 +0800

    f

--- rust/scripts/cross-build-linux-aarch64.ps1
+++ rust/scripts/cross-build-linux-aarch64.ps1
@@ -2,4 +2,4 @@ $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 Set-Location $PSScriptRoot/..
 cargo install cross
-cross build --target aarch64-unknown-linux-gnu --all-features -v
+cross build --target aarch64-unknown-linux-gnu --all-features

60f112fa1792c0d09982b1c784951f1e0e5025b3
Glatzel <893016099@qq.com>
2025-07-20 13:48:33 +0800

    f

--- rust/scripts/cross-build-linux-aarch64.ps1
+++ rust/scripts/cross-build-linux-aarch64.ps1
@@ -1,13 +1,14 @@
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 if ($IsLinux) {
+  Set-Location $PSScriptRoot/..
   pixi global install proj -c https://repo.prefix.dev/glatzel --platform linux-aarch64
 
   # Set PKG_CONFIG_PATH to vcpkg's pkgconfig directory
   $p = resolve-path ~/.pixi/envs/proj/proj/arm64-linux-release/lib/pkgconfig
   $env:PKG_CONFIG_PATH = "$p" + ":" + "$env:PKG_CONFIG_PATH"
   $env:PKG_CONFIG_ALLOW_CROSS = 1
-  Set-Location ..
+  
   cargo build --target aarch64-unknown-linux-gnu --all-features
 }
 else { exit 1 }

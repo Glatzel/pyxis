0b3e2af68e53a4905b9fc8af515034cbe2c7ae59
Glatzel <893016099@qq.com>
2025-07-23 10:47:05 +0800

    f

--- rust/scripts/setup.ps1
+++ rust/scripts/setup.ps1
@@ -10,8 +10,12 @@ if ($IsMacOS) {
 }
 Write-Output $(uname -m)
 if ($IsLinux -and ($(uname -m) -eq 'x86_64' )) {
+    rustup install stable-x86_64-unknown-linux-musl
+    rustup default stable-x86_64-unknown-linux-musl
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-linux-release/lib/pkgconfig
 }
 if ($IsLinux -and ($(uname -m) -eq 'aarch64' )) {
+    rustup install stable-aarch-unknown-linux-musl
+    rustup default stable-aarch-unknown-linux-musl
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-linux-release/lib/pkgconfig
 }

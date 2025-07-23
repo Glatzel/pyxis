c4403d56acea0fa893dea25c479a03cdd71ff87f
Glatzel <893016099@qq.com>
2025-07-23 08:52:47 +0800

    f

--- rust/scripts/cross-build-linux-aarch64.ps1
+++ rust/scripts/cross-build-linux-aarch64.ps1
@@ -5,10 +5,10 @@ if ($IsLinux) {
   sudo apt-get update
   sudo apt-get install -y g++-aarch64-linux-gnu
   rustup target add aarch64-unknown-linux-gnu
-  pixi global install proj
+  pixi install
 
   # Set PKG_CONFIG_PATH to vcpkg's pkgconfig directory
-  $p = resolve-path ~/.pixi/envs/proj/proj/arm64-linux-release/lib/pkgconfig
+  $p = resolve-path .pixi/envs/proj/proj/arm64-linux-release/lib/pkgconfig
   $env:PKG_CONFIG_PATH = "$p" + ":" + "$env:PKG_CONFIG_PATH"
   $env:PKG_CONFIG_ALLOW_CROSS = 1
   cargo build --target aarch64-unknown-linux-gnu --all-features

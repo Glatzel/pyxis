72c22c97774774d723ca7247d0b6c612103bc7c8
Glatzel <893016099@qq.com>
2025-07-23 11:37:51 +0800

    f

--- rust/rattler/ci-pyxis-cli.ps1
+++ rust/rattler/ci-pyxis-cli.ps1
@@ -9,11 +9,13 @@ if ($IsWindows -or $IsMacOS) {
     cargo build --release -p pyxis-cli
 }
 if ($IsLinux -and ($(uname -m) -eq 'x86_64' )) {
+    $env:PKG_CONFIG_ALLOW_CROSS = 1
     sudo apt install musl-tools
     rustup target add x86_64-unknown-linux-musl
     cargo build --release -p pyxis-cli --target x86_64-unknown-linux-musl
 }
 if ($IsLinux -and ($(uname -m) -eq 'aarch64' )) {
+    $env:PKG_CONFIG_ALLOW_CROSS = 1
     sudo apt install musl-tools
     rustup target add aarch64-unknown-linux-musl
     cargo build --release -p pyxis-cli --target aarch64-unknown-linux-musl

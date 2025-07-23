310a0936df4412879e317a4a16fb1d5001404ce3
Glatzel <893016099@qq.com>
2025-07-23 09:48:07 +0800

    f

--- rust/rattler/build-pyxis-cli.ps1
+++ rust/rattler/build-pyxis-cli.ps1
@@ -1,13 +1,8 @@
 New-Item $env:PREFIX/pyxis -ItemType Directory -ErrorAction SilentlyContinue
 New-Item $env:PREFIX/pyxis/bin -ItemType Directory -ErrorAction SilentlyContinue
-$ROOT = git rev-parse --show-toplevel
 if ($IsWindows) {
-    Copy-Item "$ROOT/rust/dist/cli/pyxis.exe" "$env:PREFIX/pyxis/bin/pyxis.exe"
+    Copy-Item $PSScriptRoot/dist/cli/pyxis.exe "$env:PREFIX/pyxis/bin/pyxis.exe"
 }
-if ($IsLinux -or $IsMacOS) {
-    Copy-Item "$ROOT/rust/dist/cli/pyxis" "$env:PREFIX/pyxis/bin/pyxis"
-}
-Copy-Item "$ROOT/cpp/dist/*" "$env:PREFIX/pyxis/" -Recurse
-if (-not $IsMacOS) {
-    Copy-Item "$ROOT/cuda/dist/*" "$env:PREFIX/pyxis/" -Recurse
+else {
+    Copy-Item $PSScriptRoot/dist/cli/pyxis "$env:PREFIX/pyxis/bin/pyxis"
 }

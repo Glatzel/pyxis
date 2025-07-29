601153e190765c44d41d416a6d60a22566522092
Glatzel <893016099@qq.com>
2025-07-29 18:03:12 +0800

    f

--- rust/crates/pyxis-cli/src/utils.rs
+++ rust/crates/pyxis-cli/src/utils.rs
@@ -9,23 +9,25 @@ pub fn init_proj_builder() -> miette::Result<Arc<Context>> {
 }
 #[cfg(debug_assertions)]
 pub fn start_deadlock_detection() {
-    tokio::task::spawn_blocking(|| {
-        loop {
-            std::thread::sleep(std::time::Duration::from_secs(10));
-            let deadlocks = parking_lot::deadlock::check_deadlock();
-            if deadlocks.is_empty() {
-                continue;
-            }
-
-            clerk::error!("{} deadlocks detected", deadlocks.len());
-            for (i, threads) in deadlocks.iter().enumerate() {
-                let mut msg = format!("Deadlock #{}\n", i);
-                for t in threads {
-                    msg.push_str(format!("Thread Id {:#?}\n", t.thread_id()).as_str());
-                    msg.push_str(format!("{:#?}", t.backtrace()).as_str());
+    if std::env::var("CI").is_err() {
+        tokio::task::spawn_blocking(|| {
+            loop {
+                std::thread::sleep(std::time::Duration::from_secs(10));
+                let deadlocks = parking_lot::deadlock::check_deadlock();
+                if deadlocks.is_empty() {
+                    continue;
                 }
-                clerk::error!("{}", msg);
+
+                clerk::error!("{} deadlocks detected", deadlocks.len());
+                for (i, threads) in deadlocks.iter().enumerate() {
+                    let mut msg = format!("Deadlock #{}\n", i);
+                    for t in threads {
+                        msg.push_str(format!("Thread Id {:#?}\n", t.thread_id()).as_str());
+                        msg.push_str(format!("{:#?}", t.backtrace()).as_str());
+                    }
+                    clerk::error!("{}", msg);
+                }
             }
-        }
-    });
+        });
+    }
 }

d4f647dc76f20423e1cd69e400feb48806db59d7
Glatzel <893016099@qq.com>
2025-07-30 15:58:50 +0800

    f

--- rust/crates/pyxis-cli/src/utils.rs
+++ rust/crates/pyxis-cli/src/utils.rs
@@ -11,29 +11,26 @@ pub fn init_proj_builder() -> miette::Result<Arc<Context>> {
 }
 #[cfg(debug_assertions)]
 pub fn start_deadlock_detection(shutdown_rx: watch::Receiver<()>) {
-    // Skip in CI
-    if std::env::var("CI").is_err() {
-        tokio::task::spawn_blocking(move || {
-            loop {
-                if shutdown_rx.has_changed().is_ok() {
-                    break;
-                }
-                std::thread::sleep(std::time::Duration::from_secs(10));
-                let deadlocks = parking_lot::deadlock::check_deadlock();
-                if deadlocks.is_empty() {
-                    continue;
-                }
-
-                clerk::error!("{} deadlocks detected", deadlocks.len());
-                for (i, threads) in deadlocks.iter().enumerate() {
-                    let mut msg = format!("Deadlock #{i}\n");
-                    for t in threads {
-                        msg.push_str(format!("Thread Id {:#?}\n", t.thread_id()).as_str());
-                        msg.push_str(format!("{:#?}", t.backtrace()).as_str());
-                    }
-                    clerk::error!("{}", msg);
-                }
+    tokio::task::spawn_blocking(move || {
+        loop {
+            if shutdown_rx.has_changed().is_ok() {
+                break;
             }
-        });
-    }
+            std::thread::sleep(std::time::Duration::from_secs(10));
+            let deadlocks = parking_lot::deadlock::check_deadlock();
+            if deadlocks.is_empty() {
+                continue;
+            }
+
+            clerk::error!("{} deadlocks detected", deadlocks.len());
+            for (i, threads) in deadlocks.iter().enumerate() {
+                let mut msg = format!("Deadlock #{i}\n");
+                for t in threads {
+                    msg.push_str(format!("Thread Id {:#?}\n", t.thread_id()).as_str());
+                    msg.push_str(format!("{:#?}", t.backtrace()).as_str());
+                }
+                clerk::error!("{}", msg);
+            }
+        }
+    });
 }

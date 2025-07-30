478d5cc85ad9dc99c095b91bfd7f1127fed547a7
Glatzel <893016099@qq.com>
2025-07-30 16:11:47 +0800

    f

--- rust/rattler/ci-pyxis-cli.ps1
+++ rust/rattler/ci-pyxis-cli.ps1
@@ -3,7 +3,7 @@ $PSNativeCommandUseErrorActionPreference = $true
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 & ./scripts/setup.ps1
-cargo build --bins --release
+cargo build --bin pyxis --release
 Set-Location $PSScriptRoot
 pixi run rattler-build build
 Set-Location $ROOT

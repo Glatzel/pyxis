4570f5580bec804de1279a4073820a1e6725146d
Glatzel <893016099@qq.com>
2025-07-31 15:34:11 +0800

    f

--- rust/crates/pyxis-cli/tests/test_example.rs
+++ rust/crates/pyxis-cli/tests/test_example.rs
@@ -1,24 +1,68 @@
-use std::path::PathBuf;
-
 use assert_cmd::Command;
+use assert_cmd::assert::Assert;
+use miette::IntoDiagnostic;
+fn print_output(cmd: &Assert) {
+    let output = cmd.get_output();
+    let stdout = String::from_utf8_lossy(output.stdout.as_slice());
+    let stderr = String::from_utf8_lossy(output.stderr.as_slice());
 
-pub fn get_example_exe(name: &str) -> PathBuf {
-    let root = PathBuf::from(env!("CARGO_WORKSPACE_DIR"));
-    let mut exe = root.clone();
-    if cfg!(windows) {
-        exe.push(format!("target/llvm-cov-target/debug/examples/{name}.exe"));
+    // Print results
+    if output.status.success() {
+        println!("Output:\n{stdout}");
     } else {
-        exe.push(format!("target/llvm-cov-target/debug/examples/{name}"));
+        eprintln!("Error:\n{stderr}");
     }
-    exe
 }
 #[test]
-fn test_bayer_image() {
-    let exe = get_example_exe("jiaxing_bump_station");
-    Command::new(exe).assert().success();
+fn test_jiaxing_bump_station() -> miette::Result<()> {
+    let cmd = Command::cargo_bin("pyxis")
+        .into_diagnostic()?
+        .args([
+            "transform",
+            "-n",
+            "Jiaxing bump station",
+            "-x",
+            "121.091701",
+            "-y",
+            "30.610765",
+            "-z",
+            "0",
+            "-o",
+            "json",
+        ])
+        .args(["crypto", "--from", "wgs84", "--to", "gcj02"])
+        .assert()
+        .success();
+    print_output(&cmd);
+    Ok(())
 }
 #[test]
-fn test_zhengyong_expressway_dehua_east_interchange() {
-    let exe = get_example_exe("zhengyong_expressway_dehua_east_interchange");
-    Command::new(exe).assert().success();
+fn test_zhengyong_expressway_dehua_east_interchange() -> miette::Result<()> {
+    let cmd = Command::cargo_bin("pyxis")
+        .into_diagnostic()?
+    .args([
+            "-v",
+            "transform",
+            "-n", "Zhengyong expressway Dehua east interchange",
+            "-x", "469704.6693",
+            "-y", "2821940.796",
+            "-z", "0",
+            "-o", "plain",
+        ])
+        .args([
+            "datum-compensate",
+            "--hb", "400",
+            "-r", "6378137",
+            "--x0", "500000",
+            "--y0", "0",
+        ])
+        .args([
+            "proj",
+            "--from", "+proj=tmerc +lat_0=0 +lon_0=118.5 +k=1 +x_0=500000 +y_0=0 +ellps=GRS80 +units=m +no_defs +type=crs",
+            "--to", "+proj=longlat +datum=WGS84 +no_defs +type=crs",
+        ])
+        .assert()
+        .success();
+    print_output(&cmd);
+    Ok(())
 }

b1c082b182b160983a91e086e5fb4be285e1bc4e
Glatzel <chenzelin9794@gmail.com>
2025-07-31 15:55:16 +0800

    chore: Update list_devices.rs

--- rust/crates/pyxis-cli/tools/list_devices.rs
+++ rust/crates/pyxis-cli/tools/list_devices.rs
@@ -1,8 +1,6 @@
-use clerk::LogLevel;
 use rax::device::{DeviceFilter, list_devices};
 
 fn main() -> miette::Result<()> {
-    clerk::init_log_with_level(LogLevel::TRACE);
     let devices = list_devices(DeviceFilter::all)?;
     println!("{} devices found.", devices.len());
     for d in devices {

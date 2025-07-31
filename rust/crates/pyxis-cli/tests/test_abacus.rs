f5e60e2d1d3df9ced498bc913bd950d0daa57265
Glatzel <chenzelin9794@gmail.com>
2025-07-31 16:35:41 +0800

    chore: test subfolder (#569)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

---
+++ rust/crates/pyxis-cli/tests/test_abacus.rs
@@ -0,0 +1,12 @@
+mod abacus {
+    mod test_command_chain;
+    mod test_crypto;
+    mod test_datum_compensate;
+    mod test_migrate2d;
+    mod test_output;
+    mod test_proj;
+    mod test_rotate;
+    mod test_scale;
+    mod test_space;
+    mod test_translate;
+}

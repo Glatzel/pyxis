afd484b8380c91bd5b36cd0e97bc1834b1bc877b
Glatzel <chenzelin9794@gmail.com>
2025-08-18 18:08:57 +0800

    chore: update configuration files synced file(s) with Glatzel/template (#598)
    synced local file(s) with
    [Glatzel/template](https://github.com/Glatzel/template).
    
    
    
    <details>
    <summary>Changed files</summary>
    <ul>
    <li>synced local directory <code>./pre-commit/</code> with remote
    directory <code>./pre-commit/</code></li>
    </ul>
    </details>
    
    ---
    
    This PR was created automatically by the
    [repo-file-sync-action](https://github.com/BetaHuhn/repo-file-sync-action)
    workflow run
    [#17037449744](https://github.com/Glatzel/template/actions/runs/17037449744)

--- pre-commit/cargo-fmt.ps1
+++ pre-commit/cargo-fmt.ps1
@@ -1,8 +1,9 @@
 # This File is automatically synchronized from https://github.com/Glatzel/template
 
-
-rustup toolchain install nightly --profile=minimal
-rustup component add rustfmt --toolchain nightly
+if ($env:CI) {
+    rustup toolchain install nightly --profile=minimal
+    rustup component add rustfmt --toolchain nightly
+}
 
 $ROOT = git rev-parse --show-toplevel
 Set-Location $ROOT

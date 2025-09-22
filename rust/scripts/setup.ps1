8f41297551fb9ce2cc352527568b5f76249e448e
Glatzel <893016099@qq.com>
2025-09-22 10:23:56 +0800

    f

--- rust/scripts/setup.ps1
+++ rust/scripts/setup.ps1
@@ -3,16 +3,16 @@ pixi install
 if ($IsWindows) {
     $bin = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/bin
     $env:Path = "$bin" + ";" + "$env:Path"
-    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-windows-static/lib/pkgconfig
+    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/lib/pkgconfig
 }
 elseif ($IsMacOS) {
-    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-osx-release/lib/pkgconfig
+    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib/pkgconfig
 }
 elseif ($IsLinux -and ($(uname -m) -eq 'x86_64' )) {
     sudo apt install -y libudev-dev libc6-dev
-    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-linux-release/lib/pkgconfig
+    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib/pkgconfig
 }
 elseif ($IsLinux -and ($(uname -m) -eq 'aarch64' )) {
     sudo apt install -y libudev-dev libc6-dev
-    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-linux-release/lib/pkgconfig
+    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib/pkgconfig
 }

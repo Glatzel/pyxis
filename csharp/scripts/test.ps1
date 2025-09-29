f30e14de1d6e7d7fd4e5528e44d0bcd5fb7dea27
Glatzel <chenzelin9794@gmail.com>
2025-09-30 07:39:27 +0800

    feat: remove csharp and opencl (#665)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- csharp/scripts/test.ps1
+++
@@ -1,7 +0,0 @@
-$ROOT = git rev-parse --show-toplevel
-Set-Location $PSScriptRoot
-Set-Location ..
-dotnet test --logger:junit --collect:"XPlat Code Coverage"
-try { dotnet-coverage -h }
-catch { dotnet tool install --global dotnet-coverage }
-Set-Location $ROOT

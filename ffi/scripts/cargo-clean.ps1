8a2f1a862567a851c6c86747e79e2ebb4b115c4e
Glatzel <chenzelin9794@gmail.com>
2025-10-01 10:00:36 +0800

    chore: update configuration files synced file(s) with Glatzel/template (#668)
    synced local file(s) with
    [Glatzel/template](https://github.com/Glatzel/template).
    
    
    
    <details>
    <summary>Changed files</summary>
    <ul>
    <li>created local directory <code>./ffi</code> and copied all sub
    files/folders from remote directory <code>./rust</code></li>
    </ul>
    </details>
    
    ---
    
    This PR was created automatically by the
    [repo-file-sync-action](https://github.com/BetaHuhn/repo-file-sync-action)
    workflow run
    [#18148498978](https://github.com/Glatzel/template/actions/runs/18148498978)

---
+++ ffi/scripts/cargo-clean.ps1
@@ -0,0 +1,6 @@
+# This File is automatically synchronized from https://github.com/Glatzel/template
+
+$ROOT = git rev-parse --show-toplevel
+Set-Location $PSScriptRoot/..
+cargo clean
+Set-Location $ROOT

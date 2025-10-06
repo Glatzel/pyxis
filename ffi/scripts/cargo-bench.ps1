d8cee4a78428bac22a469244584fe7bb5e460dd8
Glatzel <chenzelin9794@gmail.com>
2025-10-06 11:22:51 +0800

    chore: Delete ffi directory

--- ffi/scripts/cargo-bench.ps1
+++
@@ -1,9 +0,0 @@
-# This File is automatically synchronized from https://github.com/Glatzel/template
-
-$config = if (-not $args) { '--workspace' } else { $args }
-if (Test-Path $PSScriptRoot/setup.ps1) { &$PSScriptRoot/setup.ps1 }
-$ROOT = git rev-parse --show-toplevel
-Set-Location $PSScriptRoot/..
-cargo bench @config
-Set-Location $PSScriptRoot
-Set-Location $ROOT

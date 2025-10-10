20c515c53adce30fff167f646c759dc67f4014e5
Glatzel <893016099@qq.com>
2025-10-10 12:05:05 +0800

    更新 setup.ps1

--- rust/scripts/setup.ps1
+++ rust/scripts/setup.ps1
@@ -9,6 +9,7 @@ elseif ($IsMacOS) {
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-osx-release/lib/pkgconfig
 }
 elseif ($IsLinux -and ($(uname -m) -eq 'x86_64' )) {
+    sudo apt update
     sudo apt install -y libudev-dev libc6-dev
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-linux-release/lib/pkgconfig
 }

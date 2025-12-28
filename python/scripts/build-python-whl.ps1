eba8e3ea70600cc75f4810f09e980b94110dd696
Glatzel <chenzelin9794@gmail.com>
2025-12-28 22:06:13 +0800

    feat: build package in demon forge (#740)

--- python/scripts/build-python-whl.ps1
+++ python/scripts/build-python-whl.ps1
@@ -1,13 +1,5 @@
-param (
-    [ValidateSet("develop","release")]
-    $config = "develop"
-)
-$ROOT = git rev-parse --show-toplevel
-
 Set-Location $PSScriptRoot/..
-Remove-Item $ROOT/dist/pyxis*.whl -ErrorAction SilentlyContinue
+Remove-Item ./dist/pyxis*.whl -ErrorAction SilentlyContinue
 Remove-Item ./pyxis/pyxis_py.pyd -ErrorAction SilentlyContinue
 Remove-Item ./pyxis/**__pycache__ -Recurse -ErrorAction SilentlyContinue
-pixi run maturin build --out ./dist --profile $config
-
-Set-Location $ROOT
+pixi run maturin build --out ./dist --profile release
--- python/scripts/maturin-develop.ps1
+++ python/scripts/maturin-develop.ps1
@@ -1,12 +1,8 @@
-param (
-    [ValidateSet("develop","release")]
-    $config = "develop"
-)
 $ROOT = git rev-parse --show-toplevel
 
 Set-Location $PSScriptRoot/..
 Remove-Item pyxis/pyxis.pyd -ErrorAction SilentlyContinue
 
-pixi run maturin develop --profile $config
+pixi run maturin develop
 
 Set-Location $ROOT

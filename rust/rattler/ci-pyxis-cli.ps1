25baed7f42a8d930d0a593e3c593d50c27d72ed9
Glatzel <chenzelin9794@gmail.com>
2025-12-28 21:31:30 +0800

    feat: build package in demon forge

--- rust/rattler/ci-pyxis-cli.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = "Stop"
-$PSNativeCommandUseErrorActionPreference = $true
-$ROOT = git rev-parse --show-toplevel
-Set-Location $PSScriptRoot/..
-& ./scripts/setup.ps1
-cargo build --bin pyxis --release
-Set-Location $PSScriptRoot
-pixi run rattler-build build
-Set-Location $ROOT

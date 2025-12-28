f19ed4affe2b2c5b7077f9b4d1467992bb5c77d7
Glatzel <chenzelin9794@gmail.com>
2025-12-28 21:43:01 +0800

    f

--- python/scripts/build-python-whl.ps1
+++ python/scripts/build-python-whl.ps1
@@ -1,9 +1,5 @@
-$ROOT = git rev-parse --show-toplevel
-
 Set-Location $PSScriptRoot/..
-Remove-Item $ROOT/dist/pyxis*.whl -ErrorAction SilentlyContinue
+Remove-Item ./dist/pyxis*.whl -ErrorAction SilentlyContinue
 Remove-Item ./pyxis/pyxis_py.pyd -ErrorAction SilentlyContinue
 Remove-Item ./pyxis/**__pycache__ -Recurse -ErrorAction SilentlyContinue
 pixi run maturin build --out ./dist --profile release
-
-Set-Location $ROOT

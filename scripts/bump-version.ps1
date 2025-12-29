67316dfbd58024ef8f59c576c7be25725207a145
Glatzel <chenzelin9794@gmail.com>
2025-12-29 19:17:00 +0800

    f

--- scripts/bump-version.ps1
+++ scripts/bump-version.ps1
@@ -6,15 +6,7 @@ $cargoTomlPath = "./python/Cargo.toml"
 (Get-Content -Path $cargoTomlPath) -replace '^version = .*', "version = `"$version`"" | Set-Content -Path $cargoTomlPath
 Write-Host "Updated python version to $version"
 
-$recipe_path = "./python/rattler/recipe.yaml"
-(Get-Content -Path $recipe_path) -replace '^  version: .*', "  version: $version" | Set-Content -Path $recipe_path
-Write-Host "Updated python rattler version to $version"
-
 # rust
 $cargoTomlPath = "./rust/Cargo.toml"
 (Get-Content -Path $cargoTomlPath) -replace '^version = .*', "version = `"$version`"" | Set-Content -Path $cargoTomlPath
 Write-Host "Updated rust version to $version"
-
-$recipe_path = "./rust/rattler/recipe.yaml"
-(Get-Content -Path $recipe_path) -replace '^  version: .*', "  version: $version" | Set-Content -Path $recipe_path
-Write-Host "Updated rust ratter version to $version"

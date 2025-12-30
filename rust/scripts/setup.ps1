932bb5c8917f7fa01245a75431744931b71b07c6
Glatzel <chenzelin9794@gmail.com>
2025-12-30 19:53:10 +0800

    build: move apt command from script to ci

--- rust/scripts/setup.ps1
+++ rust/scripts/setup.ps1
@@ -9,14 +9,10 @@ elseif ($IsMacOS) {
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-osx-release/lib/pkgconfig
 }
 elseif ($IsLinux -and ($(uname -m) -eq 'x86_64' )) {
-    sudo apt update
-    sudo apt install -y libudev-dev libc6-dev
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-linux-release/lib/pkgconfig
     $env:PKG_CONFIG_PATH = "/usr/lib/x86_64-linux-gnu/pkgconfig`:${env:PKG_CONFIG_PATH}"
 }
 elseif ($IsLinux -and ($(uname -m) -eq 'aarch64' )) {
-    sudo apt update
-    sudo apt install -y libudev-dev libc6-dev
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-linux-release/lib/pkgconfig
     $env:PKG_CONFIG_PATH = "/usr/lib/aarch64-linux-gnu/pkgconfig`:${env:PKG_CONFIG_PATH}"
 }

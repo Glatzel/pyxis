1dc9c6e9cc9aafb262ef0a1184d6a4b3b31e4619
Glatzel <chenzelin9794@gmail.com>
2026-01-03 12:46:12 +0800

    chore: update build script (#745)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- rust/scripts/setup.ps1
+++ rust/scripts/setup.ps1
@@ -5,14 +5,14 @@ if ($IsWindows) {
     $env:Path = "$bin" + ";" + "$env:Path"
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-windows-static/lib/pkgconfig
 }
-elseif ($IsMacOS) {
+if ($IsMacOS) {
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-osx-release/lib/pkgconfig
 }
-elseif ($IsLinux -and ($(uname -m) -eq 'x86_64' )) {
+if ($IsLinux -and ($(uname -m) -eq 'x86_64' )) {
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-linux-release/lib/pkgconfig
     $env:PKG_CONFIG_PATH = "/usr/lib/x86_64-linux-gnu/pkgconfig`:${env:PKG_CONFIG_PATH}"
 }
-elseif ($IsLinux -and ($(uname -m) -eq 'aarch64' )) {
+if ($IsLinux -and ($(uname -m) -eq 'aarch64' )) {
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-linux-release/lib/pkgconfig
     $env:PKG_CONFIG_PATH = "/usr/lib/aarch64-linux-gnu/pkgconfig`:${env:PKG_CONFIG_PATH}"
 }

073317e20270ea258f7190db0d8865e63d2677a0
Glatzel <chenzelin9794@gmail.com>
2026-01-17 21:01:23 +0800

    f

--- rust/scripts/cargo-test.ps1
+++ rust/scripts/cargo-test.ps1
@@ -1,7 +1,7 @@
 # This File is automatically synchronized from https://github.com/Glatzel/template
 
 $config = if ($args.Count) { $args } else { @('--workspace', '--all-features') }
-if (Test-Path $PSScriptRoot/setup.ps1) { &$PSScriptRoot/setup.ps1 }
+if (Test-Path $PSScriptRoot/setup.ps1) { . $PSScriptRoot/setup.ps1 }
 if ($env:CI) { pixi global install cargo-llvm-cov cargo-nextest }
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..

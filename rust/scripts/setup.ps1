9255f33b3901b5090c65841092509a3424167a92
Glatzel <chenzelin9794@gmail.com>
2026-01-17 21:00:48 +0800

    f

---
+++ rust/scripts/setup.ps1
@@ -0,0 +1,19 @@
+Set-Location $PSScriptRoot/..
+pixi install
+if ($IsWindows) {
+    $bin = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/bin
+    $env:Path = "$bin" + ";" + "$env:Path"
+    $env:PKG_CONFIG_PATH = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/lib/pkgconfig);${env:PKG_CONFIG_PATH}"
+}
+if ($IsMacOS) {
+    $env:Path = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib)`:$env:Path"
+    $env:PKG_CONFIG_PATH = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib/pkgconfig)`:${env:PKG_CONFIG_PATH}"
+}
+if ($IsLinux -and ($(uname -m) -eq 'x86_64' )) {
+    $env:Path = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib)`:$env:Path"
+    $env:PKG_CONFIG_PATH = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib/pkgconfig)`:/usr/lib/x86_64-linux-gnu/pkgconfig`:${env:PKG_CONFIG_PATH}"
+}
+if ($IsLinux -and ($(uname -m) -eq 'aarch64' )) {
+    $env:Path = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib)`:$env:Path"
+    $env:PKG_CONFIG_PATH = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib/pkgconfig)`:/usr/lib/aarch64-linux-gnu/pkgconfig`:${env:PKG_CONFIG_PATH}"
+}

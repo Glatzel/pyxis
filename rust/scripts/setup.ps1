3a36f8ff20f96049ff069258e6105fe093791a13
Glatzel <chenzelin9794@gmail.com>
2026-01-17 22:21:08 +0800

    f

--- rust/scripts/setup.ps1
+++ rust/scripts/setup.ps1
@@ -6,7 +6,7 @@ if ($IsWindows) {
 }
 if ($IsMacOS) {
     $env:Path = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib)`:$env:Path"
-    $env:LD_LIBRARY_PATH = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib)`:$env:LD_LIBRARY_PATH"
+    $env:DYLD_LIBRARY_PATH = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib)`:$env:DYLD_LIBRARY_PATH"
     $env:PROJ_ROOT = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default)"
 }
 if ($IsLinux -and ($(uname -m) -eq 'x86_64' )) {

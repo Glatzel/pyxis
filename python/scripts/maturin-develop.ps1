a177413c46f3a6c11ce7e4678d2cd2b5f8a749c1
Glatzel <chenzelin9794@gmail.com>
2026-03-15 21:53:40 +0800

    ci: use action (#815)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- python/scripts/maturin-develop.ps1
+++ python/scripts/maturin-develop.ps1
@@ -1,8 +1,3 @@
-$ROOT = git rev-parse --show-toplevel
-
 Set-Location $PSScriptRoot/..
 Remove-Item pyxis/pyxis.pyd -ErrorAction SilentlyContinue
-
 pixi run maturin develop
-
-Set-Location $ROOT
--- rust/scripts/setup.ps1
+++ rust/scripts/setup.ps1
@@ -10,24 +10,16 @@ if ($IsMacOS) {
     $env:DYLD_LIBRARY_PATH = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib)`:$env:DYLD_LIBRARY_PATH"
     $env:PROJ_ROOT = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default)"
 }
-if ($IsLinux) {
+if ($IsLinux -and $env:CI) {
     sudo apt update
     sudo apt install -y libudev-dev libc6-dev
 }
 if ($IsLinux -and ($(uname -m) -eq 'x86_64' )) {
-    if ($env:CI) {
-        sudo apt-get update
-        sudo apt install -y libudev-dev libc6-dev
-    }
     $env:Path = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib)`:$env:Path"
     $env:LD_LIBRARY_PATH = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib)`:$env:LD_LIBRARY_PATH"
     $env:PROJ_ROOT = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default)"
 }
 if ($IsLinux -and ($(uname -m) -eq 'aarch64' )) {
-    if ($env:CI) {
-        sudo apt-get update
-        sudo apt install -y libudev-dev libc6-dev
-    }
     $env:Path = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib)`:$env:Path"
     $env:LD_LIBRARY_PATH = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/lib)`:$env:LD_LIBRARY_PATH"
     $env:PROJ_ROOT = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default)"

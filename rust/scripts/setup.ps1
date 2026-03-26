0b38237e0ad745725fd332c9424b7880e184737a
Glatzel <chenzelin9794@gmail.com>
2026-03-26 13:33:43 +0800

    chore: update shared files synced file(s) with Glatzel/template (#838)
    synced local file(s) with
    [Glatzel/template](https://github.com/Glatzel/template).
    
    
    
    <details>
    <summary>Changed files</summary>
    <ul>
    <li>synced local <code>./lefthook.yaml</code> with remote
    <code>./lefthook.yaml</code></li><li>synced local directory
    <code>./.github/actions/rust</code> with remote directory
    <code>./.github/actions/rust</code></li><li>created local directory
    <code>./.github/actions/common</code> and copied all sub files/folders
    from remote directory <code>./.github/actions/common</code></li>
    </ul>
    </details>
    
    ---
    
    This PR was created automatically by the
    [repo-file-sync-action](https://github.com/BetaHuhn/repo-file-sync-action)
    workflow run
    [#23577818807](https://github.com/Glatzel/template/actions/runs/23577818807)

--- rust/scripts/setup.ps1
+++ rust/scripts/setup.ps1
@@ -1,5 +1,4 @@
 Set-Location $PSScriptRoot/..
-git submodule update --init --recursive
 pixi install
 if ($IsWindows) {
     $env:Path = "$(Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/bin);$env:Path"

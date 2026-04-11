85a2d3005b797578ccaa5ba4a27099d8533e0c0c
Glatzel <chenzelin9794@gmail.com>
2026-04-11 09:20:56 +0800

    f

--- rust/crates/proj-sys/build.rs
+++ rust/crates/proj-sys/build.rs
@@ -63,11 +63,11 @@ fn generate_bindings(proj_root: &PathBuf) {
         .unwrap();
     bindings
         .write_to_file("./src/bindings.rs")
-        .expect(format!("Couldn't write bindings to './src/bindings.rs' !"));
+        .expect(format!("Couldn't write bindings to './src/bindings.rs' !").as_str());
     bindings
         .write_to_file(PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs"))
         .expect(format!(
             "Couldn't write bindings to {}",
             env::var("OUT_DIR").unwrap()
-        ));
+        ).as_str());
 }

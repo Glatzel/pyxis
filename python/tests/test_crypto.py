82f7cdf7536dd1a7de75f6f30f225c389caa0f67
Glatzel <chenzelin9794@gmail.com>
2026-07-11 11:42:53 +0800

    chore: update shared files synced file(s) with Glatzel/template (#929)
    synced local file(s) with
    [Glatzel/template](https://github.com/Glatzel/template).
    
    
    
    <details>
    <summary>Changed files</summary>
    <ul>
    <li>created local directory <code>./.github/actions/lefthook/</code> and
    copied all sub files/folders from remote directory
    <code>./.github/actions/lefthook/</code></li><li>synced local
    <code>./lefthook.yaml</code> with remote
    <code>./lefthook.yaml</code></li><li>synced local
    <code>./.github/workflows/ci-lefthook.yml</code> with remote
    <code>./.github/workflows/ci-lefthook.yml</code></li><li>synced local
    <code>./.github/codecov.yml</code> with remote
    <code>./.github/codecov.yml</code></li><li>synced local directory
    <code>./.github/actions/rust</code> with remote directory
    <code>./.github/actions/rust</code></li><li>synced local directory
    <code>./.github/actions/common</code> with remote directory
    <code>./.github/actions/common</code></li>
    </ul>
    </details>
    
    ---
    
    This PR was created automatically by the
    [repo-file-sync-action](https://github.com/BetaHuhn/repo-file-sync-action)
    workflow run
    [#29137366872](https://github.com/Glatzel/template/actions/runs/29137366872)

--- python/tests/test_crypto.py
+++ python/tests/test_crypto.py
@@ -3,55 +3,36 @@ import pytest
 import pyxis
 from pyxis import COORD_CRYPTO_SPACE
 
-bd09 = (121.10271724622564, 30.61484575976839)
-gcj02 = (121.09626927850977, 30.608604368560773)
-wgs84 = (121.0917077, 30.6107779)
-
 
 @pytest.mark.parametrize(
-    ("src", "dst", "input", "expected"),
+    ("src", "dst"),
     [
-        (COORD_CRYPTO_SPACE.WGS84, COORD_CRYPTO_SPACE.BD09, wgs84, bd09),
-        (COORD_CRYPTO_SPACE.WGS84, COORD_CRYPTO_SPACE.GCJ02, wgs84, gcj02),
-        (COORD_CRYPTO_SPACE.GCJ02, COORD_CRYPTO_SPACE.BD09, gcj02, bd09),
-        (COORD_CRYPTO_SPACE.GCJ02, COORD_CRYPTO_SPACE.WGS84, gcj02, wgs84),
-        (COORD_CRYPTO_SPACE.BD09, COORD_CRYPTO_SPACE.GCJ02, bd09, gcj02),
-        (COORD_CRYPTO_SPACE.BD09, COORD_CRYPTO_SPACE.WGS84, bd09, wgs84),
-        pytest.param(
-            COORD_CRYPTO_SPACE.WGS84, COORD_CRYPTO_SPACE.WGS84, bd09, wgs84, marks=pytest.mark.xfail(strict=True)
-        ),
-        pytest.param("cgcs", COORD_CRYPTO_SPACE.WGS84, bd09, wgs84, marks=pytest.mark.xfail(strict=True)),
+        (COORD_CRYPTO_SPACE.WGS84, COORD_CRYPTO_SPACE.BD09),
+        (COORD_CRYPTO_SPACE.WGS84, COORD_CRYPTO_SPACE.GCJ02),
+        (COORD_CRYPTO_SPACE.GCJ02, COORD_CRYPTO_SPACE.BD09),
+        (COORD_CRYPTO_SPACE.GCJ02, COORD_CRYPTO_SPACE.WGS84),
+        (COORD_CRYPTO_SPACE.BD09, COORD_CRYPTO_SPACE.GCJ02),
+        (COORD_CRYPTO_SPACE.BD09, COORD_CRYPTO_SPACE.WGS84),
     ],
 )
 @pytest.mark.parametrize("exact", [True, False])
-def test_convert(src, dst, exact, input, expected):
-    lon, lat = pyxis.crypto(input[0], input[1], src, dst, exact)
-
-    assert lon == pytest.approx(expected[0])
-    assert lat == pytest.approx(expected[1])
-
-
-bd09_array = (np.array([121.10271732371203, 121.10271732371203]), np.array([30.61484572185035, 30.61484572185035]))
-gcj02_array = (np.array([121.09626935575027, 121.09626935575027]), np.array([30.608604331756705, 30.608604331756705]))
-wgs84_array = (
-    np.array([121.0917077, 121.0917077], dtype=np.float64),
-    np.array([30.6107779, 30.6107779], dtype=np.float64),
-)
+def test_convert(src, dst, exact, snapshot):
+    result = pyxis.crypto(120, 30, src, dst, exact)
+    assert result == snapshot(name=f"{src}-{dst}-{exact}")
 
 
 @pytest.mark.parametrize(
-    ("src", "dst", "input", "expected"),
+    ("src", "dst"),
     [
-        (COORD_CRYPTO_SPACE.WGS84, COORD_CRYPTO_SPACE.BD09, wgs84_array, bd09),
-        (COORD_CRYPTO_SPACE.WGS84, COORD_CRYPTO_SPACE.GCJ02, wgs84_array, gcj02),
-        (COORD_CRYPTO_SPACE.GCJ02, COORD_CRYPTO_SPACE.BD09, gcj02_array, bd09),
-        (COORD_CRYPTO_SPACE.GCJ02, COORD_CRYPTO_SPACE.WGS84, gcj02_array, wgs84),
-        (COORD_CRYPTO_SPACE.BD09, COORD_CRYPTO_SPACE.GCJ02, bd09_array, gcj02),
-        (COORD_CRYPTO_SPACE.BD09, COORD_CRYPTO_SPACE.WGS84, bd09_array, wgs84),
+        (COORD_CRYPTO_SPACE.WGS84, COORD_CRYPTO_SPACE.BD09),
+        (COORD_CRYPTO_SPACE.WGS84, COORD_CRYPTO_SPACE.GCJ02),
+        (COORD_CRYPTO_SPACE.GCJ02, COORD_CRYPTO_SPACE.BD09),
+        (COORD_CRYPTO_SPACE.GCJ02, COORD_CRYPTO_SPACE.WGS84),
+        (COORD_CRYPTO_SPACE.BD09, COORD_CRYPTO_SPACE.GCJ02),
+        (COORD_CRYPTO_SPACE.BD09, COORD_CRYPTO_SPACE.WGS84),
     ],
 )
 @pytest.mark.parametrize("exact", [True, False])
-def test_convert_array(src, dst, input, expected, exact):
-    lon, lat = pyxis.crypto(input[0], input[1], src, dst, exact)
-    assert lon[0] == pytest.approx(expected[0])
-    assert lat[0] == pytest.approx(expected[1])
+def test_convert_array(src, dst, exact, snapshot):
+    result = pyxis.crypto(np.array([120, 121]), np.array([30, 31]), src, dst, exact)
+    assert result == snapshot(name=f"{src}-{dst}-{exact}")

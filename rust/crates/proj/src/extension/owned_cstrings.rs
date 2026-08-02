65c2b7d6395dc55c4a92bff5c80c7f713f8b3879
Glatzel <chenzelin9794@gmail.com>
2026-08-02 21:08:37 +0800

    Update owned_cstrings.rs

--- rust/crates/proj/src/extension/owned_cstrings.rs
+++ rust/crates/proj/src/extension/owned_cstrings.rs
@@ -9,26 +9,26 @@ use crate::data_types::ProjError;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct OwnedCStrings {
-    _owned_cstrings: Vec<CString>,
+    owned_cstrings: Vec<CString>,
 }
 impl OwnedCStrings {
     pub fn new() -> Self {
         Self {
-            _owned_cstrings: Vec::with_capacity(0),
+            owned_cstrings: Vec::with_capacity(0),
         }
     }
     pub fn with_capacity(n: usize) -> Self {
         Self {
-            _owned_cstrings: Vec::with_capacity(n),
+            owned_cstrings: Vec::with_capacity(n),
         }
     }
     pub fn push<T>(&mut self, value: T) -> Result<*const c_char, ProjError>
     where
         T: ToCString,
     {
-        self._owned_cstrings.push(value.to_cstring()?);
+        self.owned_cstrings.push(value.to_cstring()?);
         Ok(self
-            ._owned_cstrings
+            .owned_cstrings
             .last()
             .ok_or_else(|| ProjError::new("Last owned cstring is missing.".to_string()))?
             .as_ptr())
@@ -42,5 +42,5 @@ impl OwnedCStrings {
             None => Ok(ptr::null()),
         }
     }
-    pub const fn len(&self) -> usize { self._owned_cstrings.len() }
+    pub const fn len(&self) -> usize { self.owned_cstrings.len() }
 }

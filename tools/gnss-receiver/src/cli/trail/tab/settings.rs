5512a5a85b444ccefb1cc38e54173640e6836e05
Glatzel <chenzelin9794@gmail.com>
2026-08-05 11:50:51 +0800

    f

--- tools/gnss-receiver/src/cli/trail/tab/settings.rs
+++ tools/gnss-receiver/src/cli/trail/tab/settings.rs
@@ -12,7 +12,7 @@ impl super::ITab for TabSettings {
     fn handle_mouse(&mut self, _mouse: crossterm::event::MouseEvent) {}
     fn draw(
         &mut self,
-        f: &mut ratatui::Frame,
+        f: &mut ratatui::Frame<'_>,
         area: ratatui::layout::Rect,
         _raw_nmea: &VecDeque<(Talker, Identifier, String)>,
     ) -> mischief::Result<()> {

85d01923e757dcd0657647f080fca3a44debe013
Glatzel <chenzelin9794@gmail.com>
2026-08-31 12:35:02 +0800

    chore: update shared files synced file(s) with Glatzel/template (#1006)
    synced local file(s) with
    [Glatzel/template](https://github.com/Glatzel/template).
    
    
    
    <details>
    <summary>Changed files</summary>
    <ul>
    <li>synced local <code>./.github/workflows/ci-lefthook.yml</code> with
    remote <code>./.github/workflows/ci-lefthook.yml</code></li><li>synced
    local <code>./.github/workflows/changelog.yml</code> with remote
    <code>./.github/workflows/changelog.yml</code></li><li>synced local
    <code>./.github/workflows/release.yml</code> with remote
    <code>./.github/workflows/release.yml</code></li>
    </ul>
    </details>
    
    ---
    
    This PR was created automatically by the
    [repo-file-sync-action](https://github.com/BetaHuhn/repo-file-sync-action)
    workflow run
    [#33349511747](https://github.com/Glatzel/template/actions/runs/33349511747)
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tools/gnss-receiver/src/cli/trail/app.rs
+++ tools/gnss-receiver/src/cli/trail/app.rs
@@ -21,7 +21,8 @@ pub struct App {
 
 impl App {
     pub fn new() -> mischief::Result<Self> {
-        // let raw_nmea = VecDeque::with_capacity(SETTINGS.lock().trail.capacity);
+        // let raw_nmea =
+        // VecDeque::with_capacity(SETTINGS.lock().trail.capacity);
         Ok(Self {
             status: 0,
 
